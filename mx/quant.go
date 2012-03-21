65f11ef7276d4b8b22a1ef718a6c794deabbcc66
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-21 16:10:34 +0100

    uniform3 fix

--- mx/quant.go
+++ mx/quant.go
@@ -13,25 +13,29 @@ type Quant interface {
 // Uniform quantity is uniform over space.
 type Uniform interface {
 	Quant
-	Get(comp int) float32
+	Get(comp int) float32 // Get the specified component, independently of position.
 }
 
+// Scalar quantity has 1 component.
 type Scalar interface {
 	Quant
-	IGet1(index int) float32
+	IGet1(index int) float32 // Get the scalar value for this position index.
 }
 
+// UniformScalar is uniform over space and has 1 component.
 type UniformScalar interface {
 	Quant
-	Get1() float32
+	Get1() float32 // Get the scalar value, independently of position.
 }
 
+// Vector quantity has 3 components.
 type Vector interface {
 	Quant
-	IGet3(index int) [3]float32
+	IGet3(index int) [3]float32 // Get the vector value for this position index.
 }
 
+// UniformVector is uniform over space and has 3 components.
 type UniformVector interface {
 	Quant
-	IGet3(index int) [3]float32
+	Get3() [3]float32 // 
 }

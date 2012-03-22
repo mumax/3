b9ff3eaad9ec427c1759a1fd53497387995ad676
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-22 15:07:12 +0100

    update whiteboard graph

--- mx/list.go
+++ mx/list.go
@@ -26,35 +26,3 @@ func (this *List) IGet(comp, index int) float32 {
 func (this *List) NComp() int {
 	return len(this.Comp)
 }
-
-//// Allocates an returns a new Array
-//func NewArray(components int, size3D []int) *Array {
-//	t := new(Array)
-//	t.Init(components, size3D)
-//	return t
-//}
-//
-//func (a *Array) Rank() int {
-//	return len(a.Size)
-//}
-//
-//func (a *Array) Len() int {
-//	return a.Size[0] * a.Size[1] * a.Size[2] * a.Size[3]
-//}
-//
-//func (a *Array) NComp() int {
-//	return a.Size[0]
-//}
-//
-//// Component array, shares storage with original
-//func (a *Array) Component(component int) *Array {
-//	comp := new(Array)
-//	copy(comp.Size[:], a.Size[:])
-//	comp.Size[0] = 1 // 1 component
-//	comp.Size4D = comp.Size[:]
-//	comp.Size3D = comp.Size[1:]
-//	comp.List = a.Comp[component]
-//	comp.Array = Slice4D(comp.List, comp.Size4D)
-//	comp.Comp = Slice2D(comp.List, []int{1, Prod(comp.Size3D)})
-//	return comp
-//}

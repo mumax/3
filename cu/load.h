ed1a4c426b27c8c588ba182568f6f33387c94b7b
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-03-27 17:43:55 +0200

    minor changes

--- cu/load.h
+++ cu/load.h
@@ -3,10 +3,11 @@
 
 #include "geom.h"
 
-#define load_vector(out_x, out_y, out_z, in, i)\
+#define load_vector(out_x, out_y, out_z, in, i){\
 	out_x = in[0*Ncell + i]; \
 	out_y = in[1*Ncell + i]; \
-	out_z = in[2*Ncell + i];
+	out_z = in[2*Ncell + i]; \
+}
 
 #define load_uniformscalar(out, in)\
 	out = in;

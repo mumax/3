7572ae835ebb33adaa5cd30d31224e017a32c658
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-05-23 20:56:58 +0200

    backup

--- nc/vector.go
+++ nc/vector.go
@@ -6,12 +6,16 @@ import (
 
 type Vector [3]float32
 
-const(
-	X=0
-	Y=1
-	Z=2
+const (
+	X = 0
+	Y = 1
+	Z = 2
 )
 
-func(v*Vector)String()  string{
+func (v *Vector) String() string {
 	return fmt.Sprint(v[X], ", ", v[Y], ",", v[Z])
+}
+
+func (a Vector) Add(b Vector) Vector {
+	return Vector{a[X] + b[X], a[Y] + b[Y], a[Z] + b[Z]}
 }
--- nc/vector_test.go
+++ nc/vector_test.go
@@ -1,12 +1,21 @@
 package nc
 
-import(
+import (
 	"testing"
 )
 
-func BenchmarkString(bench *testing.B){
-	v:=Vector{1,2,3}
-	for i:=0;i<bench.N; i++{
+func BenchmarkString(bench *testing.B) {
+	v := Vector{1, 2, 3}
+	for i := 0; i < bench.N; i++ {
 		v.String()
+	}
+}
+
+
+func BenchmarkAdd(bench *testing.B) {
+	a := Vector{1, 2, 3}
+	b := Vector{1, 2, 3}
+	for i := 0; i < bench.N; i++ {
+		a.Add(b)
 	}
 }

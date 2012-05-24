c51f4b74b475c28afc8b9ae8797c551b252301a1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-05-24 08:50:04 +0200

    benchmark sqrtf

--- nc/sqrtf_test.go
+++ nc/sqrtf_test.go
@@ -5,13 +5,37 @@ import (
 	"testing"
 )
 
-func TestSqrtf(t *testing.T) {
+func TestSqrtf(test *testing.T) {
 	f := Sqrtf(2)
 	if f != 1.41421356237 {
-		t.Error("Sqrtf(2):", f)
+		test.Error("Sqrtf(2):", f)
 	}
 	f = Sqrtf(-1)
 	if !math.IsNaN(float64(f)) {
-		t.Error("Sqrtf(-1):", f)
+		test.Error("Sqrtf(-1):", f)
 	}
+}
+
+func BenchmarkSqrtf(bench *testing.B) {
+	var a float32
+	for i := 0; i < bench.N; i++ {
+		a = Sqrtf(15.7)
+	}
+	use(a)
+}
+
+// Portable sqrtf.
+func sqrtf(x float32) float32 {
+	return float32(math.Sqrt(float64(x)))
+}
+
+// Avoids "unused variable" for benchmark funcs.
+func use(x interface{}) {}
+
+func BenchmarkSqrtfPortable(bench *testing.B) {
+	var a float32
+	for i := 0; i < bench.N; i++ {
+		a = sqrtf(15.7)
+	}
+	use(a)
 }

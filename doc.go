8dc184c516ae63af34ca62bbf56a19ab41c43bf8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-05-30 11:42:03 +0200

    move completely to github

--- doc.go
+++ doc.go
@@ -3,7 +3,10 @@
 */
 package cuda4
 
+// Dummy imports so that
+// 	go get github.com/barnex/cuda4
+// will install everything.
 import(
-	_ "cuda4/cu"
-	_ "cuda4/cufft"
+	_ "github.com/barnex/cuda4/cu"
+	_ "github.com/barnex/cuda4/cufft"
 )

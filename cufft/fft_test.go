9cf0884c951fb057f55b99453d73bb44e7c0a7c8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-05-30 12:29:59 +0200

    add fft_test.go

---
+++ cufft/fft_test.go
@@ -0,0 +1,24 @@
+package cufft
+
+import (
+	"github.com/barnex/cuda4/cu"
+	"testing"
+	"unsafe"
+)
+
+func ExampleFFT1D(test *testing.T) {
+	N := 5
+
+	hostIn := make([]float32, N)
+	devIn := cu.MemAlloc(int64(len(hostIn)) * cu.SIZEOF_FLOAT32)
+	defer cu.MemFree(&devIn)
+	cu.MemcpyHtoD(devIn, unsafe.Pointer(&hostIn[0]), devIn.Bytes())
+
+	hostOut := make([]complex64, N/2+1)
+	devOut := cu.MemAlloc(int64(len(hostOut)) * cu.SIZEOF_COMPLEX64)
+	defer cu.MemFree(&devOut)
+
+	plan := Plan1d(N, R2C, 1)
+
+	plan.ExecR2C(devIn, devOut)
+}

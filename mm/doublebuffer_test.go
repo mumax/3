2a47c7068fed91b0c2d18073bd53959e8da5913f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-01 16:13:11 +0200

    [BROKEN] two-way channels seem too complicated

---
+++ mm/doublebuffer_test.go
@@ -0,0 +1,5 @@
+package mm
+
+import (
+	"testing"
+)
--- mm/mm.go
+++ mm/mm.go
@@ -9,6 +9,7 @@ var (
 	size [3]int // 3D geom size
 	N    int    // product of size
 	warp int    // buffer size for Range()
+
 	//m0, m1 VectorBlock // reduced magnetization, at old and new t
 	//heff   VectorBlock // effective field
 	//gamma  float32     // gyromagnetic ratio
@@ -18,6 +19,18 @@ var (
 
 func Main() {
 
+	initSize()
+
+	m := MakeDoubleBuffer(N)
+
+	pipe := MakePipe()
+	m.SendPipe = pipe.SendPipe()
+	m.RecvPipe = pipe.RecvPipe()
+
+	m.Cycle()
+}
+
+func initSize() {
 	N0, N1, N2 := 1, 4, 8
 	size := [3]int{N0, N1, N2}
 	N = N0 * N1 * N2
@@ -32,8 +45,4 @@ func Main() {
 		warp--
 	}
 	log.Println("warp:", warp)
-
-}
-
-type Torque struct {
 }

b0bda717953e5f424628dcbe47a3452589883399
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-11 15:38:09 +0200

    return of the fat arrow

--- nc/bookkeeper.go
+++ nc/bookkeeper.go
@@ -42,7 +42,14 @@ func WriteGraph() {
 				boxName[i] = channame(conn[i])
 			}
 		}
-		dot.Connect(boxName[DST], boxName[SRC], tagOfChan[conn[SRC]], 1)
+
+		// weight of the arrow is number of components.
+		w := 1
+		v := reflect.ValueOf(conn[SRC]).Elem()
+		if v.Kind() == reflect.Array {
+			w = v.Len()
+		}
+		dot.Connect(boxName[DST], boxName[SRC], tagOfChan[conn[SRC]], w)
 	}
 
 }
@@ -121,57 +128,3 @@ func boxname(value Box) string {
 func channame(c Chan) string {
 	return fmt.Sprintf("chan0x%x", int(reflect.ValueOf(c).Elem().UnsafeAddr()))
 }
-
-// Register a quantity taken form channel, give it a name.
-//func RegisterTag(box Box, chanPtr Chan, name string) { // rm box, use boxforchanptr, mv SendQuant
-//	Assert(isOutputChan(chanPtr))
-//	setBoxFor(name, box)
-//	setChanFor(name, chanPtr)
-//	log.Println("[plumber]", boxname(box), "provides", name)
-//	registerComponentQuants(box, chanPtr, name)
-//}
-
-// Automatically register components of vector quantities as "quant.x", ...
-//func registerComponentQuants(box Box, chanPtr Chan, name string) { // rm box
-//	Assert(isOutputChan(chanPtr))
-//	switch c := chanPtr.(type) {
-//	default:
-//		return // nothing to see here
-//	case *[3][]chan<- []float32:
-//		RegisterTag(box, &c[X], name+".x")
-//		RegisterTag(box, &c[Y], name+".y")
-//		RegisterTag(box, &c[Z], name+".z")
-//	case *[3][]chan<- float64:
-//		RegisterTag(box, &c[X], name+".x")
-//		RegisterTag(box, &c[Y], name+".y")
-//		RegisterTag(box, &c[Z], name+".z")
-//	}
-//}
-
-//func srcBoxFor(quant string) Box {
-//	if b, ok := srcBoxForQuant[quant]; ok {
-//		return b
-//	}
-//	panic("no such quantity " + quant)
-//}
-//
-//func srcChanFor(quant string) Chan {
-//	if b, ok := srcChanForQuant[quant]; ok {
-//		return b
-//	}
-//	panic("no such quantity " + quant)
-//}
-//
-//func setBoxFor(quant string, box Box) {
-//	if _, ok := srcBoxForQuant[quant]; ok {
-//		panic("already defined " + quant)
-//	}
-//	srcBoxForQuant[quant] = box
-//}
-//
-//func setChanFor(quant string, c Chan) {
-//	if _, ok := srcChanForQuant[quant]; ok {
-//		panic("already defined " + quant)
-//	}
-//	srcChanForQuant[quant] = c
-//}

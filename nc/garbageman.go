6e210497f2494e8e92d27dda97054a751b9a56e3
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-12 21:16:38 +0200

    gc without separate goroutine

--- nc/garbageman.go
+++ nc/garbageman.go
@@ -10,30 +10,10 @@ import (
 )
 
 var (
-	garbage  chan []float32
-	fresh    chan []float32
+	fresh    chan []float32 // used as fifo, replace by hand-written stack!
 	refcount = make(map[*float32]int)
 	lock     sync.Mutex
 )
-
-func runGC() {
-	log.Println("GC started")
-	for {
-		g := <-garbage
-		count := decr(g)
-		Assert(count > -2)
-		if count == -1 {
-			incr(g, 1) // set to 0 for next use.
-			select {
-			case fresh <- g:
-				log.Println("recycle", &g[0])
-			default:
-				log.Println("spilling", &g[0])
-				delete(refcount, &g[0]) // allow it to be GC'd
-			}
-		}
-	}
-}
 
 func incr(s []float32, count int) {
 	lock.Lock()
@@ -49,34 +29,34 @@ func incr3(s [3][]float32, count int) {
 	refcount[&s[Z][0]] += count
 }
 
-func decr(s []float32) (count int) {
-	lock.Lock()
-	defer lock.Unlock()
-	count = refcount[&s[0]]
-	count--
-	refcount[&s[0]] = count
-	return count
-}
+//func decr(s []float32) (count int) {
+//	lock.Lock()
+//	defer lock.Unlock()
+//	count = refcount[&s[0]]
+//	count--
+//	refcount[&s[0]] = count
+//	return count
+//}
 
-func count(s []float32) (count int) {
-	lock.Lock()
-	defer lock.Unlock()
-	return refcount[&s[0]]
-}
+//func count(s []float32) (count int) {
+//	lock.Lock()
+//	defer lock.Unlock()
+//	return refcount[&s[0]]
+//}
 
 func Buffer() []float32 {
 	lock.Lock()
 	defer lock.Unlock()
-	//log.Println("buffer for", caller())
-	//log.Println(refcount)
+
 	select {
-	default:
-		slice := make([]float32, WarpLen())
-		log.Println("alloc", &slice[0])
-		return slice
 	case f := <-fresh:
 		log.Println("re-use", &f[0])
 		return f
+	default:
+		slice := make([]float32, WarpLen())
+		log.Println("alloc", &slice[0])
+		refcount[&slice[0]] = 0
+		return slice
 	}
 	return nil // silence gc
 }
@@ -87,24 +67,34 @@ func caller() string {
 }
 
 func Buffer3() [3][]float32 {
-	//log.Println("buffer3 for", caller())
 	return [3][]float32{Buffer(), Buffer(), Buffer()}
 }
 
 func initGarbageman() {
-	garbage = make(chan []float32, 0)
 	fresh = make(chan []float32, 5*NumWarp()) // need big buffer to avoid spilling
-	go runGC()
 }
 
 func Recycle(garbages ...[]float32) {
+	lock.Lock()
+	defer lock.Unlock()
+
 	for _, g := range garbages {
 
-		if count(g) < 0 {
-			Panic("ref count", count(g))
+		count, ok := refcount[&g[0]]
+		if !ok {
+			continue // slice does not originate from here
 		}
-
-		garbage <- g
+		if count == 0 { // can be recycled
+			select {
+			case fresh <- g:
+				log.Println("recycle", &g[0])
+			default:
+				log.Println("spilling", &g[0])
+				delete(refcount, &g[0]) // allow it to be GC'd
+			}
+		} else { // cannot be recycled, just yet
+			refcount[&g[0]] = count - 1
+		}
 	}
 }
 

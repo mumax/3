bb9d314bc4edb5f55cc72bbdac77c4c633176622
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-13 12:09:29 +0200

    cleanup

--- nc/wizard.go
+++ nc/wizard.go
@@ -87,42 +87,6 @@ func isConnected(field reflect.Value) bool {
 	return false // silence 6g
 }
 
-// Check boxes for unconnected inputs.
-// TODO: sources connected to the same dest box twice might also need checking
-func Vet(boxes ...Box) {
-	Register(boxes...)
-	ok := true
-	for _, box := range boxes {
-		val := reflect.ValueOf(box).Elem()
-		typ := val.Type()
-
-		for i := 0; i < typ.NumField(); i++ {
-			field := val.Field(i)
-
-			// skip unexported
-			if unicode.IsLower(rune(typ.Field(i).Name[0])) {
-				continue
-			}
-
-			// only consider channels
-			dst := field.Addr().Interface()
-			if !isChan(dst) {
-				continue
-			}
-
-			// check connected
-			tag := string(typ.Field(i).Tag)
-			if !isConnected(field) {
-				log.Println("vet: not connected:", boxname(box), typ.Field(i).Name, tag)
-				ok = false
-			}
-		}
-	}
-	if !ok {
-		Panic("vet error(s)")
-	}
-}
-
 // Vet and Run all boxes.
 func GoRun(box ...Runner) {
 	// first vet all boxes at once

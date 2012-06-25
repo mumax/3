e5facf32ef814c049742c2268a8e07f229c912ed
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-25 10:04:29 +0200

    set/get ctx benchmark

--- cu/context_test.go
+++ cu/context_test.go
@@ -14,3 +14,22 @@ func TestContext(t *testing.T) {
 	fmt.Println("CtxGetDevice:", CtxGetDevice())
 	(&ctx).Destroy()
 }
+
+func BenchmarkGetContext(b *testing.B) {
+	b.StopTimer()
+	ctx := CtxCreate(CTX_SCHED_AUTO, 0)
+	CtxSetCurrent(ctx)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		CtxGetCurrent()
+	}
+}
+
+func BenchmarkSetContext(b *testing.B) {
+	b.StopTimer()
+	ctx := CtxCreate(CTX_SCHED_AUTO, 0)
+	b.StartTimer()
+	for i := 0; i < b.N; i++ {
+		ctx.SetCurrent()
+	}
+}

b67950022a1e55e93c4f8603011b95a5d309c27c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-26 17:16:11 +0200

    add complex64s

--- safe/complex64s.go
+++ safe/complex64s.go
@@ -1,6 +1,9 @@
 package safe
 
-import "unsafe"
+import (
+	"github.com/barnex/cuda4/cu"
+	"unsafe"
+)
 
 // Slice of complex64's on the GPU.
 type Complex64s struct{ slice }
@@ -8,23 +11,44 @@ type Complex64s struct{ slice }
 // Make a slice of complex64's on the GPU.
 // Initialized to zero.
 func MakeComplex64s(len_ int) Complex64s {
-	return Complex64s{makeslice(len_, sizeofComplex64)}
+	return Complex64s{makeslice(len_, cu.SIZEOF_COMPLEX64)}
 }
 
+// Return a slice from start (inclusive) to stop (exclusive),
+// sharing the underlying storage with the original slice.
+// Slices obtained in this way should not be Free()'d
 func (s Complex64s) Slice(start, stop int) Complex64s {
-	return Complex64s{s.slice.slice(start, stop, sizeofComplex64)}
+	return Complex64s{s.slice.slice(start, stop, cu.SIZEOF_COMPLEX64)}
 }
 
+// Copy src from host to dst on the device.
 func (dst Complex64s) CopyHtoD(src []complex64) {
-	dst.copyHtoD(unsafe.Pointer(&src[0]), len(src), sizeofComplex64)
+	dst.copyHtoD(unsafe.Pointer(&src[0]), len(src), cu.SIZEOF_COMPLEX64)
 }
 
+// Copy src form device to dst on host.
 func (src Complex64s) CopyDtoH(dst []complex64) {
-	src.copyDtoH(unsafe.Pointer(&dst[0]), len(dst), sizeofComplex64)
+	src.copyDtoH(unsafe.Pointer(&dst[0]), len(dst), cu.SIZEOF_COMPLEX64)
 }
 
+// Copy src on host to dst on host.
 func (dst Complex64s) CopyDtoD(src Complex64s) {
-	dst.copyDtoD(&src.slice, sizeofComplex64)
+	dst.copyDtoD(&src.slice, cu.SIZEOF_COMPLEX64)
+}
+
+// Copy src from host to dst on the device, asynchronously.
+func (dst Complex64s) CopyHtoDAsync(src []complex64, stream cu.Stream) {
+	dst.copyHtoDAsync(unsafe.Pointer(&src[0]), len(src), cu.SIZEOF_COMPLEX64, stream)
+}
+
+// Copy src form device to dst on host, asynchronously.
+func (src Complex64s) CopyDtoHAsync(dst []complex64, stream cu.Stream) {
+	src.copyDtoHAsync(unsafe.Pointer(&dst[0]), len(dst), cu.SIZEOF_COMPLEX64, stream)
+}
+
+// Copy src on host to dst on host, asynchronously.
+func (dst Complex64s) CopyDtoDAsync(src Complex64s, stream cu.Stream) {
+	dst.copyDtoDAsync(&src.slice, cu.SIZEOF_COMPLEX64, stream)
 }
 
 // Returns a fresh copy on host.
@@ -34,4 +58,3 @@ func (src Complex64s) Host() []complex64 {
 	return cpy
 }
 
-const sizeofComplex64 = 8
--- safe/subs.sh
+++ safe/subs.sh
@@ -2,6 +2,12 @@
 
 subs32='s/loat32/loat64/g;'
 subs32+='s/FLOAT32/FLOAT64/g;'
-#sed $subs32 float32s.go > float64s.go
 
-sed $subs32 float32s_test.go > float64s_test.go
+#sed $subs32 float32s.go > float64s.go
+#sed $subs32 float32s_test.go > float64s_test.go
+
+subsc64='s/Float32/Complex64/g;'
+subsc64+='s/float32/complex64/g;'
+subsc64+='s/FLOAT32/COMPLEX64/g;'
+#sed $subsc64 float32s_test.go > complex64s_test.go
+#sed $subsc64 float32s.go > complex64s.go

1e97d7550274e5d52ce2aa9edb56d42e66f02648
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-26 17:12:19 +0200

    subs float64s

--- safe/float64s.go
+++ safe/float64s.go
@@ -1,27 +1,54 @@
 package safe
 
-import "unsafe"
+import (
+	"github.com/barnex/cuda4/cu"
+	"unsafe"
+)
 
+// Slice of float64's on the GPU.
 type Float64s struct{ slice }
 
+// Make a slice of float64's on the GPU.
+// Initialized to zero.
 func MakeFloat64s(len_ int) Float64s {
-	return Float64s{makeslice(len_, sizeofFloat64)}
+	return Float64s{makeslice(len_, cu.SIZEOF_FLOAT64)}
 }
 
+// Return a slice from start (inclusive) to stop (exclusive),
+// sharing the underlying storage with the original slice.
+// Slices obtained in this way should not be Free()'d
 func (s Float64s) Slice(start, stop int) Float64s {
-	return Float64s{s.slice.slice(start, stop, sizeofFloat64)}
+	return Float64s{s.slice.slice(start, stop, cu.SIZEOF_FLOAT64)}
 }
 
+// Copy src from host to dst on the device.
 func (dst Float64s) CopyHtoD(src []float64) {
-	dst.copyHtoD(unsafe.Pointer(&src[0]), len(src), sizeofFloat64)
+	dst.copyHtoD(unsafe.Pointer(&src[0]), len(src), cu.SIZEOF_FLOAT64)
 }
 
+// Copy src form device to dst on host.
 func (src Float64s) CopyDtoH(dst []float64) {
-	src.copyDtoH(unsafe.Pointer(&dst[0]), len(dst), sizeofFloat64)
+	src.copyDtoH(unsafe.Pointer(&dst[0]), len(dst), cu.SIZEOF_FLOAT64)
 }
 
+// Copy src on host to dst on host.
 func (dst Float64s) CopyDtoD(src Float64s) {
-	dst.copyDtoD(&src.slice, sizeofFloat64)
+	dst.copyDtoD(&src.slice, cu.SIZEOF_FLOAT64)
+}
+
+// Copy src from host to dst on the device, asynchronously.
+func (dst Float64s) CopyHtoDAsync(src []float64, stream cu.Stream) {
+	dst.copyHtoDAsync(unsafe.Pointer(&src[0]), len(src), cu.SIZEOF_FLOAT64, stream)
+}
+
+// Copy src form device to dst on host, asynchronously.
+func (src Float64s) CopyDtoHAsync(dst []float64, stream cu.Stream) {
+	src.copyDtoHAsync(unsafe.Pointer(&dst[0]), len(dst), cu.SIZEOF_FLOAT64, stream)
+}
+
+// Copy src on host to dst on host, asynchronously.
+func (dst Float64s) CopyDtoDAsync(src Float64s, stream cu.Stream) {
+	dst.copyDtoDAsync(&src.slice, cu.SIZEOF_FLOAT64, stream)
 }
 
 // Returns a fresh copy on host.
@@ -30,5 +57,3 @@ func (src Float64s) Host() []float64 {
 	src.CopyDtoH(cpy)
 	return cpy
 }
-
-const sizeofFloat64 = 8
--- safe/subs.sh
+++ safe/subs.sh
@@ -1,3 +1,7 @@
 #! /bin/bash
 
-sed 's/loat32/loat64/g' float32s.go
+subs32='s/loat32/loat64/g;'
+subs32+='s/FLOAT32/FLOAT64/g;'
+#sed $subs32 float32s.go > float64s.go
+
+sed $subs32 float32s_test.go > float64s_test.go

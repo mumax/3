a214d9b8dbee518eca474df61351e9f86a99a89e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-27 14:36:25 +0200

    fft3dc2r OK

--- safe/fft3d_test.go
+++ safe/fft3d_test.go
@@ -33,3 +33,65 @@ func ExampleFFT3DR2C() {
 	// input: [[[1 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]] [[1 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]]]
 	// output: [[[(2+0i) (+2+0i) (+2+0i) (+2-0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2-0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2-0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2-0i) (+2+0i)]] [[(+0+0i) (+0+0i) (+0+0i) (+0-0i) (+0+0i)] [(+0+0i) (+0+0i) (+0+0i) (+0-0i) (+0+0i)] [(+0+0i) (+0+0i) (+0+0i) (+0-0i) (+0+0i)] [(+0+0i) (+0+0i) (+0+0i) (+0-0i) (+0+0i)]]]
 }
+
+func ExampleFFT3DC2R() {
+	InitCuda()
+
+	Nx, Ny, Nz := 2, 4, 8
+
+	fft := FFT3DC2R(Nx, Ny, Nz)
+	defer fft.Destroy()
+
+	input := MakeComplex64s(fft.InputLen())
+	defer input.Free()
+
+	inputData := make([]complex64, fft.InputLen())
+	for i := range inputData {
+		inputData[i] = 2
+	}
+	input.CopyHtoD(inputData)
+
+	output := MakeFloat32s(fft.OutputLen())
+	defer output.Free()
+
+	fft.Exec(input, output)
+
+	Ix, Iy, Iz := fft.InputSize()
+	fmt.Println("input:", Reshape3DComplex64(input.Host(), Ix, Iy, Iz))
+	fmt.Println("output:", Reshape3DFloat32(output.Host(), Nx, Ny, Nz))
+
+	// Output:
+	// input: [[[(2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)]] [[(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)] [(+2+0i) (+2+0i) (+2+0i) (+2+0i) (+2+0i)]]]
+	// output: [[[128 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]] [[0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]]]
+}
+
+func ExampleFFT3D() {
+	InitCuda()
+
+	Nx, Ny, Nz := 2, 4, 8
+
+	forward := FFT3DR2C(Nx, Ny, Nz)
+	defer forward.Destroy()
+
+	input := MakeFloat32s(forward.InputLen())
+	defer input.Free()
+
+	inputData := make([]float32, forward.InputLen())
+	inputData[5] = 1
+	input.CopyHtoD(inputData)
+
+	output := MakeComplex64s(forward.OutputLen())
+	defer output.Free()
+
+	forward.Exec(input, output)
+
+	backward := FFT3DC2R(Nx, Ny, Nz)
+	backward.Exec(output, input)
+
+	fmt.Println("input:", Reshape3DFloat32(inputData, Nx, Ny, Nz))
+	fmt.Println("forward+inverse:", Reshape3DFloat32(input.Host(), Nx, Ny, Nz))
+
+	// Output:
+	// input: [[[0 0 0 0 0 1 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]] [[0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]]]
+	// forward+inverse: [[[0 0 0 0 0 64 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]] [[0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0] [0 0 0 0 0 0 0 0]]]
+}
--- safe/fft3dc2r.go
+++ safe/fft3dc2r.go
@@ -6,20 +6,20 @@ import (
 )
 
 // 3D single-precission real-to-complex FFT plan.
-type FFT3DR2CPlan struct {
+type FFT3DC2RPlan struct {
 	fftplan
 	size3D
 }
 
 // 3D single-precission real-to-complex FFT plan.
-func FFT3DR2C(Nx, Ny, Nz int) FFT3DR2CPlan {
-	handle := cufft.Plan3d(Nx, Ny, Nz, cufft.R2C)
-	return FFT3DR2CPlan{fftplan{handle, 0}, size3D{Nx, Ny, Nz}}
+func FFT3DC2R(Nx, Ny, Nz int) FFT3DC2RPlan {
+	handle := cufft.Plan3d(Nx, Ny, Nz, cufft.C2R)
+	return FFT3DC2RPlan{fftplan{handle, 0}, size3D{Nx, Ny, Nz}}
 }
 
 // Execute the FFT plan.
 // src and dst are 3D arrays stored 1D arrays.
-func (p FFT3DR2CPlan) Exec(src Float32s, dst Complex64s) {
+func (p FFT3DC2RPlan) Exec(src Complex64s, dst Float32s) {
 	oksrclen := p.InputLen()
 	if src.Len() != oksrclen {
 		panic(fmt.Errorf("size mismatch: expecting src len %v, got %v", oksrclen, src.Len()))
@@ -28,25 +28,25 @@ func (p FFT3DR2CPlan) Exec(src Float32s, dst Complex64s) {
 	if dst.Len() != okdstlen {
 		panic(fmt.Errorf("size mismatch: expecting dst len %v, got %v", okdstlen, dst.Len()))
 	}
-	p.handle.ExecR2C(src.Pointer(), dst.Pointer())
+	p.handle.ExecC2R(src.Pointer(), dst.Pointer())
 }
 
 // 3D size of the input array.
-func (p FFT3DR2CPlan) InputSize() (Nx, Ny, Nz int) {
-	return p.size3D[0], p.size3D[1], p.size3D[2]
-}
-
-// 3D size of the output array.
-func (p FFT3DR2CPlan) OutputSize() (Nx, Ny, Nz int) {
+func (p FFT3DC2RPlan) InputSize() (Nx, Ny, Nz int) {
 	return p.size3D[0], p.size3D[1], p.size3D[2]/2 + 1
 }
 
+// 3D size of the output array.
+func (p FFT3DC2RPlan) OutputSize() (Nx, Ny, Nz int) {
+	return p.size3D[0], p.size3D[1], p.size3D[2]
+}
+
 // Required length of the (1D) input array.
-func (p FFT3DR2CPlan) InputLen() int {
+func (p FFT3DC2RPlan) InputLen() int {
 	return prod3(p.InputSize())
 }
 
 // Required length of the (1D) output array.
-func (p FFT3DR2CPlan) OutputLen() int {
+func (p FFT3DC2RPlan) OutputLen() int {
 	return prod3(p.OutputSize())
 }

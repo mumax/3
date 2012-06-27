ffc0d71c57276ee048a47433bd2a2532b7bf46cc
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-06-27 10:08:10 +0200

    cleanup fftplan

---
+++ safe/fft1dr2c.go
@@ -0,0 +1,42 @@
+package safe
+
+import (
+	"fmt"
+	"github.com/barnex/cuda4/cufft"
+)
+
+// 1D single-precission real-to-complex FFT plan.
+type FFT1DR2CPlan struct {
+	fftplan
+	size1D
+	batch int
+}
+
+// 1D single-precission real-to-complex FFT plan.
+func FFT1DR2C(size, batch int) FFT1DR2CPlan {
+	handle := cufft.Plan1d(size, cufft.R2C, batch)
+	return FFT1DR2CPlan{fftplan{handle, 0}, size1D(size), batch}
+}
+
+// Execute the FFT plan.
+func (p FFT1DR2CPlan) Exec(src Float32s, dst Complex64s) {
+	oksrclen := p.InputLen()
+	if src.Len() != oksrclen {
+		panic(fmt.Errorf("size mismatch: expecting src len %v, got %v", oksrclen, src.Len()))
+	}
+	okdstlen := p.OutputLen()
+	if dst.Len() != okdstlen {
+		panic(fmt.Errorf("size mismatch: expecting dst len %v, got %v", okdstlen, dst.Len()))
+	}
+	p.handle.ExecR2C(src.Pointer(), dst.Pointer())
+}
+
+// Required length of the input array.
+func (p FFT1DR2CPlan) InputLen() int {
+	return p.batch * p.Size()
+}
+
+// Required length of the output array.
+func (p FFT1DR2CPlan) OutputLen() int {
+	return p.batch * (p.Size()/2 + 1)
+}
--- safe/fftplan.go
+++ safe/fftplan.go
@@ -1,46 +1,33 @@
 package safe
 
+// INTERNAL
+// Base implementation for all FFT plans.
+
 import (
-	"fmt"
 	"github.com/barnex/cuda4/cu"
 	"github.com/barnex/cuda4/cufft"
 )
 
+// Base implementation for all FFT plans.
 type fftplan struct {
 	handle cufft.Handle
-	cu.Stream
+	stream cu.Stream
 }
 
+// For the sake of embedding.
 type size1D int
 
 func (s size1D) Size() int { return int(s) }
 
 func (p fftplan) Destroy() { p.handle.Destroy() }
 
+// Associates a CUDA stream with the FFT plan.
 func (p fftplan) SetStream(stream cu.Stream) {
 	p.handle.SetStream(stream)
-	p.Stream = stream
+	p.stream = stream
 }
 
-type FFT1DR2CPlan struct {
-	fftplan
-	size1D
-	batch int
-}
-
-func FFT1DR2C(size, batch int) FFT1DR2CPlan {
-	handle := cufft.Plan1d(size, cufft.R2C, batch)
-	return FFT1DR2CPlan{fftplan{handle, 0}, size1D(size), batch}
-}
-
-func (p FFT1DR2CPlan) Exec(src Float32s, dst Complex64s) {
-	oksrclen := p.batch * p.Size()
-	if src.Len() != oksrclen {
-		panic(fmt.Errorf("size mismatch: expecting src len %v, got %v", oksrclen, src.Len()))
-	}
-	okdstlen := p.batch * (p.Size()/2 + 1)
-	if dst.Len() != okdstlen {
-		panic(fmt.Errorf("size mismatch: expecting dst len %v, got %v", okdstlen, dst.Len()))
-	}
-	p.handle.ExecR2C(src.Pointer(), dst.Pointer())
+// Returns the CUDA stream associated with the FFT plan.
+func (p fftplan) Stream() cu.Stream {
+	return p.stream
 }

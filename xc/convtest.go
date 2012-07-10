94288f3f7632b9277e5a9ec247c25353d0f40991
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-07-10 14:04:25 +0200

    better conv test

--- xc/convtest.go
+++ xc/convtest.go
@@ -1,9 +1,10 @@
 package xc
 
 import (
-	"math"
-	//"math/rand"
+	"fmt"
 	"github.com/barnex/cuda4/safe"
+	"math"
+	"math/rand"
 	"nimble-cube/core"
 )
 
@@ -14,7 +15,7 @@ func (c *Conv) Test() {
 
 	for i := 0; i < 3; i++ {
 		for j := range c.input[i] {
-			c.input[i][j] = 1 //2*rand.Float32() - 1
+			c.input[i][j] = 2*rand.Float32() - 1
 			input[i] = make([]float32, N)
 			copy(input[i], c.input[i])
 		}
@@ -23,26 +24,38 @@ func (c *Conv) Test() {
 		core.Debug("input:", i, core.Format(safe.Reshape3DFloat32(input[i], c.size[0], c.size[1], c.size[2])))
 	}
 
-	//c.Push(N / 2)
 	c.Push(N)
-	//c.Pull(N / 2)
 	c.Pull(N)
-	//c.Push(N)
-	//c.Pull(N)
+	c.checkError()
 
+	c.Push(N / 2)
+	c.Push(N)
+	c.Pull(N / 4)
+	c.Pull(N - 1)
+	c.Pull(N)
+	c.checkError()
+
+}
+
+const FFT_TOLERANCE = 1e-6
+
+func (c *Conv) checkError() {
 	NFFT := prod(PadSize(c.size))
 	rms := 0.0
 	for i := 0; i < 3; i++ {
 		for j := range c.input[i] {
-			rms += sqr(float64(input[i][j]) - float64(c.output[i][j])/float64(NFFT))
+			rms += sqr(float64(c.input[i][j]) - float64(c.output[i][j])/float64(NFFT))
 		}
 	}
 	rms = math.Sqrt(rms / float64(3*NFFT))
 
-	for i := 0; i < 3; i++ {
-		core.Debug("output:", i, core.Format(safe.Reshape3DFloat32(c.output[i], c.size[0], c.size[1], c.size[2])))
+	core.Debug("RMS fft error:", rms)
+	if rms > FFT_TOLERANCE {
+		panic(fmt.Errorf("FFT RMS error: %v > tolerance (%v)", rms, FFT_TOLERANCE))
 	}
-	core.Log("RMS fft error:", rms)
+	if rms == 0 {
+		panic(fmt.Errorf("FFT RMS error: %v: too good to be true", rms))
+	}
 }
 
 func sqr(x float64) float64 {

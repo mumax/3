2365d4dd6111eea8e47c2b61c525ed67527b6a45
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-07-12 12:37:28 +0200

    simplify conv test

--- xc/convtest.go
+++ xc/convtest.go
@@ -11,14 +11,12 @@ import (
 // Run self-test and report estimated relative RMS error.
 func (c *Conv) Test() {
 	N := c.n
-	var input [3][]float32
 
+	// make random input data between -1 and 1
 	for i := 0; i < 3; i++ {
 		for j := range c.input[i] {
 			c.input[i][j] = 2*rand.Float32() - 1
 		}
-		copy(input[i], c.input[i])
-		input[i] = make([]float32, N)
 	}
 	//	for i := 0; i < 3; i++ {
 	//		core.Debug("input:", i, core.Format(safe.Reshape3DFloat32(input[i], c.size[0], c.size[1], c.size[2])))
@@ -43,7 +41,7 @@ func (c *Conv) Test() {
 	}
 }
 
-const FFT_TOLERANCE = 1e-5
+const FFT_TOLERANCE = 1e-5 // panic if RMS error of fw+bw transform on random data is larger than this.
 
 func (c *Conv) checkError() {
 	NFFT := prod(PadSize(c.size))

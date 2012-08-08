d27edd7466c36cd94d53c8f64480023856bd69bc
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-08-08 22:38:56 +0200

    add dump/writer

--- dump/writer.go
+++ dump/writer.go
@@ -1,9 +1,34 @@
 package dump
 
 import (
+	"hash"
+	"hash/crc64"
 	"io"
 )
 
+var table = crc64.MakeTable(crc64.ISO)
+
 type Writer struct {
-	writer io.Writer
+	Header
+	out io.Writer
+	crc hash.Hash
+}
+
+func NewWriter(out io.Writer) *Writer {
+	w := new(Writer)
+	w.out = out
+	w.crc = crc64.New(table)
+	return w
+}
+
+func (w *Writer) WriteHeader() error {
+	return nil
+}
+
+func (w *Writer) WriteData(data []float32) error {
+	return nil
+}
+
+func (w *Writer) WriteHash() error {
+	return nil
 }

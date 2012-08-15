92f685045be098b1f972cae3d78e057c2a191e3f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-08-15 09:02:34 +0200

    start exchange6

---
+++ mag/exchange6.go
@@ -0,0 +1,12 @@
+package mag
+
+func Exchange6(m [3][][][]float32, Hex [3][][][]float32){
+	for i := range m[0]{
+		for j:=range m[0][i]{
+			for k:=range m[0][i][j]{
+				m0 := Vector{m[X][i][j][k], m[Y][i][j][k], mZ[][][]}	
+			}
+		}
+	}
+}
+

379f3dc810bb1ddfc3ee44a709fc9ab5b5ad909e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-08-22 15:07:29 +0200

    cleanup

--- gpu/conv/test.go
+++ gpu/conv/test.go
@@ -2,12 +2,13 @@ package conv
 
 // Interface of any convolution.
 type Conv interface {
-	Input() [3][][][]float32
-	Output() [3][][][]float32
-	Kernel() [3][3][][][]float32
-	Exec()
+	Input() [3][][][]float32     // Input data
+	Output() [3][][][]float32    // Output data
+	Kernel() [3][3][][][]float32 // Convolution kernel
+	Exec()                       // Executes the convolution
 }
 
+// Test if the convolution gives the same result as the brute-force implementation.
 func Test(c Conv) {
 
 }

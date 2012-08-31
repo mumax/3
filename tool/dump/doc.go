60ce1e7db075622a8fe6cf94c70171cbb01df681
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-08-31 12:15:53 +0200

    spin-off tools/ to nimble-tools.git

--- tool/dump/doc.go
+++
@@ -1,9 +0,0 @@
-/*
- Tool for manipulating data in the dump format.
- TODO: use printf verb for all output data
- TODO: add dump output format :-)
- TODO: on error: print rouge file and continue with the others.
- TODO: add gzip support
- TODO: add -update flag that updates only out-of-date files.
-*/
-package main

708f4898e6c8342c03e9416735b0446e33ad72a1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-04 11:57:35 +0200

    cleanup

--- gpu/ptx/copypad.cu
+++ gpu/ptx/copypad.cu
@@ -1,4 +1,3 @@
-
 // Copies src (3D array, size S0 x S1 x S2) to larger dst (3D array, size D0 x D1 x D2).
 // src data is offset by o0,o1,o2.
 // The remainder of dst is NOT zero-padded.
@@ -6,33 +5,16 @@
 //	a    ->  a x
 //	         x x
 //
-// Launch config:
-// 	?
 extern "C" __global__ void 
 copypad(float* dst, int D0, int D1, int D2, 
         float* src, int S0, int S1, int S2, 
         int o0, int o1, int o2){
 
-	// swap j/k, swap S1/S2, D1/D2
 	int j = blockIdx.y * blockDim.y + threadIdx.y; // index in src slice
 	int k = blockIdx.x * blockDim.x + threadIdx.x;
 
-	if(j>=S1 || k>=S2){
-	//printf("(%dx%dx%d x %dx%dx%d):(%d %d %d:%d %d %d): return1\n",
-	//		gridDim.x, gridDim.y, gridDim.z, 
-	//		blockDim.x, blockDim.y, blockDim.z,
-	//		blockIdx.x, blockIdx.y, blockIdx.z,
-	//		threadIdx.x, threadIdx.y, threadIdx.z);
- 		return;	// out of src bounds
-	}
-	// TODO: fuse
-	if(j>=D1 || k>=D2){
-	//printf("(%dx%dx%d x %dx%dx%d):(%d %d %d:%d %d %d): return2\n",
-	//		gridDim.x, gridDim.y, gridDim.z, 
-	//		blockDim.x, blockDim.y, blockDim.z,
-	//		blockIdx.x, blockIdx.y, blockIdx.z,
-	//		threadIdx.x, threadIdx.y, threadIdx.z);
- 		return;	// out of dst bounds
+	if(j>=S1 || k>=S2 || j>=D1 || k>=D2){
+ 		return;	// out of  bounds
 	}
 
 	int J = j + o1;  // index in full src
@@ -40,17 +22,7 @@ copypad(float* dst, int D0, int D1, int D2,
 	
 	for (int i=0; i<S0; i++){
  		int I = i + o0; // index in full src
-
-//	printf("(%dx%dx%d x %dx%dx%d):(%d %d %d:%d %d %d): %d -> %d\n",
-//			gridDim.x, gridDim.y, gridDim.z, 
-//			blockDim.x, blockDim.y, blockDim.z,
-//			blockIdx.x, blockIdx.y, blockIdx.z,
-//			threadIdx.x, threadIdx.y, threadIdx.z,
-//			i*S1*S2 + j*S2 + k, I*D1*D2 + J*D2 + K);
-	
 		dst[I*D1*D2 + J*D2 + K] = src[i*S1*S2 + j*S2 + k];
 	}
-}
-
-
+} 
 
--- gpu/ptx/copypad.go
+++ gpu/ptx/copypad.go
@@ -11,7 +11,7 @@ const COPYPAD = `
 .target sm_30
 .address_size 64
 
-	.file	1 "/tmp/tmpxft_00001b15_00000000-9_copypad.cpp3.i"
+	.file	1 "/tmp/tmpxft_00005527_00000000-9_copypad.cpp3.i"
 	.file	2 "/home/arne/src/nimble-cube/gpu/ptx/copypad.cu"
 
 .visible .entry copypad(
@@ -28,83 +28,81 @@ const COPYPAD = `
 	.param .u32 copypad_param_10
 )
 {
-	.reg .pred 	%p<10>;
+	.reg .pred 	%p<11>;
 	.reg .s32 	%r<40>;
 	.reg .f32 	%f<2>;
 	.reg .s64 	%rd<9>;
 
 
 	ld.param.u64 	%rd3, [copypad_param_0];
-	ld.param.u32 	%r19, [copypad_param_2];
-	ld.param.u32 	%r20, [copypad_param_3];
+	ld.param.u32 	%r17, [copypad_param_2];
+	ld.param.u32 	%r18, [copypad_param_3];
 	ld.param.u64 	%rd4, [copypad_param_4];
-	ld.param.u32 	%r21, [copypad_param_5];
-	ld.param.u32 	%r22, [copypad_param_6];
-	ld.param.u32 	%r23, [copypad_param_7];
-	ld.param.u32 	%r24, [copypad_param_8];
-	ld.param.u32 	%r25, [copypad_param_9];
-	ld.param.u32 	%r26, [copypad_param_10];
+	ld.param.u32 	%r19, [copypad_param_5];
+	ld.param.u32 	%r20, [copypad_param_6];
+	ld.param.u32 	%r21, [copypad_param_7];
+	ld.param.u32 	%r22, [copypad_param_8];
+	ld.param.u32 	%r23, [copypad_param_9];
+	ld.param.u32 	%r24, [copypad_param_10];
 	cvta.to.global.u64 	%rd1, %rd3;
 	cvta.to.global.u64 	%rd2, %rd4;
-	.loc 2 17 1
+	.loc 2 13 1
 	mov.u32 	%r1, %ntid.y;
 	mov.u32 	%r2, %ctaid.y;
 	mov.u32 	%r3, %tid.y;
-	mad.lo.s32 	%r4, %r1, %r2, %r3;
-	.loc 2 18 1
-	mov.u32 	%r5, %ntid.x;
-	mov.u32 	%r6, %ctaid.x;
-	mov.u32 	%r7, %tid.x;
-	mad.lo.s32 	%r8, %r5, %r6, %r7;
-	.loc 2 20 1
-	setp.ge.s32 	%p1, %r8, %r23;
-	setp.ge.s32 	%p2, %r4, %r22;
-	or.pred  	%p3, %p1, %p2;
-	@%p3 bra 	BB0_4;
+	mad.lo.s32 	%r25, %r1, %r2, %r3;
+	.loc 2 14 1
+	mov.u32 	%r4, %ntid.x;
+	mov.u32 	%r5, %ctaid.x;
+	mov.u32 	%r6, %tid.x;
+	mad.lo.s32 	%r26, %r4, %r5, %r6;
+	.loc 2 16 1
+	setp.lt.s32 	%p1, %r25, %r20;
+	setp.lt.s32 	%p2, %r26, %r21;
+	and.pred  	%p3, %p2, %p1;
+	setp.lt.s32 	%p4, %r25, %r17;
+	and.pred  	%p5, %p3, %p4;
+	setp.lt.s32 	%p6, %r26, %r18;
+	and.pred  	%p7, %p5, %p6;
+	.loc 2 23 1
+	setp.gt.s32 	%p8, %r19, 0;
+	.loc 2 16 1
+	and.pred  	%p9, %p8, %p7;
+	@!%p9 bra 	BB0_3;
+	bra.uni 	BB0_1;
 
-	.loc 2 29 1
-	setp.lt.s32 	%p4, %r8, %r20;
-	setp.lt.s32 	%p5, %r4, %r19;
-	and.pred  	%p6, %p5, %p4;
-	.loc 2 41 1
-	setp.gt.s32 	%p7, %r21, 0;
-	.loc 2 29 1
-	and.pred  	%p8, %p6, %p7;
-	@!%p8 bra 	BB0_4;
-	bra.uni 	BB0_2;
-
-BB0_2:
-	.loc 2 41 1
-	add.s32 	%r28, %r7, %r26;
-	mad.lo.s32 	%r29, %r5, %r6, %r28;
-	add.s32 	%r30, %r3, %r25;
+BB0_1:
+	.loc 2 23 1
+	add.s32 	%r28, %r6, %r24;
+	mad.lo.s32 	%r29, %r4, %r5, %r28;
+	add.s32 	%r30, %r3, %r23;
 	mad.lo.s32 	%r31, %r1, %r2, %r30;
-	mad.lo.s32 	%r32, %r24, %r19, %r31;
-	mad.lo.s32 	%r38, %r20, %r32, %r29;
-	mul.lo.s32 	%r10, %r20, %r19;
-	mad.lo.s32 	%r37, %r23, %r4, %r8;
-	mul.lo.s32 	%r12, %r23, %r22;
+	mad.lo.s32 	%r32, %r22, %r17, %r31;
+	mad.lo.s32 	%r38, %r18, %r32, %r29;
+	mul.lo.s32 	%r8, %r18, %r17;
+	mad.lo.s32 	%r37, %r21, %r25, %r26;
+	mul.lo.s32 	%r10, %r21, %r20;
 	mov.u32 	%r39, 0;
 
-BB0_3:
-	.loc 2 51 1
+BB0_2:
+	.loc 2 25 1
 	mul.wide.s32 	%rd5, %r37, 4;
 	add.s64 	%rd6, %rd2, %rd5;
 	mul.wide.s32 	%rd7, %r38, 4;
 	add.s64 	%rd8, %rd1, %rd7;
 	ld.global.f32 	%f1, [%rd6];
 	st.global.f32 	[%rd8], %f1;
-	.loc 2 41 1
-	add.s32 	%r38, %r38, %r10;
-	add.s32 	%r37, %r37, %r12;
-	.loc 2 41 18
+	.loc 2 23 1
+	add.s32 	%r38, %r38, %r8;
+	add.s32 	%r37, %r37, %r10;
+	.loc 2 23 18
 	add.s32 	%r39, %r39, 1;
-	.loc 2 41 1
-	setp.lt.s32 	%p9, %r39, %r21;
-	@%p9 bra 	BB0_3;
+	.loc 2 23 1
+	setp.lt.s32 	%p10, %r39, %r19;
+	@%p10 bra 	BB0_2;
 
-BB0_4:
-	.loc 2 53 2
+BB0_3:
+	.loc 2 27 2
 	ret;
 }
 

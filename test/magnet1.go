48987d6e763b639006a060b24e8bc14c8c182e0f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-04 12:07:42 +0200

    start magnet1.go

--- test/magnet1.go
+++ test/magnet1.go
@@ -1,37 +1,42 @@
 package main
 
 import (
-//	"math"
-//	"nimble-cube/core"
-//	"nimble-cube/gpu/conv"
-//   "nimble-cube/mag"
+	"math"
+	"strconv"
+	"flag"
+	"nimble-cube/core"
+	"nimble-cube/dump"
+	"nimble-cube/gpu/conv"
+   	"nimble-cube/mag"
 )
 
 func main() {
-	//	N0 := 1
-	//	N1 := 8
-	//	N2 := 16
-	//	N := N0 * N1 * N2
-	//	size := [3]int{N0, N1, N2}
-	//
-	//	cellsize := [3]float64{3e-9, 3.125e-9, 3.125e-9}
-	//
-	//	m := core.MakeVectors(size)
-	//	Θ := 0.
-	//	mag.Uniform(m, mag.Vector{0, float32(math.Sin(Θ)), float32(math.Cos(Θ))})
-	//	core.Print("m\n", m)
-	//
-	//	Hex := core.MakeVectors(size)
-	//	mag.Exchange6(m, Hex, cellsize)
-	//	core.Print("Hex\n", Hex)
-	//
-	//	Hd := core.MakeVectors(size)
-	//	acc := 4
-	//	kernel := mag.BruteKernel(size, cellsize, [3]int{0, 0, 0}, acc)
-	//	core.Print("kernel\n", kernel)
-	//	demag := conv.NewSymmetric(m, Hd, kernel)
-	//	demag.Push(N)
-	//	demag.Pull(N)
-	//	core.Print("Hd\n", Hd)
-	//
+		flag.Parse()
+		N0, N1, N2 := intflag(0), intflag(1), intflag(2)
+		//N := N0 * N1 * N2
+		size := [3]int{N0, N1, N2}
+		cellsize := [3]float64{3e-9, 3.125e-9, 3.125e-9}
+	
+		// demag
+		acc := 4
+		kernel := mag.BruteKernel(size, cellsize, [3]int{0, 0, 0}, acc)
+		demag := conv.NewSymmetric(size, kernel)
+		m:=demag.Input()
+		Hd := demag.Output()
+
+		// inital mag
+		Θ := 0.
+		mag.Uniform(m, mag.Vector{0, float32(math.Sin(Θ)), float32(math.Cos(Θ))})
+		dump.Quick("m", m[:])
+
+		demag.Exec()
+		dump.Quick("hd", Hd[:])
+
+	
+}
+
+func intflag(idx int)int{
+		val, err := strconv.Atoi(flag.Arg(idx))
+	core.Fatal(err)		
+	return val
 }

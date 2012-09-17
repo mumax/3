20a9af00981541f13de9bc2d6a129c9e5374c180
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-17 16:17:11 +0200

    rwmutex_test

--- core/rwmutex_test.go
+++ core/rwmutex_test.go
@@ -1,35 +1,42 @@
 package core
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
 
 func TestRWMutex(t *testing.T) {
 	N := 10
+	a := make([]int, N)
+	frames := 3
 	m := NewRWMutex(N)
 
 	go func() {
-		m.RLock(0, 5)
-		Log("r", 0, 5)
-
-		m.RLock(5, 10)
-		Log("r", 5, 10)
-
-		m.RLock(0, 5)
-		Log("r", 0, 5)
-
-		m.RLock(5, 10)
-		Log("r", 5, 10)
+		count := 0
+		for i := 0; i < frames; i++ {
+			prev := 0
+			for j := 1; j <= N; j++ {
+				m.RLock(prev, j)
+				if count != a[prev] {
+					t.Error("got", a[prev], "expected", count)
+				}
+				count++
+				prev = j
+				time.Sleep(time.Duration(rand.Int63n(1000)))
+			}
+		}
 	}()
 
-	m.Lock(0, 5)
-	Log("w", 0, 5)
-
-	m.Lock(5, 10)
-	Log("w", 5, 10)
-
-	m.Lock(0, 5)
-	Log("w", 0, 5)
-
-	m.Lock(5, 10)
-	Log("w", 5, 10)
-
+	count := 0
+	for i := 0; i < frames; i++ {
+		prev := 0
+		for j := 1; j <= N; j++ {
+			m.Lock(prev, j)
+			a[prev] = count
+			count++
+			prev = j
+			time.Sleep(time.Duration(rand.Int63n(1000)))
+		}
+	}
 }

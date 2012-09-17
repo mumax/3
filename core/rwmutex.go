757a2254f2404c5355b196abd5c68c3767db9e04
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-17 11:42:08 +0200

    rwmutex: canRLock

--- core/rwmutex.go
+++ core/rwmutex.go
@@ -20,6 +20,7 @@ func NewRWMutex(N int) *RWMutex {
 	return m
 }
 
+// Lock for writing [start, stop[.
 func (m *RWMutex) Lock(start, stop int) {
 	if start > stop || start >= m.N || stop > m.N || start < 0 || stop < 0 {
 		Panicf("rwmutex: lock: invalid arguments: start=%v, stop=%v, n=%v", start, stop, m.N)
@@ -27,10 +28,27 @@ func (m *RWMutex) Lock(start, stop int) {
 
 	m.cond.L.Lock()
 	for !m.canWLock(start, stop) {
+		Debug("lock: wait")
 		m.cond.Wait()
 	}
 	m.a, m.b = start, stop
 	m.cond.L.Unlock()
+	m.cond.Broadcast()
+}
+
+// Lock for reading [start, stop[.
+func (m *RWMutex) RLock(start, stop int) {
+	if start > stop || start >= m.N || stop > m.N || start < 0 || stop < 0 {
+		Panicf("rwmutex: rlock: invalid arguments: start=%v, stop=%v, n=%v", start, stop, m.N)
+	}
+	m.cond.L.Lock()
+	for !m.canRLock(start, stop) {
+		Debug("rlock: wait")
+		m.cond.Wait()
+	}
+	m.c, m.d = start, stop
+	m.cond.L.Unlock()
+	m.cond.Broadcast()
 }
 
 // Can m safely lock for writing [start, stop[ ?
@@ -42,6 +60,18 @@ func (m *RWMutex) canWLock(a, b int) bool {
 	Logf("canwlock: a=%v, b=%v, c=%v, d=%v", a, b, c, d)
 	ok := (ib <= ia) // intersection should be empty
 	Log("canwlock", a, b, ":", ok)
+	return ok
+}
+
+// Can m safely lock for reading [start, stop[ ?
+// Not thread-safe, assumes state mutex is locked.
+func (m *RWMutex) canRLock(c, d int) bool {
+	a, b := m.a, m.b
+	// intersection of read & write interval:
+	ia, ib := max(a, c), min(b, d)
+	Logf("canrlock: a=%v, b=%v, c=%v, d=%v", a, b, c, d)
+	ok := (ib <= ia) // intersection should be empty
+	Log("canrlock", c, d, ":", ok)
 	return ok
 }
 
--- core/rwmutex_test.go
+++ core/rwmutex_test.go
@@ -6,11 +6,17 @@ func TestRWMutex(t *testing.T) {
 	N := 100
 	m := NewRWMutex(N)
 
-	m.c = 5
-	m.d = 10
 
-	m.Lock(0, 50)
-	m.Lock(50, 100)
-	m.Lock(0, 0)
-	m.Lock(0, 50)
+	m.Lock(0, 1)
+
+	go func() {
+		m.RLock(0, 1)
+		m.RLock(1, 2)
+		m.RLock(2, 3)
+		m.RLock(4, 5)
+	}()
+
+	m.Lock(1, 2)
+	m.Lock(2, 3)
+	m.Lock(3, 4)
 }

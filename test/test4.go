6ec688573aa1305b7e902d31cbfc946641cbc7f9
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-20 10:54:45 +0200

    comment out dead code

--- test/test4.go
+++ test/test4.go
@@ -1,15 +1,15 @@
 package main
 
-import(
+import (
 	. "nimble-cube/core"
-	"nimble-cube/mag"
 	"nimble-cube/gpu/conv"
+	"nimble-cube/mag"
 )
 
-func main(){
+func main() {
 	N0, N1, N2 := 1, 32, 128
-	cx, cy, cz := 3e-9, 3.125e-9, 3.125e-9	
-	mesh := NewMesh(N0, N1, N2, cx, cy, cz) 
+	cx, cy, cz := 3e-9, 3.125e-9, 3.125e-9
+	mesh := NewMesh(N0, N1, N2, cx, cy, cz)
 
 	acc := 8
 	kernel := mag.BruteKernel(mesh.ZeroPadded(), acc)

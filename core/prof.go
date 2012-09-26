adbe9763c1132abb8a9f2fac9e219a9b81408ca8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-09-26 15:43:06 +0200

    cleanup

--- core/prof.go
+++ core/prof.go
@@ -16,6 +16,7 @@ var (
 	timeline  = make([]*stamp, 0, 1000)
 	profstate sync.Mutex
 	profstart time.Time
+	keys      []string
 )
 
 type stamp struct {
@@ -49,27 +50,38 @@ func profWriteDone(tag string) {
 
 func ProfDump(out_ io.Writer) {
 	out := tabwriter.NewWriter(out_, 8, 1, 1, ' ', 0)
-	var keys []string
+	keys = keys[:0]
 	for k, _ := range tags {
 		keys = append(keys, k)
 	}
 	sort.Strings(keys)
 
-	for i, s := range timeline {
-		del := "|"
-		if i%4 == 0 {
-			del = "- - -"
-		}
+	for _, s := range timeline {
+		//	// delimiter
+		//	del := "|"
+		//	if i%4 == 0 {
+		//		del = "- - -"
+		//	}
+
+		// enable/disable "running" status for this tag
 		tags[s.tag] = (s.delta >= 0)
-		fmt.Fprintf(out, "%15v ", s.Time.Sub(profstart))
-		for _, k := range keys {
-			if tags[k] == true {
-				fmt.Fprint(out, "\t", k)
-			} else {
-				fmt.Fprint(out, "\t"+del)
-			}
-		}
-		fmt.Fprintln(out)
-		out.Flush()
+
+		// print status of all tags
+		profPrintTags(out, s)
 	}
+}
+
+var del = "|"
+
+func profPrintTags(out *tabwriter.Writer, s *stamp) {
+	fmt.Fprintf(out, "%15v ", s.Time.Sub(profstart))
+	for _, k := range keys {
+		if tags[k] == true {
+			fmt.Fprint(out, "\t", k)
+		} else {
+			fmt.Fprint(out, "\t"+del)
+		}
+	}
+	fmt.Fprintln(out)
+	out.Flush()
 }

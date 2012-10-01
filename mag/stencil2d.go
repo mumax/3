8617d86fb053a733855701de320b80e6fd9eada9
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-01 15:43:42 +0200

    [broken] stencil2d streaming attempt #1

--- mag/stencil2d.go
+++ mag/stencil2d.go
@@ -14,35 +14,66 @@ type Stencil2D struct {
 
 // New 2D 5-point stencil. Weights have to be set afterwards.
 func NewStencil2D(in core.RChan3, out core.Chan3) *Stencil2D {
+	core.Assert(in.Size()[0] == 1)
+	core.Assert(in.Size() == out.Size())
 	return &Stencil2D{in: in, out: out}
 }
 
 func (s *Stencil2D) Run() {
-	core.Debug("running 5-point stencil")
+	core.Debug("running 4-neighbor 2D stencil")
 	size := s.in.Size()
-	N := core.Prod(size)
-	N1 := size[1]
-	//	bl := core.BlockLen(s.in.Size())
-	//	nB := div(N , bl)
-	//	bs := core.BlockSize(s.in.Size())
-	//	bs2 := bs // size of two subsequent m blocks
-	//	bs[1] *= 2 
+	bs := core.BlockSize(size)             // block size
+	bl := core.Prod(bs)                    // elements per block
+	bs2 := [3]int{bs[0], 2 * bs[1], bs[2]} // size of 2 blocks
+	//bs1 := [3]int{bs[0], bs[1] + 1, bs[2]} // size of block + 1 line
+	N := core.Prod(size) // elements per frame
+	N1 := size[1]        // lines per frame
+	//N2 := size[2]                          // lines per frame
+	B1 := bs[1]      // lines per block
+	nB := div(N, bl) // number of blocks
 
 	for {
-		in := s.in.ReadNext(N)
-		In := core.Reshape3(in, size) // todo: bs
-		out := s.out.WriteNext(N)
-		Out := core.Reshape3(out, size)
+		in := s.in.ReadNext(bl)
+		In := core.Reshape3(in, bs)
 
-		for c := range In {
-			// i=0 (2D)
-			for j := 0; j < len(In[c][0]); j++ {
-				s.span(In[c][0], Out[c][0], j, N1)
+		out := s.out.WriteNext(bl)
+		Out := core.Reshape3(out, bs)
+
+		// very first line of entire frame
+		s.span3(In, Out, 0, N1)
+
+		for b := 0; b < nB-1; b++ {
+			// block bulk
+			for j := 1; j < B1-1; j++ {
+				s.span3(In, Out, j, N1)
 			}
+			// block last line
+			in = s.in.ReadDelta(0, bl)
+			In := core.Reshape3(in, bs2)
+			s.span3(In, Out, B1-1, N1)
+
+			// next block first line
+			out = s.out.WriteDelta(0, bl)
+			Out = core.Reshape3(out, bs2)
+			s.span3(In, Out, B1, N1)
+			out = s.out.WriteDelta(bl, 0)
+			Out = core.Reshape3(out, bs)
+			in = s.in.ReadDelta(bl, 0)
+			In = core.Reshape3(in, bs)
 		}
 
+		// last block bulk+last line
+		for j := 1; j < B1; j++ {
+			s.span3(In, Out, j, N1)
+		}
 		s.out.WriteDone()
 		s.in.ReadDone()
+	}
+}
+
+func (s *Stencil2D) span3(input, output [3][][][]float32, idx, N1 int) {
+	for c := range input {
+		s.span(input[c][0], output[c][0], idx, N1) // [0]: 2D
 	}
 }
 

cd02e640374454943214d4b0b7c83993d1510006
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-02 09:21:13 +0200

    play/test4

--- play/test4.go
+++ play/test4.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	. "nimble-cube/core"
+	"nimble-cube/dump"
 	"nimble-cube/gpu/conv"
 	"nimble-cube/mag"
 	"os"
@@ -25,7 +26,7 @@ func main() {
 	Msat := 1.0053
 	aex := Mu0 * 13e-12 / Msat
 	hex := MakeChan3(size, "Hex")
-	Stack(mag.NewExchange2D(m.MakeRChan3(), hex, mesh, aex))
+	Stack(mag.NewExchange6(m.MakeRChan3(), hex, mesh, aex))
 
 	heff := MakeChan3(size, "Heff")
 	Stack(NewAdder3(heff, hd.MakeRChan3(), hex.MakeRChan3()))
@@ -37,10 +38,22 @@ func main() {
 	const dt = 100e-15
 	solver := mag.NewEuler(m, torque.MakeRChan3(), dt)
 	mag.SetAll(m.UnsafeArray(), mag.Uniform(0, 0.1, 1))
+	Stack(dump.NewAutosaver("test4m.dump", m.MakeRChan3(), 100))
+	Stack(dump.NewAutosaver("test4hex.dump", hex.MakeRChan3(), 100))
+	Stack(dump.NewAutotable("test4m.table", m.MakeRChan3(), 10))
 
 	RunStack()
 
-	solver.Steps(5000)
+	solver.Steps(10)
+	res := m.UnsafeArray()
+	got := [3]float32{res[0][0][0][0], res[1][0][0][0], res[2][0][0][0]}
+	//expect := [3]float32{-0.0758771, 0.17907965, 0.9809042}
+	Log("result:", got)
+	//	if got != expect {
+	//		Fatal(fmt.Errorf("expected: %v", expect))
+	//	}
+	// TODO: drain
+
 	ProfDump(os.Stdout)
 	Cleanup()
 }

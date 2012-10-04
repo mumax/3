410bc5dd1a685298df721a39f36e85ed27591460
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-04 14:13:27 +0200

    cleanup

--- gpu/conv/symmetric.go
+++ gpu/conv/symmetric.go
@@ -257,37 +257,6 @@ func (c *Symmetric) initFFTKern() {
 	}
 }
 
-// Extract real parts, copy them from src to dst.
-// In the meanwhile, check if imaginary parts are nearly zero
-// and scale the kernel to compensate for unnormalized FFTs.
-func scaleRealParts(dstList []float32, src safe.Float32s, scale float32) {
-	core.Assert(2*len(dstList) == src.Len())
-	srcList := src.Host()
-
-	// Normally, the FFT'ed kernel is purely real because of symmetry,
-	// so we only store the real parts...
-	maximg := float32(0.)
-	maxreal := float32(0.)
-	for i := 0; i < src.Len()/2; i++ {
-		dstList[i] = srcList[2*i] * scale
-		if fmath.Abs(srcList[2*i+0]) > maxreal {
-			maxreal = fmath.Abs(srcList[2*i+0])
-		}
-		if fmath.Abs(srcList[2*i+1]) > maximg {
-			maximg = fmath.Abs(srcList[2*i+1])
-		}
-	}
-	// ...however, we check that the imaginary parts are nearly zero,
-	// just to be sure we did not make a mistake during kernel creation.
-	//core.Debug("FFT Kernel max imaginary part=", maximg)
-	//core.Debug("FFT Kernel max real part=", maxreal)
-	core.Debug("FFT Kernel max imaginary/real part=", maximg/maxreal)
-	if maximg/maxreal > 1e-5 { // TODO: is this reasonable?
-		panic(fmt.Errorf("xc: FFT Kernel max imaginary/real part=%v", maximg/maxreal))
-	}
-
-}
-
 func (c *Symmetric) Exec() {
 	c.Push(c.n)
 	c.Pull(c.n)

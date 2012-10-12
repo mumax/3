6f56c773b56a378d67ea539f54c3d1df7518dcfd
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-12 11:07:50 +0200

    all gpu test file complete

--- play/gpu4.go
+++ play/gpu4.go
@@ -2,14 +2,17 @@ package main
 
 import (
 	. "nimble-cube/core"
-	//"nimble-cube/dump"
+	"nimble-cube/dump"
 	"nimble-cube/gpu/conv"
-	//"nimble-cube/mag"
+	"nimble-cube/mag"
 	"nimble-cube/gpu"
 	"os"
 )
 
 func main() {
+
+	// mesh
+
 	a := 1
 	N0, N1, N2 := 1, a*32, a*128
 	cx, cy, cz := 3e-9, 3.125e-9, 3.125e-9
@@ -18,39 +21,44 @@ func main() {
 	Log("mesh:", mesh)
 	Log("block:", BlockSize(mesh.GridSize()))
 
+	// quantities
+
 	m := MakeChan3(size, "m")
 	mGPU := gpu.MakeChan3(size, "mGPU")
 	Stack(conv.NewUploader(m.MakeRChan3() , mGPU))
 
+	b := gpu.MakeChan3(size, "B")
 
-//	hd := MakeChan3(size, "Hd")
-//
-//	acc := 1
-//	kernel := mag.BruteKernel(mesh.ZeroPadded(), acc)
-//	Stack(conv.NewSymmetricHtoD(size, kernel, m.MakeRChan3(), hd))
-//
-//	Msat := 1.0053
-//	aex := Mu0 * 13e-12 / Msat
-//	hex := MakeChan3(size, "Hex")
-//	Stack(gpu.NewExchange2D(m.MakeRChan3(), hex, mesh, aex))
-//
-//	heff := MakeChan3(size, "Heff")
-//	Stack(NewAdder3(heff, hd.MakeRChan3(), hex.MakeRChan3()))
-//
-//	var alpha float32 = 1
-//	torque := MakeChan3(size, "τ")
-//	Stack(mag.NewLLGTorque(torque, m.MakeRChan3(), heff.MakeRChan3(), alpha))
-//
-//	var dt float32 = 100e-15
-//	solver := mag.NewEuler(m, torque.MakeRChan3(), dt)
-//	mag.SetAll(m.UnsafeArray(), mag.Uniform(0, 0.1, 1))
-//	Stack(dump.NewAutosaver("test4m.dump", m.MakeRChan3(), 100))
-//	Stack(dump.NewAutosaver("test4hex.dump", hex.MakeRChan3(), 100))
-//	Stack(dump.NewAutotable("test4m.table", m.MakeRChan3(), 100))
+	acc := 1
+	kernel := mag.BruteKernel(mesh.ZeroPadded(), acc)
+	Stack(conv.NewSymm2D(size, kernel, mGPU.MakeRChan3(), b))
+
+	Msat := 1.0053
+	aex := Mu0 * 13e-12 / Msat
+	bex := gpu.MakeChan3(size, "Bex")
+	Stack(gpu.NewExchange6(mGPU.MakeRChan3(), bex, mesh, aex))
+
+	beGPU := gpu.MakeChan3(size, "BeGPU")
+	Stack(gpu.NewAdder3(beGPU, bex.MakeRChan3(), b.MakeRChan3()))
+
+	be := MakeChan3(size, "Be")
+	Stack(conv.NewDownloader(beGPU.MakeRChan3(), be))
+	
+
+	var alpha float32 = 1
+	torque := MakeChan3(size, "τ")
+	Stack(mag.NewLLGTorque(torque, m.MakeRChan3(), be.MakeRChan3(), alpha))
+
+	var dt float32 = 100e-15
+	solver := mag.NewEuler(m, torque.MakeRChan3(), dt)
+	mag.SetAll(m.UnsafeArray(), mag.Uniform(0, 0.1, 1))
+	Stack(dump.NewAutosaver("test4m.dump", m.MakeRChan3(), 100))
+	//Stack(dump.NewAutosaver("test4bex.dump", bex.MakeRChan3(), 100))
+	Stack(dump.NewAutotable("test4m.table", m.MakeRChan3(), 100))
 
 	RunStack()
 
-	//solver.Steps(100)
+	solver.Steps(100)
 
 	ProfDump(os.Stdout)
 	Cleanup()

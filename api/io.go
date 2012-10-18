1f69cbabc07779400e4c7f5e7dfa6988c1c7ba61
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-18 11:44:46 +0200

    api: newmesh

--- api/io.go
+++ api/io.go
@@ -13,3 +13,15 @@ func SetOD(od string) {
 func GetOD() string {
 	return core.OD
 }
+
+// Logs a message.
+func Log(msg ...interface{}) {
+	core.Log(msg...)
+}
+
+// Retruns a new mesh with N0 x N1 x N2 cells of size cellx x celly x cellz.
+// Optional periodic boundary conditions (pbc): number of repetitions
+// in X, Y, Z direction. PBC 0, 0, 0 means no periodicity.
+func NewMesh(N0, N1, N2 int, cellx, celly, cellz float64, pbc ...int) *core.Mesh {
+	return core.NewMesh(N0, N1, N2, cellx, celly, cellz, pbc...)
+}

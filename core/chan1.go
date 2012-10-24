54e0b23638560407293ca5a1f23d3bd046f0f146
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-24 11:54:12 +0200

    add chandata.go

--- core/chan1.go
+++ core/chan1.go
@@ -1,35 +1,15 @@
 package core
 
+
 type Chan1 struct {
 	chandata // array+list
 	mutex    *RWMutex
-}
-
-type chandata struct {
-	*Info
-	array [][][]float32
-	list  []float32
 }
 
 func MakeChan(tag, unit string, m *Mesh, blocks ...int) Chan1 {
 	tag = UniqueTag(tag)
 	data := makedata(tag, unit, m, blocks...)
 	return Chan1{data, NewRWMutex(data.BlockLen(), tag)}
-}
-
-// Implements Chans
-func (c *Chan1) Chan() []Chan1 {
-	return []Chan1{*c}
-}
-
-func makedata(tag, unit string, m *Mesh, blocks ...int) chandata {
-	size := m.Size()
-	var c chandata
-	c.Info = NewInfo(tag, unit, m, blocks...)
-
-	c.array = MakeFloats(size)
-	c.list = Contiguous(c.array)
-	return c
 }
 
 // WriteNext locks and returns a slice of length n for 
@@ -52,18 +32,4 @@ func (c *Chan1) WriteDelta(Δstart, Δstop int) []float32 {
 	c.mutex.WriteDelta(Δstart, Δstop)
 	a, b := c.mutex.WRange()
 	return c.list[a:b]
-}
-
-// UnsafeData returns the underlying storage without locking.
-// Intended only for page-locking, not for reading or writing.
-func (d *chandata) UnsafeData() []float32 {
-	return d.list
-}
-
-func (d *chandata) UnsafeArray() [][][]float32 {
-	return d.array
-}
-
-func (d *chandata) Size() [3]int {
-	return SizeOf(d.array)
 }
---
+++ core/chandata.go
@@ -0,0 +1,33 @@
+package core
+
+import "fmt"
+
+type chandata struct {
+	*Info
+	array [][][]float32
+	list  []float32
+}
+
+func makedata(tag, unit string, m *Mesh, blocks ...int) chandata {
+	size := m.Size()
+	var c chandata
+	c.Info = NewInfo(tag, unit, m, blocks...)
+	c.array = MakeFloats(size)
+	c.list = Contiguous(c.array)
+	return c
+}
+
+// UnsafeData returns the underlying storage without locking.
+// Intended only for page-locking, not for reading or writing.
+func (d *chandata) UnsafeData() []float32      { return d.list }
+func (d *chandata) UnsafeArray() [][][]float32 { return d.array }
+func (d *chandata) Size() [3]int               { return SizeOf(d.array) }
+func (d *chandata) NComp() int                 { return len(d.list) }
+
+func (q Chan1) String() string {
+	unit := q.unit
+	if unit != "" {
+		unit = " [" + unit + "]"
+	}
+	return fmt.Sprint(q.tag, unit, ": ", q.NComp(), "x", q.Size(), ", ", q.nBlocks, " blocks")
+}

09a88bb392de38c25f330c6a77ba935530f53690
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-30 15:31:07 +0100

    cleanup chan3

--- core/chan3.go
+++ core/chan3.go
@@ -4,58 +4,26 @@ package core
 type Chan3 ChanN
 
 func MakeChan3(tag, unit string, m *Mesh, blocks ...int) Chan3 {
-	c := make(Chan3, 3)
-	for i := range c {
-		c[i] = MakeChan(tag, unit, m, blocks...)
-	}
-	AddQuant(tag)
-	return c
+	return Chan3(MakeChanN(3, tag, unit, m, blocks...))
 }
 
-// WriteNext locks and returns a slice of length n for 
-// writing the next n elements to the Chan3.
-// When done, WriteDone() should be called to "send" the
-// slice down the Chan3. After that, the slice is not valid any more.
 func (c Chan3) WriteNext(n int) [3][]float32 {
 	next := ChanN(c).WriteNext(n)
 	return [3][]float32{next[0], next[1], next[2]}
-	//var next [3][]float32
-	//for i := range c {
-	//	c[i].WriteNext(n)
-	//	a, b := c[i].mutex.WRange()
-	//	next[i] = c[i].slice.Slice(a, b).list
-	//}
-	//return next
-}
-
-// WriteDone() signals a slice obtained by WriteNext() is fully
-// written and can be sent down the Chan3.
-func (c Chan3) WriteDone() {
-	ChanN(c).WriteDone()
-	//for i := range c {
-	//	c[i].WriteDone()
-	//}
 }
 
 func (c Chan3) WriteDelta(Δstart, Δstop int) [3][]float32 {
 	next := ChanN(c).WriteDelta(Δstart, Δstop)
 	return [3][]float32{next[0], next[1], next[2]}
-	//var next [3][]float32
-	//for i := range c {
-	//	c[i].WriteDelta(Δstart, Δstop)
-	//	a, b := c[i].mutex.WRange()
-	//	next[i] = c[i].slice.Slice(a, b).list
-	//}
-	//return next
 }
 
 func (c Chan3) Mesh() *Mesh  { return ChanN(c).Mesh() }
+func (c Chan3) WriteDone()   { ChanN(c).WriteDone() }
 func (c Chan3) Size() [3]int { return ChanN(c).Size() }
 func (c Chan3) Unit() string { return ChanN(c).Unit() }
 func (c Chan3) Tag() string  { return ChanN(c).Tag() }
+func (c Chan3) ChanN() ChanN { return ChanN(c) }
 
-// UnsafeData returns the underlying storage without locking.
-// Intended only for page-locking, not for reading or writing.
 func (c Chan3) UnsafeData() [3][]float32 {
 	return [3][]float32{c[0].slice.list, c[1].slice.list, c[2].slice.list}
 }
@@ -63,6 +31,3 @@ func (c Chan3) UnsafeData() [3][]float32 {
 func (c Chan3) UnsafeArray() [3][][][]float32 {
 	return [3][][][]float32{c[0].slice.array, c[1].slice.array, c[2].slice.array}
 }
-func (c Chan3) Comp(idx int) Chan1 { return c[idx] }
-
-func (c Chan3) ChanN() ChanN { return ChanN(c) }

6e75a0b0f3491639473b2d68c6d0489a14d7a20b
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-30 15:40:30 +0100

    simplify rchan3

--- core/rchan3.go
+++ core/rchan3.go
@@ -1,7 +1,7 @@
 package core
 
 // Read-only Chan3.
-type RChan3 [3]RChan1
+type RChan3 RChanN
 
 func (c Chan3) NewReader() RChan3 {
 	return RChan3{c[0].NewReader(), c[1].NewReader(), c[2].NewReader()}
@@ -11,7 +11,7 @@ func (c Chan3) NewReader() RChan3 {
 // reading the next n elements from the Chan3.
 // When done, ReadDone() should be called .
 // After that, the slice is not valid any more.
-func (c *RChan3) ReadNext(n int) [3][]float32 {
+func (c RChan3) ReadNext(n int) [3][]float32 {
 	var next [3][]float32
 	for i := range c {
 		c[i].mutex.ReadNext(n)
@@ -23,13 +23,13 @@ func (c *RChan3) ReadNext(n int) [3][]float32 {
 
 // ReadDone() signals a slice obtained by WriteNext() is fully
 // written and can be sent down the Chan3.
-func (c *RChan3) ReadDone() {
+func (c RChan3) ReadDone() {
 	for i := range c {
 		c[i].ReadDone()
 	}
 }
 
-func (c *RChan3) ReadDelta(Δstart, Δstop int) [3][]float32 {
+func (c RChan3) ReadDelta(Δstart, Δstop int) [3][]float32 {
 	var next [3][]float32
 	for i := range c {
 		c[i].mutex.ReadDelta(Δstart, Δstop)
@@ -39,13 +39,13 @@ func (c *RChan3) ReadDelta(Δstart, Δstop int) [3][]float32 {
 	return next
 }
 
-func (c *RChan3) Mesh() *Mesh  { return c[0].Mesh }
-func (c *RChan3) Size() [3]int { return c[0].Size() }
-func (c *RChan3) Unit() string { return c[0].Unit() }
-func (c *RChan3) Tag() string  { return c[0].Tag() }
+func (c RChan3) Mesh() *Mesh  { return c[0].Mesh }
+func (c RChan3) Size() [3]int { return c[0].Size() }
+func (c RChan3) Unit() string { return c[0].Unit() }
+func (c RChan3) Tag() string  { return c[0].Tag() }
 
 // UnsafeData returns the underlying storage without locking.
 // Intended only for page-locking, not for reading or writing.
-func (c *RChan3) UnsafeData() [3][]float32 {
+func (c RChan3) UnsafeData() [3][]float32 {
 	return [3][]float32{c[0].slice.list, c[1].slice.list, c[2].slice.list}
 }

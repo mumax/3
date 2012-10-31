63fb30038ae1175cc597eb6cc0c48ec7be4aeebb
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-10-31 11:22:55 +0100

    minor cleanup

--- gpu/sum.go
+++ gpu/sum.go
@@ -2,12 +2,8 @@ package gpu
 
 import (
 	"github.com/barnex/cuda5/cu"
-	//	"github.com/barnex/cuda5/safe"
 	"fmt"
 	"nimble-cube/core"
-
-//	"nimble-cube/gpu/ptx"
-//	"unsafe"
 )
 
 type Sum struct {
@@ -77,34 +73,3 @@ func (s *Sum) Exec() {
 }
 
 func (s *Sum) Output() ChanN { return s.sum }
-
-//var maddCode cu.Function
-//
-//func madd(dst, src1 safe.Float32s, factor1 float32, src2 safe.Float32s, factor2 float32, stream cu.Stream) {
-//
-//	core.Assert(dst.Len() == src1.Len() && dst.Len() == src2.Len())
-//
-//	if maddCode == 0 {
-//		mod := cu.ModuleLoadData(ptx.MADD)
-//		maddCode = mod.GetFunction("madd")
-//	}
-//
-//	N := dst.Len()
-//	gridDim, blockDim := Make1DConf(N)
-//
-//	dstptr := dst.Pointer()
-//	src1ptr := src1.Pointer()
-//	src2ptr := src2.Pointer()
-//
-//	args := []unsafe.Pointer{
-//		unsafe.Pointer(&dstptr),
-//		unsafe.Pointer(&src1ptr),
-//		unsafe.Pointer(&factor1),
-//		unsafe.Pointer(&src2ptr),
-//		unsafe.Pointer(&factor2),
-//		unsafe.Pointer(&N)}
-//
-//	shmem := 0
-//	cu.LaunchKernel(maddCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
-//	stream.Synchronize()
-//}

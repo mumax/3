3c639902cdd05280cea5673534b177b47d76ca08
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-06 20:22:35 +0100

    implement core/slice

--- core/slice.go
+++ core/slice.go
@@ -2,31 +2,56 @@ package core
 
 import (
 	"github.com/barnex/cuda5/safe"
+	"unsafe"
+	"reflect"
 )
 
 type Slice struct {
-	// ptr unsafe.Pointer
-	// size [3]uint16
-	// flag byte
-	list []float32
-	gpu  safe.Float32s
+	ptr unsafe.Pointer
+	len_ int
+	MemType 
 }
 
-const(
-	CPUACCESS = 1 << iota
-	GPUACCESS
+const (
+	CPUMemory MemType = 1 << iota
+	GPUMemory
 )
 
-//func MakeSlice(ptr, size, flag)
+type MemType byte
+
+func(m MemType) GPUAccess() bool{
+	return m & GPUMemory != 0
+}
+
+func(m MemType) CPUAccess() bool{
+	return m & CPUMemory != 0
+}
+
+func MakeSlice(ptr unsafe.Pointer, len_ int, flag MemType) Slice{
+	return Slice{ptr, len_, flag}
+}
 
 func (s *Slice) Slice(a, b int) Slice {
-	return Slice{s.list[a:b], s.gpu.Slice(a, b)}
+
 }
 
 func (s Slice) Host() []float32 {
-	return s.list
+	if s.MemType & CPUMemory == 0{
+		Panicf("slice not accessible by CPU (memory type %v)", s.MemType)
+	}
+	var list []float32
+	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&list))
+	hdr.Data = uintptr(s.ptr)
+	hdr.Len = s.len_
+	hdr.Cap = s.len_
+	return list
 }
 
 func (s Slice) Gpu() safe.Float32s {
-	return s.gpu
+	if s.MemType & GPUMemory == 0{
+		Panicf("slice not accessible by GPU (memory type %v)", s.MemType)
+	}
+	var floats safe.Float32s
+	floats.UnsafeSet(s.ptr, s.len_, s.len_)
+	return floats
 }

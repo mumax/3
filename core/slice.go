e0f916aa673b0f0c3662b26d011654ff90ffc1c6
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-06 20:34:39 +0100

    implement slice

--- core/slice.go
+++ core/slice.go
@@ -2,14 +2,14 @@ package core
 
 import (
 	"github.com/barnex/cuda5/safe"
-	"unsafe"
 	"reflect"
+	"unsafe"
 )
 
 type Slice struct {
-	ptr unsafe.Pointer
+	ptr  unsafe.Pointer
 	len_ int
-	MemType 
+	flag MemType
 }
 
 const (
@@ -19,24 +19,36 @@ const (
 
 type MemType byte
 
-func(m MemType) GPUAccess() bool{
-	return m & GPUMemory != 0
+func (s Slice) GPUAccess() bool {
+	return s.flag&GPUMemory != 0
 }
 
-func(m MemType) CPUAccess() bool{
-	return m & CPUMemory != 0
+func (s Slice) CPUAccess() bool {
+	return s.flag&CPUMemory != 0
 }
 
-func MakeSlice(ptr unsafe.Pointer, len_ int, flag MemType) Slice{
+func UnsafeSlice(ptr unsafe.Pointer, len_ int, flag MemType) Slice {
+	if len_ < 0 {
+		panic("negative slice length")
+	}
 	return Slice{ptr, len_, flag}
 }
 
-func (s *Slice) Slice(a, b int) Slice {
+func Float32ToSlice(list []float32) Slice {
+	return Slice{unsafe.Pointer(&list[0]), len(list), CPUMemory}
+}
 
+func (s *Slice) Slice(a, b int) Slice {
+	ptr := unsafe.Pointer(uintptr(s.ptr) + SizeofFloat32*a)
+	len_ := b - a
+	if a >= s.len_ || b > s.len_ || a > b {
+		Panicf("slice range out of bounds: [%v:%v] (len=%v)", a, b, s.len_)
+	}
+	return Slice{ptr, len_, s.MemType}
 }
 
 func (s Slice) Host() []float32 {
-	if s.MemType & CPUMemory == 0{
+	if s.MemType&CPUMemory == 0 {
 		Panicf("slice not accessible by CPU (memory type %v)", s.MemType)
 	}
 	var list []float32
@@ -47,8 +59,8 @@ func (s Slice) Host() []float32 {
 	return list
 }
 
-func (s Slice) Gpu() safe.Float32s {
-	if s.MemType & GPUMemory == 0{
+func (s Slice) Device() safe.Float32s {
+	if s.MemType&GPUMemory == 0 {
 		Panicf("slice not accessible by GPU (memory type %v)", s.MemType)
 	}
 	var floats safe.Float32s

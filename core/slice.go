acd57b4c399f3a51e4118ae5e0eb2be7755ac73c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-06 17:27:18 +0100

    core.Slice: add gpu

--- core/slice.go
+++ core/slice.go
@@ -1,14 +1,23 @@
 package core
 
+import (
+	"github.com/barnex/cuda5/safe"
+)
+
 type Slice struct {
 	list  []float32
 	array [][][]float32
+	gpu   safe.Float32s
 }
 
 func (s *Slice) Slice(a, b int) Slice {
-	return Slice{s.list[a:b], nil}
+	return Slice{s.list[a:b], nil, s.gpu.Slice(a, b)}
 }
 
 func (s Slice) Host() []float32 {
 	return s.list
+}
+
+func (s Slice) Gpu() safe.Float32s {
+	return s.gpu
 }

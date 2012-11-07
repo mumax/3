8063d00a5abf146c4bfc272b0a231c4ba65f94af
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-07 20:11:06 +0100

    work on test4

--- test/test4.go
+++ test/test4.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	"nimble-cube/dump"
-	"nimble-cube/gpu/conv"
-	"nimble-cube/mag"
-	. "nimble-cube/nimble"
+	//"code.google.com/p/nimble-cube/dump"
+	"code.google.com/p/nimble-cube/gpu/conv"
+	"code.google.com/p/nimble-cube/mag"
+	. "code.google.com/p/nimble-cube/nimble"
 	"os"
 )
 
@@ -16,36 +16,36 @@ func main() {
 	mesh := NewMesh(N0, N1, N2, cx, cy, cz)
 	Log("mesh:", mesh)
 
-	m := MakeChan3("m", "", mesh)
-	hd := MakeChan3("Hd", "", mesh)
+	m := MakeChan3("m", "", mesh, UnifiedMemory)
+	hd := MakeChan3("Hd", "", mesh, UnifiedMemory)
 
 	acc := 8
 	kernel := mag.BruteKernel(ZeroPad(mesh), acc)
-	Stack(conv.NewSymmetricHtoD(mesh, kernel, m.NewReader(), hd))
+	Stack(conv.NewSymm2D(mesh, kernel, m, hd))
 
-	Msat := 1.0053
-	aex := mag.Mu0 * 13e-12 / Msat
-	hex := MakeChan3("Hex", "", mesh)
-	Stack(mag.NewExchange6(m.NewReader(), hex, mesh, aex))
-
-	heff := MakeChan3("Heff", "", mesh)
-	Stack(NewAdder3(heff, hd.NewReader(), hex.NewReader()))
-
-	const alpha = 1
-	torque := MakeChan3("τ", "", mesh)
-	Stack(mag.NewLLGTorque(torque, m.NewReader(), heff.NewReader(), alpha))
-
-	const dt = 50e-15
-	solver := mag.NewEuler(m, torque.NewReader(), mag.Gamma0, dt)
-	mag.SetAll(m.UnsafeArray(), mag.Uniform(0, 0.1, 1))
-	Stack(dump.NewAutosaver("h.dump", hd.NewReader(), 1))
-	Stack(dump.NewAutosaver("m.dump", m.NewReader(), 1))
-	Stack(dump.NewAutosaver("hex.dump", hex.NewReader(), 1))
-	Stack(dump.NewAutotable("m.table", m.NewReader(), 1))
-
-	RunStack()
-
-	solver.Steps(100)
+//	Msat := 1.0053
+//	aex := mag.Mu0 * 13e-12 / Msat
+//	hex := MakeChan3("Hex", "", mesh)
+//	Stack(mag.NewExchange6(m.NewReader(), hex, mesh, aex))
+//
+//	heff := MakeChan3("Heff", "", mesh)
+//	Stack(NewAdder3(heff, hd.NewReader(), hex.NewReader()))
+//
+//	const alpha = 1
+//	torque := MakeChan3("τ", "", mesh)
+//	Stack(mag.NewLLGTorque(torque, m.NewReader(), heff.NewReader(), alpha))
+//
+//	const dt = 50e-15
+//	solver := mag.NewEuler(m, torque.NewReader(), mag.Gamma0, dt)
+//	mag.SetAll(m.UnsafeArray(), mag.Uniform(0, 0.1, 1))
+//	Stack(dump.NewAutosaver("h.dump", hd.NewReader(), 1))
+//	Stack(dump.NewAutosaver("m.dump", m.NewReader(), 1))
+//	Stack(dump.NewAutosaver("hex.dump", hex.NewReader(), 1))
+//	Stack(dump.NewAutotable("m.table", m.NewReader(), 1))
+//
+//	RunStack()
+//
+//	solver.Steps(100)
 	//	res := m.UnsafeArray()
 	//	got := [3]float32{res[0][0][0][0], res[1][0][0][0], res[2][0][0][0]}
 	//	expect := [3]float32{-0.075877085, 0.17907967, 0.9809043}

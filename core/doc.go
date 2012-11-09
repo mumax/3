c0b59cf70af221dad81fac4f42135ca5ebcf1f08
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-09 11:03:43 +0100

    add pkg docs

---
+++ core/doc.go
@@ -0,0 +1,7 @@
+/*
+ Package core contains the internal core API.
+ This package is normally not required for writing
+ simulation input scripts. 
+ See package nimble for that.
+*/
+package core
--- nimble/doc.go
+++ nimble/doc.go
@@ -1,4 +1,4 @@
 /*
-	Core functionality.
+ Package nimble provides the basic user API for building simulations.
 */
 package nimble

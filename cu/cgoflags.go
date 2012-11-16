4fa9026192a56e522f9030fa69960e98988dbfb4
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-16 10:26:43 +0100

    include  /opt/cuda for out-of-the box arch linux compilation

--- cu/cgoflags.go
+++ cu/cgoflags.go
@@ -4,6 +4,6 @@ package cu
 
 import "C"
 
-//#cgo LDFLAGS:-L/usr/lib/nvidia/ -L/usr/lib64/nvidia/ -L/usr/lib/nvidia-current/ -lcuda
-//#cgo CFLAGS:-I/usr/local/cuda/include/
+//#cgo LDFLAGS:-L/usr/lib/nvidia/ -L/usr/lib64/nvidia/ -L/usr/lib/nvidia-current/ -L/opt/cuda/lib -L/opt/cuda/lib64 -lcuda
+//#cgo CFLAGS:-I/usr/local/cuda/include/ -I/opt/cuda/include
 import "C"

f4bc27e3df8910c3558bb6787ed06633ef50f42e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-19 14:48:40 +0100

    implement cpu.Madd

--- cpu/device.go
+++ cpu/device.go
@@ -1,6 +1,6 @@
 package cpu
 
-import(
+import (
 	"code.google.com/p/nimble-cube/nimble"
 	"github.com/barnex/cuda5/cu"
 )
@@ -9,6 +9,16 @@ type Device int
 
 const CPUDevice Device = 0
 
-func(d Device)InitThread(){}
-func(d Device)Madd(dst nimble.Slice, src1, src2 nimble.Slice, w1, w2 float32, str cu.Stream){}
+func (d Device) InitThread() {
+	// intentionally empty
+}
 
+func (d Device) Madd(dst nimble.Slice, src1, src2 nimble.Slice, w1, w2 float32, str cu.Stream) {
+	Madd(dst.Host(), src1.Host(), src2.Host(), w1, w2)
+}
+
+func Madd(dst []float32, src1, src2 []float32, w1, w2 float32) {
+	for i := range dst {
+		dst[i] = w1*src1[i] + w2*src2[i]
+	}
+}
--- cpu/sum.go
+++ cpu/sum.go
@@ -5,7 +5,6 @@ import (
 	"code.google.com/p/nimble-cube/uni"
 )
 
-
 func NewSum(tag string, term1, term2 nimble.Chan, weight1, weight2 float32, mem nimble.MemType) *uni.Sum {
 	return uni.NewSum(tag, term1, term2, weight1, weight2, mem, CPUDevice)
 }

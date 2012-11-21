d9030c507322d96f21e4643c0c1d4b483c1926e7
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-21 13:13:56 +0100

    use ptxload in some funcs

--- gpu/conv/kernmulrsymm2d.go
+++ gpu/conv/kernmulrsymm2d.go
@@ -9,19 +9,9 @@ import (
 	"unsafe"
 )
 
-var (
-	kernMulRSymm2DxCode  cu.Function
-	kernMulRSymm2DyzCode cu.Function
-)
-
 func kernMulRSymm2Dyz(fftMy, fftMz safe.Complex64s, K11, K22, K12 safe.Float32s, N1, N2 int, stream cu.Stream) {
 
 	core.Assert(K11.Len() == (N1/2+1)*N2)
-
-	if kernMulRSymm2DyzCode == 0 {
-		mod := cu.ModuleLoadData(ptx.KERNMULRSYMM2DYZ)
-		kernMulRSymm2DyzCode = mod.GetFunction("kernmulRSymm2Dyz")
-	}
 
 	gridDim, blockDim := gpu.Make2DConf(N1, N2)
 
@@ -41,6 +31,7 @@ func kernMulRSymm2Dyz(fftMy, fftMz safe.Complex64s, K11, K22, K12 safe.Float32s,
 		unsafe.Pointer(&N2)}
 
 	shmem := 0
+	kernMulRSymm2DyzCode := gpu.PTXLoad(ptx.KERNMULRSYMM2DYZ, "kernmulRSymm2Dyz")
 	cu.LaunchKernel(kernMulRSymm2DyzCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
 }
 
@@ -48,10 +39,7 @@ func kernMulRSymm2Dx(fftMx safe.Complex64s, K00 safe.Float32s, N1, N2 int, strea
 
 	core.Assert(K00.Len() == (N1/2+1)*N2)
 
-	if kernMulRSymm2DxCode == 0 {
-		mod := cu.ModuleLoadData(ptx.KERNMULRSYMM2DX)
-		kernMulRSymm2DxCode = mod.GetFunction("kernmulRSymm2Dx")
-	}
+	kernMulRSymm2DxCode := gpu.PTXLoad(ptx.KERNMULRSYMM2DX, "kernmulRSymm2Dx")
 
 	gridDim, blockDim := gpu.Make2DConf(N1, N2)
 
--- gpu/ptxload.go
+++ gpu/ptxload.go
@@ -3,7 +3,7 @@ package gpu
 // This files implements loading and caching of CUDA PTX code.
 
 import (
-	"code.google.com/p/nimble-cube/gpu/ptx"
+	"code.google.com/p/nimble-cube/core"
 	"github.com/barnex/cuda5/cu"
 )
 
@@ -11,7 +11,8 @@ var ptxfuncs = make(map[string]cu.Function)
 
 func PTXLoad(code, function string) cu.Function {
 	if ptxfuncs[code] == 0 {
-		mod := cu.ModuleLoadData(ptx.EXCHANGE6)
+		core.Log("loading PTX code for", function)
+		mod := cu.ModuleLoadData(code)
 		ptxfuncs[code] = mod.GetFunction(function)
 	}
 	return ptxfuncs[code]

2887a8629f0758869c39c9d6578f4606354aa5b7
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-22 15:02:30 +0100

    gpu.sum

---
+++ gpu/device.go
@@ -0,0 +1,22 @@
+package gpu
+
+import (
+	"code.google.com/p/nimble-cube/nimble"
+	"github.com/barnex/cuda5/cu"
+)
+
+type Device int
+
+const GPUDevice Device = 0
+
+func (d Device) InitThread() {
+	LockCudaThread()
+}
+
+func (d Device) StreamCreate() cu.Stream {
+	return cu.StreamCreate()
+}
+
+func (d Device) Madd(dst nimble.Slice, src1, src2 nimble.Slice, w1, w2 float32, str cu.Stream) {
+	Madd(dst.Device(), src1.Device(), src2.Device(), w1, w2, str)
+}
--- gpu/sum.go
+++ gpu/sum.go
@@ -1,79 +1,40 @@
 package gpu
 
-//import (
-//	"fmt"
-//	"github.com/barnex/cuda5/cu"
-//	"nimble-cube/core"
-//)
-//
-//type Sum struct {
-//	sum     core.ChanN
-//	term    []core.RChanN
-//	weight  []float32
-//	stream  cu.Stream
-//	running bool
-//	readlen int // smallest of all blockLen's.
-//}
-//
-//func RunSum(tag string, term1_ core.Chan, weight1 float32, term2_ core.Chan, weight2 float32, nBlocks ...int) *Sum {
-//	term1, term2 := term1_.ChanN().NewReader(), term2_.ChanN().NewReader()
-//	output := MakeChanN(term1.NComp(), tag, term1.Unit(), term1.Mesh(), nBlocks...)
-//	sum := &Sum{sum: output, stream: cu.StreamCreate()}
-//	sum.MAdd(term1, weight1)
-//	sum.MAdd(term2, weight2)
-//	sum.readlen = output.BlockLen() // TODO: core.Min(output.BlockLen(), core.Min(term1.BlockLen(), term2.BlockLen()))
-//	core.Stack(sum)
-//	return sum
-//}
-//
-//func (s *Sum) MAdd(term core.RChanN, weight float32) {
-//	// Fail-fast check. May not always fire but should be OK to catch obvious bugs.
-//	if s.running {
-//		core.Fatal(fmt.Errorf("sum: madd: already running"))
-//	}
-//
-//	if len(s.term) != 0 {
-//		core.CheckEqualSize(term.Size(), s.term[0].Size())
-//		core.CheckUnits(term.Unit(), s.term[0].Unit())
-//		core.Assert(*term.Mesh() == *s.term[0].Mesh()) // TODO: nice error handling
-//	}
-//	s.term = append(s.term, term)
-//	s.weight = append(s.weight, weight)
-//	//TODO:
-//	//s.readlen = core.Min(s.readlen, term.BlockLen())
-//}
-//
-//func (s *Sum) Run() {
-//	s.running = true
-//	LockCudaThread()
-//	for {
-//		s.Exec()
-//	}
-//}
-//
-//func (s *Sum) Exec() {
-//	N := s.sum.BlockLen()
-//	nComp := s.sum.NComp()
-//
-//	// TODO: components could be streamed
-//	for c := 0; c < nComp; c++ {
-//
-//		A := s.term[0][c].ReadNext(N)
-//		B := s.term[1][c].ReadNext(N)
-//		S := s.sum[c].WriteNext(N)
-//
-//		madd(S, A, s.weight[0], B, s.weight[1], s.stream)
-//
-//		s.term[0][c].ReadDone()
-//		s.term[1][c].ReadDone()
-//
-//		for t := 2; t < len(s.term); t++ {
-//			C := s.term[t][c].ReadNext(N)
-//			madd(S, S, 1, C, s.weight[t], s.stream)
-//			s.term[t][c].ReadDone()
-//		}
-//		s.sum[c].WriteDone()
-//	}
-//}
-//
-//func (s *Sum) Output() core.ChanN { return s.sum }
+import (
+	"code.google.com/p/nimble-cube/core"
+	"code.google.com/p/nimble-cube/nimble"
+	"code.google.com/p/nimble-cube/uni"
+	"github.com/barnex/cuda5/cu"
+	"github.com/barnex/cuda5/safe"
+	"unsafe"
+)
+
+func NewSum(tag string, term1, term2 nimble.Chan, weight1, weight2 float32, mem nimble.MemType) *uni.Sum {
+	return uni.NewSum(tag, term1, term2, weight1, weight2, mem, GPUDevice)
+}
+
+func Madd(dst, src1, src2 safe.Float32s, factor1, factor2 float32, stream cu.Stream) {
+
+	core.Assert(dst.Len() == src1.Len() && dst.Len() == src2.Len())
+
+	maddCode := PTXLoad("madd")
+
+	N := dst.Len()
+	gridDim, blockDim := Make1DConf(N)
+
+	dstptr := dst.Pointer()
+	src1ptr := src1.Pointer()
+	src2ptr := src2.Pointer()
+
+	args := []unsafe.Pointer{
+		unsafe.Pointer(&dstptr),
+		unsafe.Pointer(&src1ptr),
+		unsafe.Pointer(&factor1),
+		unsafe.Pointer(&src2ptr),
+		unsafe.Pointer(&factor2),
+		unsafe.Pointer(&N)}
+
+	shmem := 0
+	cu.LaunchKernel(maddCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
+	stream.Synchronize()
+}

db8c96ff9bda6de6b13fbc31909727060c68c098
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-22 22:46:16 +0100

    eulersolver, rotatevec

--- gpu/eulersolver.go
+++ gpu/eulersolver.go
@@ -1,47 +1,49 @@
 package gpu
 
-//import (
-//	"github.com/barnex/cuda5/cu"
-//	"nimble-cube/core"
-//)
-//
-//// Euler solver.
-//type Euler struct {
-//	y      core.Chan3
-//	dy     core.RChan3
-//	dt     float32
-//	init   bool
-//	stream cu.Stream
-//}
-//
-//func NewEuler(y core.Chan3, dy core.RChan3, dt, multiplier float64) *Euler {
-//	return &Euler{y, dy, float32(dt * multiplier), false, cu.StreamCreate()}
-//}
-//
-//func (e *Euler) Steps(steps int) {
-//	core.Log("GPU euler solver:", steps, "steps")
-//	n := core.Prod(e.y.Size())
-//
-//	// Send out initial value
-//	if !e.init {
-//		e.y.WriteNext(n)
-//		e.init = true
-//	}
-//
-//	e.y.WriteDone()
-//
-//	for s := 0; s < steps-1; s++ {
-//		dy := e.dy.ReadNext(n)
-//		y := e.y.WriteNext(n)
-//		rotatevec(y, dy, e.dt, e.stream)
-//		e.y.WriteDone()
-//		e.dy.ReadDone()
-//	}
-//	// gentle hack:
-//	// do not split the last frame in blocks ans do not signal writeDone
-//	// then the pipeline comes in a consistent state before Steps() returns.
-//	dy := e.dy.ReadNext(n)
-//	y := e.y.WriteNext(n)
-//	rotatevec(y, dy, e.dt, e.stream)
-//	e.dy.ReadDone()
-//}
+import (
+	"code.google.com/p/nimble-cube/core"
+	"code.google.com/p/nimble-cube/nimble"
+	"github.com/barnex/cuda5/cu"
+)
+
+// Euler solver.
+type Euler struct {
+	y      nimble.ChanN
+	dy     nimble.RChanN
+	dt     float32
+	init   bool
+	stream cu.Stream
+}
+
+func NewEuler(y nimble.ChanN, dy_ nimble.ChanN, dt, multiplier float64) *Euler {
+	dy := dy_.NewReader()
+	return &Euler{y, dy, float32(dt * multiplier), false, cu.StreamCreate()}
+}
+
+func (e *Euler) Steps(steps int) {
+	core.Log("GPU euler solver:", steps, "steps")
+	n := e.y.Mesh().NCell()
+
+	// Send out initial value
+	if !e.init {
+		e.y.WriteNext(n)
+		e.init = true
+	}
+
+	e.y.WriteDone()
+
+	for s := 0; s < steps-1; s++ {
+		dy := Device3(e.dy.ReadNext(n))
+		y := Device3(e.y.WriteNext(n))
+		rotatevec(y, dy, e.dt, e.stream)
+		e.y.WriteDone()
+		e.dy.ReadDone()
+	}
+	// gentle hack:
+	// do not split the last frame in blocks ans do not signal writeDone
+	// then the pipeline comes in a consistent state before Steps() returns.
+	dy := Device3(e.dy.ReadNext(n))
+	y := Device3(e.y.WriteNext(n))
+	rotatevec(y, dy, e.dt, e.stream)
+	e.dy.ReadDone()
+}
--- gpu/rotatevec.go
+++ gpu/rotatevec.go
@@ -1,88 +1,83 @@
 package gpu
 
-//import (
-//	"github.com/barnex/cuda5/cu"
-//	"github.com/barnex/cuda5/safe"
-//	"nimble-cube/core"
-//	"nimble-cube/gpu/ptx"
-//	"unsafe"
-//)
-//
-//var rotatevecCode cu.Function
-//
-//// Rotate unit vectors v by factor * delta.
-//func rotatevec(vec, delta [3]core.Slice, factor float32, stream cu.Stream) {
-//	core.Assert(vec[0].Len() == delta[0].Len())
-//
-//	if rotatevecCode == 0 {
-//		mod := cu.ModuleLoadData(ptx.ROTATEVEC)
-//		rotatevecCode = mod.GetFunction("rotatevec")
-//	}
-//
-//	N := vec[0].Len()
-//	gridDim, blockDim := Make1DConf(N)
-//
-//	v0ptr := vec[0].Device().Pointer()
-//	v1ptr := vec[1].Device().Pointer()
-//	v2ptr := vec[2].Device().Pointer()
-//	d0ptr := delta[0].Device().Pointer()
-//	d1ptr := delta[1].Device().Pointer()
-//	d2ptr := delta[2].Device().Pointer()
-//
-//	args := []unsafe.Pointer{
-//		unsafe.Pointer(&v0ptr),
-//		unsafe.Pointer(&v1ptr),
-//		unsafe.Pointer(&v2ptr),
-//		unsafe.Pointer(&d0ptr),
-//		unsafe.Pointer(&d1ptr),
-//		unsafe.Pointer(&d2ptr),
-//		unsafe.Pointer(&factor),
-//		unsafe.Pointer(&N)}
-//
-//	shmem := 0
-//	cu.LaunchKernel(rotatevecCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
-//	stream.Synchronize()
-//}
-//
-//var rotatevec2Code cu.Function
-//
-//// Rotate unit vectors v by factor * delta.
-//func rotatevec2(vec, delta1 [3]safe.Float32s, factor1 float32, delta2 [3]safe.Float32s, factor2 float32, stream cu.Stream) {
-//	core.Assert(vec[0].Len() == delta1[0].Len() && vec[0].Len() == delta2[0].Len())
-//
-//	if rotatevec2Code == 0 {
-//		mod := cu.ModuleLoadData(ptx.ROTATEVEC2)
-//		rotatevec2Code = mod.GetFunction("rotatevec2")
-//	}
-//
-//	N := vec[0].Len()
-//	gridDim, blockDim := Make1DConf(N)
-//
-//	v0ptr := vec[0].Pointer()
-//	v1ptr := vec[1].Pointer()
-//	v2ptr := vec[2].Pointer()
-//	d0ptr := delta1[0].Pointer()
-//	d1ptr := delta1[1].Pointer()
-//	d2ptr := delta1[2].Pointer()
-//	e0ptr := delta2[0].Pointer()
-//	e1ptr := delta2[1].Pointer()
-//	e2ptr := delta2[2].Pointer()
-//
-//	args := []unsafe.Pointer{
-//		unsafe.Pointer(&v0ptr),
-//		unsafe.Pointer(&v1ptr),
-//		unsafe.Pointer(&v2ptr),
-//		unsafe.Pointer(&d0ptr),
-//		unsafe.Pointer(&d1ptr),
-//		unsafe.Pointer(&d2ptr),
-//		unsafe.Pointer(&factor1),
-//		unsafe.Pointer(&e0ptr),
-//		unsafe.Pointer(&e1ptr),
-//		unsafe.Pointer(&e2ptr),
-//		unsafe.Pointer(&factor2),
-//		unsafe.Pointer(&N)}
-//
-//	shmem := 0
-//	cu.LaunchKernel(rotatevec2Code, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
-//	stream.Synchronize()
-//}
+import (
+	"code.google.com/p/nimble-cube/core"
+	"code.google.com/p/nimble-cube/gpu/ptx"
+	"github.com/barnex/cuda5/cu"
+	"github.com/barnex/cuda5/safe"
+	"unsafe"
+)
+
+// Rotate unit vectors v by factor * delta.
+func rotatevec(vec, delta [3]safe.Float32s, factor float32, stream cu.Stream) {
+	core.Assert(vec[0].Len() == delta[0].Len())
+
+	N := vec[0].Len()
+	gridDim, blockDim := Make1DConf(N)
+
+	v0ptr := vec[0].Pointer()
+	v1ptr := vec[1].Pointer()
+	v2ptr := vec[2].Pointer()
+	d0ptr := delta[0].Pointer()
+	d1ptr := delta[1].Pointer()
+	d2ptr := delta[2].Pointer()
+
+	args := []unsafe.Pointer{
+		unsafe.Pointer(&v0ptr),
+		unsafe.Pointer(&v1ptr),
+		unsafe.Pointer(&v2ptr),
+		unsafe.Pointer(&d0ptr),
+		unsafe.Pointer(&d1ptr),
+		unsafe.Pointer(&d2ptr),
+		unsafe.Pointer(&factor),
+		unsafe.Pointer(&N)}
+
+	shmem := 0
+	rotatevecCode := PTXLoad("rotatevec")
+	cu.LaunchKernel(rotatevecCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
+	stream.Synchronize()
+}
+
+var rotatevec2Code cu.Function
+
+// Rotate unit vectors v by factor * delta.
+func rotatevec2(vec, delta1 [3]safe.Float32s, factor1 float32, delta2 [3]safe.Float32s, factor2 float32, stream cu.Stream) {
+	core.Assert(vec[0].Len() == delta1[0].Len() && vec[0].Len() == delta2[0].Len())
+
+	if rotatevec2Code == 0 {
+		mod := cu.ModuleLoadData(ptx.ROTATEVEC2)
+		rotatevec2Code = mod.GetFunction("rotatevec2")
+	}
+
+	N := vec[0].Len()
+	gridDim, blockDim := Make1DConf(N)
+
+	v0ptr := vec[0].Pointer()
+	v1ptr := vec[1].Pointer()
+	v2ptr := vec[2].Pointer()
+	d0ptr := delta1[0].Pointer()
+	d1ptr := delta1[1].Pointer()
+	d2ptr := delta1[2].Pointer()
+	e0ptr := delta2[0].Pointer()
+	e1ptr := delta2[1].Pointer()
+	e2ptr := delta2[2].Pointer()
+
+	args := []unsafe.Pointer{
+		unsafe.Pointer(&v0ptr),
+		unsafe.Pointer(&v1ptr),
+		unsafe.Pointer(&v2ptr),
+		unsafe.Pointer(&d0ptr),
+		unsafe.Pointer(&d1ptr),
+		unsafe.Pointer(&d2ptr),
+		unsafe.Pointer(&factor1),
+		unsafe.Pointer(&e0ptr),
+		unsafe.Pointer(&e1ptr),
+		unsafe.Pointer(&e2ptr),
+		unsafe.Pointer(&factor2),
+		unsafe.Pointer(&N)}
+
+	shmem := 0
+	rotatevec2Code := PTXLoad("rotatevec2")
+	cu.LaunchKernel(rotatevec2Code, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
+	stream.Synchronize()
+}

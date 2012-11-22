2f071e0172d6c5f8583b412317e151f6057f2b83
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-22 10:29:22 +0100

    add todo

--- gpu/ptx/doc.go
+++ gpu/ptx/doc.go
@@ -4,4 +4,6 @@
 package ptx
 
 // maps file names (equal to function names) on code
+// TODO: do not use map but generate functions so that
+// the linker can elide unused ptx code.
 var Code = make(map[string]string)

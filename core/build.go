62870fae827123a6077233fea0a65ab60c3e64c1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-27 20:24:09 +0100

    reduce_sum: 23GB/s on my laptop.

--- core/build.go
+++ core/build.go
@@ -1,6 +1,6 @@
 package core
 
 const (
-	BUILD_COMMIT = "1042"
-	BUILD_DATE   = "Tue Nov 27 20:00:24 CET 2012"
+	BUILD_COMMIT = "1043"
+	BUILD_DATE   = "Tue Nov 27 20:24:04 CET 2012"
 )
--- gpu/reduce_test.go
+++ gpu/reduce_test.go
@@ -1,6 +1,7 @@
 package gpu
 
 import (
+	"code.google.com/p/nimble-cube/core"
 	"code.google.com/p/nimble-cube/nimble"
 	"github.com/barnex/cuda5/cu"
 	"testing"
@@ -12,7 +13,7 @@ func TestReduceSum(t *testing.T) {
 	input := nimble.MakeSlice(N, nimble.UnifiedMemory)
 	in := input.Host()
 	for i := range in {
-		in[i] = float32(i)/100
+		in[i] = float32(i) / 100
 	}
 	str := cu.StreamCreate()
 	result := reduce_sum(input.Device(), str)
@@ -22,14 +23,15 @@ func TestReduceSum(t *testing.T) {
 }
 
 func BenchmarkReduceSum(b *testing.B) {
+	core.LOG = false
 	b.StopTimer()
 	LockCudaThread()
-	const N = 1024*1024
+	const N = 32 * 1024 * 1024
 	input := nimble.MakeSlice(N, nimble.GPUMemory)
 	str := cu.StreamCreate()
-	b.SetBytes(N*4)
+	b.SetBytes(N * 4)
 	b.StartTimer()
-	for i:=0; i<b.N; i++{
+	for i := 0; i < b.N; i++ {
 		reduce_sum(input.Device(), str)
 	}
 }

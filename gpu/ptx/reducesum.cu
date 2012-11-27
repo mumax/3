bfd154ac9b4a043e69641e788b92c086ea0984f6
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-11-27 19:23:18 +0100

    add reduce_test, fix blockdim

--- gpu/ptx/reducesum.cu
+++ gpu/ptx/reducesum.cu
@@ -13,38 +13,38 @@ inline __device__ float sum(float a, float b){
 extern "C" __global__ void
 reducesum(float *src, float *dst, int n) {
 
-	__shared__ float sdata[BLOCK];
-	int tid = threadIdx.x;
-	int i =  blockIdx.x * blockDim.x + threadIdx.x;
-
-	float mySum = 0;
-	int stride = gridDim.x * blockDim.x;
-	while (i < n) {
-    	mySum += src[i];
-    	i += stride;
-	}
-	sdata[tid] = mySum;
-	__syncthreads();
-
-	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
-		if (tid < s){
-			sdata[tid] = sum(sdata[tid], sdata[tid + s]);
-		}
-		__syncthreads();
-	}
-
-
-	// warp-synchronous
-	if (tid < 32) {
-      	volatile float* smem = sdata;
-		smem[tid] = sum(smem[tid], smem[tid + 32]); 
-		smem[tid] = sum(smem[tid], smem[tid + 16]); 
-		smem[tid] = sum(smem[tid], smem[tid +  8]); 
-		smem[tid] = sum(smem[tid], smem[tid +  4]); 
-		smem[tid] = sum(smem[tid], smem[tid +  2]); 
-		smem[tid] = sum(smem[tid], smem[tid +  1]); 
-	}
-
-	if (tid == 0) { atomicAdd(dst, sdata[0]); }
+//	__shared__ float sdata[BLOCK];
+//	int tid = threadIdx.x;
+//	int i =  blockIdx.x * blockDim.x + threadIdx.x;
+//
+//	float mySum = 0;
+//	int stride = gridDim.x * blockDim.x;
+//	while (i < n) {
+//    	mySum += src[i];
+//    	i += stride;
+//	}
+//	sdata[tid] = mySum;
+//	__syncthreads();
+//
+//	for (unsigned int s=blockDim.x/2; s>32; s>>=1) {
+//		if (tid < s){
+//			sdata[tid] = sum(sdata[tid], sdata[tid + s]);
+//		}
+//		__syncthreads();
+//	}
+//
+//
+//	// warp-synchronous
+//	if (tid < 32) {
+//      	volatile float* smem = sdata;
+//		smem[tid] = sum(smem[tid], smem[tid + 32]); 
+//		smem[tid] = sum(smem[tid], smem[tid + 16]); 
+//		smem[tid] = sum(smem[tid], smem[tid +  8]); 
+//		smem[tid] = sum(smem[tid], smem[tid +  4]); 
+//		smem[tid] = sum(smem[tid], smem[tid +  2]); 
+//		smem[tid] = sum(smem[tid], smem[tid +  1]); 
+//	}
+//
+//	if (tid == 0) { atomicAdd(dst, sdata[0]); }
 }
 
--- gpu/reduce.go
+++ gpu/reduce.go
@@ -12,12 +12,11 @@ func reduce_sum(in safe.Float32s, stream cu.Stream) float32 {
 
 	N := in.Len()
 
-	var gridDim, blockDim cu.Dim3
-	blockDim.X = 512
-	gridDim.X = 8 // TODO
+	blockDim := cu.Dim3{512, 1, 1}
+	gridDim := cu.Dim3{8, 1, 1}
 
-	inptr := unsafe.Pointer(uintptr(in.Pointer()))
-	outptr := unsafe.Pointer(uintptr(out.Pointer()))
+	inptr := in.Pointer()
+	outptr := out.Pointer()
 	args := []unsafe.Pointer{
 		unsafe.Pointer(&inptr),
 		unsafe.Pointer(&outptr),
@@ -29,7 +28,7 @@ func reduce_sum(in safe.Float32s, stream cu.Stream) float32 {
 	stream.Synchronize()
 
 	var result_ [1]float32
-	result := result_[:]
-	out.CopyDtoH(result)// async? register one arena block // , stream)
+	//result := result_[:]
+	//out.CopyDtoH(result) // async? register one arena block // , stream)
 	return result_[0]
 }

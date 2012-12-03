326766a262fcef0a7966995108e45a1fb5ab9d30
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-03 14:45:26 +0100

    3D seems OK

--- core/build.go
+++ core/build.go
@@ -1,6 +1,11 @@
 package core
 
 const (
+<<<<<<< HEAD
 	BUILD_COMMIT = "XXX"
 	BUILD_DATE   = "XXXX"
+=======
+	BUILD_COMMIT = "1107"
+	BUILD_DATE   = "Mon Dec  3 14:07:26 CET 2012"
+>>>>>>> 3D seems OK
 )

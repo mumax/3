4b5ac9f354b4d12c345d0f3be4eed91ee104ea27
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-06 14:37:03 +0100

    add resample.go

--- core/build.go
+++ core/build.go
@@ -2,5 +2,5 @@ package core
 
 const (
 	BUILD_COMMIT = "1143"
-	BUILD_DATE   = "Thu Dec  6 14:18:45 CET 2012"
+	BUILD_DATE   = "Thu Dec  6 14:18:57 CET 2012"
 )
---
+++ core/resample.go
@@ -0,0 +1,22 @@
+package core
+
+// ScaleNearest copies in to out using nearest-neighbor interpolation.
+// len(in) should be == len(out) (the number of vector components).
+// The other dimensions are supposedly different and will be interpolated over. 
+func ScaleNearest(in, out [][][][]float32) {
+	Assert(len(in) == len(out))
+	size1 := SizeOf(in[0])
+	size2 := SizeOf(out[0])
+	for c := range out {
+		for i := range out[c] {
+			i1 := (i * size1[0]) / size2[0]
+			for j := range out[0][i] {
+				j1 := (j * size1[1]) / size2[1]
+				for k := range out[0][i][j] {
+					k1 := (k * size1[2]) / size2[2]
+					out[c][i][j][k] = in[c][i1][j1][k1]
+				}
+			}
+		}
+	}
+}

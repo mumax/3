97882e3adf37590d0c54849fe5abe688f8dfbfb9
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-06 14:00:00 +0100

    arne: compensate_surface_charges: integrate over boundary

--- arne/compensate_surface_charges.go
+++ arne/compensate_surface_charges.go
@@ -12,22 +12,32 @@ import (
 // for the surface charges on the left and right edges.
 // Adding (this field * Bsat) to B_effective will mimic an infinitely long wire.
 func CompensateSurfaceCharges(m *nimble.Mesh) [3][][][]float32 {
+	core.Log("calculating field to compensate nanowire surface charge")
 	H := core.MakeVectors(m.Size())
 	world := m.WorldSize()
 	cell := m.CellSize()
-	source1 := [3]float64{world[0] / 2, world[1] / 2, 0}
-	source2 := [3]float64{world[0] / 2, world[1] / 2, world[2]}
-	q := world[0] * world[1]
-	for i := range H[0] {
-		for j := range H[0][i] {
-			for k := range H[0][i][j] {
-				dst := [3]float64{(float64(i) + 0.5) * cell[0],
-					(float64(j) + 0.5) * cell[1],
-					(float64(k) + 0.5) * cell[2]}
-				h1 := Hfield(q, source1, dst)
-				h2 := Hfield(q, source2, dst)
-				for c := 0; c < 3; c++ {
-					H[c][i][j][k] = float32(h1[c] + h2[c])
+	size := m.Size()
+	q := cell[0] * cell[1]
+
+	for I := 0; I < size[0]; I++ {
+		for J := 0; J < size[1]; J++ {
+
+			x := (float64(I) + 0.5) * cell[0]
+			y := (float64(J) + 0.5) * cell[1]
+			source1 := [3]float64{x, y, 0}
+			source2 := [3]float64{x, y, world[2]}
+			for i := range H[0] {
+				for j := range H[0][i] {
+					for k := range H[0][i][j] {
+						dst := [3]float64{(float64(i) + 0.5) * cell[0],
+							(float64(j) + 0.5) * cell[1],
+							(float64(k) + 0.5) * cell[2]}
+						h1 := Hfield(q, source1, dst)
+						h2 := Hfield(q, source2, dst)
+						for c := 0; c < 3; c++ {
+							H[c][i][j][k] += float32(h1[c] + h2[c])
+						}
+					}
 				}
 			}
 		}
--- core/build.go
+++ core/build.go
@@ -1,6 +1,6 @@
 package core
 
 const (
-	BUILD_COMMIT = "1141"
-	BUILD_DATE   = "Thu Dec  6 13:44:33 CET 2012"
+	BUILD_COMMIT = "1142"
+	BUILD_DATE   = "Thu Dec  6 13:59:56 CET 2012"
 )

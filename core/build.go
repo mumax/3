d6f08aae6d56657fa2037ef5ea66871842f1e738
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-06 10:41:29 +0100

    gofmt

--- core/build.go
+++ core/build.go
@@ -1,6 +1,6 @@
 package core
 
 const (
-	BUILD_COMMIT = "1134"
-	BUILD_DATE   = "Thu Dec  6 10:14:42 CET 2012"
+	BUILD_COMMIT = "1135"
+	BUILD_DATE   = "Thu Dec  6 10:18:30 CET 2012"
 )
--- nimble/doc.go
+++ nimble/doc.go
@@ -6,6 +6,7 @@
 
  TODO:  Chan.Buffer() buffers one frame, if not yet so.
  TODO:  Consistent API:
+	Only Chan interfaces should be used
  	NewXXX() does not Stack()
  	RunXXX() Stacks, returns output(s) ? (to avoid confusion with New)
  	ExecUnsafe()

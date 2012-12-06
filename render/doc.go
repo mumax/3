0d0f2a0bdcb4044e28871d4f44d49d77d4686cfb
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-06 20:32:09 +0100

    render doc

--- render/doc.go
+++ render/doc.go
@@ -1,7 +1,9 @@
+/*
+ 3D rendering of simulation output.
+ Dependencies (Ubuntu):
+ 	libx11-dev
+ 	libglfw-dev
+ On Ubuntu it might be needed to fix libXrandr like this:
+ 	ln -s /usr/lib/x86_64-linux-gnu/libXrandr.so.2 /usr/lib/x86_64-linux-gnu/libXrandr.so
+*/
 package render
-
-// Dependencies (Ubuntu):
-// 	libx11-dev
-// 	libglfw-dev
-// Also make sure libXrandr.so is accessible to the linker
-

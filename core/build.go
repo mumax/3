2e5e0a1205577344b5041428377759b63c2d4e8e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-06 15:58:18 +0100

    large 3D demag fails

--- core/build.go
+++ core/build.go
@@ -1,6 +1,6 @@
 package core
 
 const (
-	BUILD_COMMIT = "1145"
-	BUILD_DATE   = "Thu Dec  6 15:47:10 CET 2012"
+	BUILD_COMMIT = "1146"
+	BUILD_DATE   = "Thu Dec  6 15:50:30 CET 2012"
 )
---
+++ test/demag2.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"code.google.com/p/nimble-cube/core"
+	"code.google.com/p/nimble-cube/gpu"
+	"code.google.com/p/nimble-cube/mag"
+	"code.google.com/p/nimble-cube/nimble"
+	"fmt"
+	"os"
+)
+
+func host(s []nimble.Slice) [][]float32 {
+	h := make([][]float32, len(s))
+	for i := range h {
+		h[i] = s[i].Host()
+	}
+	return h
+}
+
+func main() {
+	nimble.Init()
+	defer nimble.Cleanup()
+	nimble.SetOD("demag1.out")
+
+	N0, N1, N2 := 4, 32, 1024
+	cx, cy, cz := 3e-9, 3.125e-9, 3.125e-9
+	mesh := nimble.NewMesh(N0, N1, N2, cx, cy, cz)
+	fmt.Println("mesh:", mesh)
+
+	mbox := gpu.NewConst("m", "", mesh, nimble.UnifiedMemory, []float64{1, 0, 0})
+	m := mbox.Output()
+
+	acc := 2
+	kernel := mag.BruteKernel(mesh, acc)
+	conv := gpu.NewConvolution("B", "T", mesh, nimble.UnifiedMemory, kernel, m)
+	B := conv.Output()
+
+	outputc := B.NewReader()
+	nimble.RunStack()
+	output := host(outputc.ReadNext(mesh.NCell()))
+
+	out0 := core.Reshape(output[0], mesh.Size())
+	out1 := core.Reshape(output[0], mesh.Size())
+	X, Y, Z := N0/2, N1/2, N2/2
+	if out0[X][Y][Z] != -0.9709071 || out1[X][Y][Z] != 0 {
+		fmt.Println("failed, got:", out0[X][Y][Z])
+		os.Exit(2)
+	} else {
+		fmt.Println("OK")
+	}
+}

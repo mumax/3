740bf2e776a55f05c7f8b9afa55817aa051088a8
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-08 21:20:50 +0100

    todo

--- core/build.go
+++ core/build.go
@@ -2,5 +2,5 @@ package core
 
 const (
 	BUILD_COMMIT = "1169"
-	BUILD_DATE   = "Fri Dec  7 16:33:39 CET 2012"
+	BUILD_DATE   = "Fri Dec  7 16:34:25 CET 2012"
 )
--- nimble/doc.go
+++ nimble/doc.go
@@ -7,9 +7,8 @@
  TODO:  Chan.Buffer() buffers one frame, if not yet so.
  TODO:  Consistent API:
 	Only Chan interfaces should be used
- 	NewXXX() does not Stack()
- 	RunXXX() Stacks, returns output(s) ? (to avoid confusion with New)
- 	ExecUnsafe()
+	Routine implements NewReader (output)
+ 	//ExecUnsafe()
 	Run()
  TODO: Automatically report run time.
 */

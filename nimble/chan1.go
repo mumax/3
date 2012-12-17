ca7f255d04e2eae0a12781e3b54106f622ea306c
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-17 14:47:40 +0100

    cleanup chan

--- nimble/chan1.go
+++ nimble/chan1.go
@@ -19,10 +19,14 @@ func MakeChan1(tag, unit string, m *Mesh, memType MemType, bufBlocks int) Chan1 
 	return Chan1{aschan1(MakeSlice(N, memType), tag, unit, m, newRWMutex(N))}
 }
 
-func (c Chan1) ChanN() ChanN { return ChanN{[]Chan1{c}} }
+func (c Chan1) ChanN() ChanN {
+	return ChanN{[]Chan1{c}}
+}
 
 // WriteDone() signals a slice obtained by WriteNext() is done written.
-func (c Chan1) WriteDone() { c.unlock() }
+func (c Chan1) WriteDone() {
+	c.unlock()
+}
 
 // WriteNext returns a buffer Slice of length n to which data
 // can be written. Should be followed by ReadDone().
@@ -33,13 +37,19 @@ func (c Chan1) WriteNext(n int) Slice {
 }
 
 // NComp returns the number of components (1: scalar, 3: vector, ...)
-func (c Chan1) NComp() int { return 1 }
+func (c Chan1) NComp() int {
+	return 1
+}
 
 // BufLen returns the largest buffer size n that can be obained
 // with ReadNext/WriteNext.
-func (c Chan1) BufLen() int { return c.buffer.Len() }
+func (c Chan1) BufLen() int {
+	return c.buffer.Len()
+}
 
-func (c Chan1) NBufferedBlocks() int { return idiv(c.NCell(), c.buffer.Len()) }
+func (c Chan1) NBufferedBlocks() int {
+	return idiv(c.NCell(), c.buffer.Len())
+}
 
 //func (c *Chan1) WriteDelta(Δstart, Δstop int) []float32 {
 //	c.mutex.WriteDelta(Δstart, Δstop)

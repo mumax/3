334afa0d20e5f2305e43cd71d318008ccde18b8e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-18 14:26:22 +0100

    render: poly

--- render/magnetization.go
+++ render/magnetization.go
@@ -4,75 +4,88 @@ import (
 	"code.google.com/p/mx3/core"
 	"code.google.com/p/mx3/dump"
 	gl "github.com/chsc/gogl/gl21"
+	"image/color"
 	"os"
 )
+
+type Poly struct {
+	vertex [4][3]float32
+	color.NRGBA
+}
+
+func (p *Poly) Render() {
+	if p.A != 0 {
+		gl.Color3b(gl.Byte(p.R), gl.Byte(p.G), gl.Byte(p.B))
+		for _, v := range p.vertex {
+			Vertex3f(v[0], v[1], v[2])
+		}
+	}
+}
 
 func Load(fname string) *dump.Frame {
 	f, err := os.Open(fname)
 	core.Fatal(err)
 	defer f.Close()
-
 	r := dump.NewReader(f, dump.CRC_ENABLED)
 	core.Fatal(r.Read())
-	Min = [3]int{0, 0, 0}
-	Max = r.MeshSize
 	return &(r.Frame)
 }
 
-var (
-	Min, Max [3]int
-)
-
-func Render(frame *dump.Frame) {
-	ClearScene()
-
-	size := frame.MeshSize
-	cell := frame.MeshStep
-	maxworld := 0.
-	for i := range size {
-		world := float64(size[i]) * cell[i]
-		if world > maxworld {
-			maxworld = world
-		}
-	}
-	scale := 1 / maxworld
-	rx, ry, rz := float32(0.5*scale*cell[0]), float32(0.5*scale*cell[1]), float32(0.5*scale*cell[2])
-
-	gl.Color3f(0.5, 0.5, 0.5)
-	//Cube(0, 0, 0, 0.5, 0.5, 0.5)
-	M := frame.Vectors()
-	cubes := 0
-	for i := Min[0]; i < Max[0]; i++ {
-		x := float32(scale * cell[0] * (float64(i-size[0]/2) + 0.5))
-		for j := Min[1]; j < Max[1]; j++ {
-			y := float32(scale * cell[1] * (float64(j-size[1]/2) + 0.5))
-			for k := Min[2]; k < Max[2]; k++ {
-				z := float32(scale * cell[2] * (float64(k-size[2]/2) + 0.5))
-				mx, my, mz := M[0][i][j][k], M[1][i][j][k], M[2][i][j][k]
-				if i == Min[0] || i == Max[0]-1 ||
-					j == Min[1] || j == Max[1]-1 ||
-					k == Min[2] || k == Max[2]-1 {
-					gl.Color3f(gl.Float(0.5*(mx+0.5)), gl.Float(0.5*(my+0.5)), gl.Float(0.5*(mz+0.5)))
-					Cube(z, y, x, rz, ry, rx)
-					cubes++
-				}
-			}
-		}
-	}
-	core.Println(cubes, " cubes")
-}
-
-func ClearScene() {
-	ambient := []gl.Float{0.7, 0.7, 0.7, 1}
-	diffuse := []gl.Float{1, 1, 1, 1}
-	lightpos := []gl.Float{0.2, 0.5, 1, 1}
-	gl.Lightfv(gl.LIGHT0, gl.AMBIENT, &ambient[0])
-	gl.Lightfv(gl.LIGHT0, gl.DIFFUSE, &diffuse[0])
-	gl.Lightfv(gl.LIGHT0, gl.POSITION, &lightpos[0])
-	gl.Enable(gl.LIGHT0)
-	ambdiff := []gl.Float{0.5, 0.5, 0.5, 1}
-	gl.Materialfv(gl.FRONT_AND_BACK, gl.AMBIENT_AND_DIFFUSE, &ambdiff[0])
-
-	gl.ClearColor(1, 1, 1, 1)
-	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
-}
+//
+//var (
+//	Min, Max [3]int
+//)
+//
+//func Render(frame *dump.Frame) {
+//	ClearScene()
+//
+//	size := frame.MeshSize
+//	cell := frame.MeshStep
+//	maxworld := 0.
+//	for i := range size {
+//		world := float64(size[i]) * cell[i]
+//		if world > maxworld {
+//			maxworld = world
+//		}
+//	}
+//	scale := 1 / maxworld
+//	rx, ry, rz := float32(0.5*scale*cell[0]), float32(0.5*scale*cell[1]), float32(0.5*scale*cell[2])
+//
+//	gl.Color3f(0.5, 0.5, 0.5)
+//	//Cube(0, 0, 0, 0.5, 0.5, 0.5)
+//	M := frame.Vectors()
+//	cubes := 0
+//	for i := Min[0]; i < Max[0]; i++ {
+//		x := float32(scale * cell[0] * (float64(i-size[0]/2) + 0.5))
+//		for j := Min[1]; j < Max[1]; j++ {
+//			y := float32(scale * cell[1] * (float64(j-size[1]/2) + 0.5))
+//			for k := Min[2]; k < Max[2]; k++ {
+//				z := float32(scale * cell[2] * (float64(k-size[2]/2) + 0.5))
+//				mx, my, mz := M[0][i][j][k], M[1][i][j][k], M[2][i][j][k]
+//				if i == Min[0] || i == Max[0]-1 ||
+//					j == Min[1] || j == Max[1]-1 ||
+//					k == Min[2] || k == Max[2]-1 {
+//					gl.Color3f(gl.Float(0.5*(mx+0.5)), gl.Float(0.5*(my+0.5)), gl.Float(0.5*(mz+0.5)))
+//					Cube(z, y, x, rz, ry, rx)
+//					cubes++
+//				}
+//			}
+//		}
+//	}
+//	core.Println(cubes, " cubes")
+//}
+//
+//func ClearScene() {
+//	ambient := []gl.Float{0.7, 0.7, 0.7, 1}
+//	diffuse := []gl.Float{1, 1, 1, 1}
+//	lightpos := []gl.Float{0.2, 0.5, 1, 1}
+//	gl.Lightfv(gl.LIGHT0, gl.AMBIENT, &ambient[0])
+//	gl.Lightfv(gl.LIGHT0, gl.DIFFUSE, &diffuse[0])
+//	gl.Lightfv(gl.LIGHT0, gl.POSITION, &lightpos[0])
+//	gl.Enable(gl.LIGHT0)
+//	ambdiff := []gl.Float{0.5, 0.5, 0.5, 1}
+//	gl.Materialfv(gl.FRONT_AND_BACK, gl.AMBIENT_AND_DIFFUSE, &ambdiff[0])
+//
+//	gl.ClearColor(1, 1, 1, 1)
+//	gl.Clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT)
+//}

3475b29c5b2f6ea1ca2a4f6834849bb86e661013
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-19 11:59:30 +0100

    render: number screenshots

--- render/screenshot.go
+++ render/screenshot.go
@@ -2,6 +2,7 @@ package render
 
 import (
 	"code.google.com/p/mx3/core"
+	"fmt"
 	gl "github.com/chsc/gogl/gl21"
 	"image"
 	"image/png"
@@ -9,19 +10,21 @@ import (
 	"os"
 )
 
+var scroti int // count screenshots
+
 func Screenshot() {
 	log.Println("screenshot")
 	img := image.NewNRGBA(image.Rect(0, 0, Width, Height))
 	gl.ReadPixels(0, 0, gl.Sizei(Width), gl.Sizei(Height), gl.RGBA, gl.UNSIGNED_INT_8_8_8_8, gl.Pointer(&img.Pix[0]))
-	//go func(){
 	// reverse byte order, opengl seems to use little endian.
 	pix := img.Pix
 	for i := 0; i < len(pix); i += 4 {
 		pix[i+0], pix[i+1], pix[i+2], pix[i+3] = pix[i+3], pix[i+2], pix[i+1], pix[i+0]
 	}
-	f, err := os.OpenFile("scrot.png", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
+	fname := fmt.Sprintf("frame%04d.png", scroti)
+	scroti++
+	f, err := os.OpenFile(fname, os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0666)
 	core.Fatal(err)
 	defer f.Close()
 	core.Fatal(png.Encode(f, img))
-	//}()
 }

4910d9b352c556be88318d65ebe2e5504e5abeda
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2012-12-19 14:18:37 +0100

    render: clean init

--- render/main.go
+++ render/main.go
@@ -3,10 +3,8 @@
 package main
 
 import (
-	"code.google.com/p/mx3/core"
 	. "code.google.com/p/mx3/render"
 	"flag"
-	gl "github.com/chsc/gogl/gl21"
 	"github.com/jteeuwen/glfw"
 	"log"
 	"time"
@@ -22,13 +20,9 @@ func main() {
 
 	Load(flag.Args())
 
-	InitWindow(800, 600)
+	Init(800, 600, *flag_smooth, *flag_multisample)
 	defer glfw.CloseWindow()
 	defer glfw.Terminate()
-
-	InitGL(*flag_smooth, *flag_multisample)
-	InitViewport()
-	InitInputHandlers()
 
 	start := time.Now()
 	frames := 0
@@ -49,39 +43,4 @@ func main() {
 
 	fps := int((float64(frames) / float64(time.Since(start))) * float64(time.Second))
 	log.Println("average FPS:", fps)
-}
-
-// number of bits in buffer
-const (
-	r, g, b, a = 8, 8, 8, 8 // 0 means auto
-	depth      = 16         // 0 means none
-	stencil    = 0          // 0 means none
-)
-
-func InitWindow(w, h int) {
-	core.Fatal(glfw.Init())
-	if *flag_multisample != 0 {
-		glfw.OpenWindowHint(glfw.FsaaSamples, *flag_multisample)
-	}
-	Width, Height = w, h
-	core.Fatal(glfw.OpenWindow(Width, Height, r, g, b, a, depth, stencil, glfw.Windowed))
-	glfw.SetWindowTitle("renderer")
-	glfw.SetSwapInterval(1)
-}
-
-func InitGL(smooth bool, multisample int) {
-	core.Fatal(gl.Init())
-
-	gl.Enable(gl.LIGHTING)
-
-	gl.Enable(gl.CULL_FACE)
-	gl.CullFace(gl.BACK)
-
-	if multisample != 0 {
-		gl.Enable(gl.MULTISAMPLE)
-	}
-
-	if smooth {
-		gl.ShadeModel(gl.SMOOTH)
-	}
 }

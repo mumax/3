412b0e918b867ee0d1371a86ac2ea99154513c06
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-07 14:09:03 +0100

    live render works more or less

--- render/live.go
+++ render/live.go
@@ -1,47 +1,61 @@
-// +build ignore
-
-package main
+package render
 
 import (
-	"code.google.com/p/mx3/frame"
+	"code.google.com/p/mx3/dump"
+	"code.google.com/p/mx3/gpu"
 	"code.google.com/p/mx3/nimble"
-	. "code.google.com/p/mx3/render"
-	"flag"
 	"github.com/jteeuwen/glfw"
-	"log"
-	"time"
+	//"sync"
 )
 
 func Live(in_ nimble.ChanN) {
-	//	in := in_.NewReader()
-	//
-	//	Frame = new(dump.Frame)
-	//	Frame.Components = in.NComp()
-	//	Frame.MeshSize = in.Mesh().Size()
-	//	Frame.MeshStep = in.Mesh().CellSize()
-	//	n := in.Mesh().NCell() * in.NComp()
-	//	Frame.Data = make([]float32, n)
-	//
-	//	Init(800, 600, *flag_smooth, *flag_multisample, true)
-	//	InitInputHandlers()
-	//	defer glfw.CloseWindow()
-	//	defer glfw.Terminate()
-	//
-	//	Viewpos[2] = -20
-	//
-	//	for glfw.WindowParam(glfw.Opened) == 1 { // window open
-	// 
-	//		if data, ok := in.TryRead(n); ok{
-	//
-	//		}
-	//
-	//		Render()
-	//		if Wantscrot {
-	//			Screenshot()
-	//			Wantscrot = false
-	//		}
-	//		wantframe <- 1
-	//
-	//	}
+	go func() {
+		in := in_.NewReader()
 
+		Frame = new(dump.Frame)
+		Frame.Components = in.NComp()
+		Frame.MeshSize = in.Mesh().Size()
+		Frame.MeshStep = in.Mesh().CellSize()
+		Crop2 = Frame.MeshSize
+		ncell := in.Mesh().NCell()
+		n := ncell * in.NComp()
+		Frame.Data = make([]float32, n)
+
+		Init(800, 600, true, 2, true)
+		InitInputHandlers()
+		defer glfw.CloseWindow()
+		defer glfw.Terminate()
+		Viewpos[2] = -20
+
+		//var lock sync.Mutex
+		wantframe := true
+
+		go func() {
+			gpu.LockCudaThread()
+			for {
+				data := in.ReadNext(ncell)
+				if wantframe {
+					for i, d := range data {
+						d.Device().CopyDtoH(Frame.Data[i*ncell : (i+1)*ncell])
+					}
+					wantframe = false
+				}
+				in.ReadDone()
+			}
+		}()
+
+		for glfw.WindowParam(glfw.Opened) == 1 { // window open
+			//lock.Lock()
+			Render()
+			if Wantscrot {
+				Screenshot()
+				Wantscrot = false
+			}
+			if wantframe == false {
+				PreRender(Frame)
+				wantframe = true
+			}
+			//lock.Unlock()
+		}
+	}()
 }

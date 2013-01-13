15ec936c5f4531c21e806b67faf2d78fab33869d
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-14 00:45:01 +0100

    new kernmulrsymm2dyz wrapper

--- gpu/conv_kernmulrsymm2d.go
+++ gpu/conv_kernmulrsymm2d.go
@@ -2,35 +2,16 @@ package gpu
 
 import (
 	"code.google.com/p/mx3/core"
+	"code.google.com/p/mx3/gpu/ptx"
 	"github.com/barnex/cuda5/cu"
 	"github.com/barnex/cuda5/safe"
 	"unsafe"
 )
 
 func kernMulRSymm2Dyz(fftMy, fftMz safe.Complex64s, K11, K22, K12 safe.Float32s, N1, N2 int, stream cu.Stream) {
-
 	core.Assert(K11.Len() == (N1/2+1)*N2)
-
 	gridDim, blockDim := Make2DConf(N1, N2)
-
-	m1ptr := fftMy.Pointer()
-	m2ptr := fftMz.Pointer()
-	k1ptr := K11.Pointer()
-	k2ptr := K22.Pointer()
-	k3ptr := K12.Pointer()
-
-	args := []unsafe.Pointer{
-		unsafe.Pointer(&m1ptr),
-		unsafe.Pointer(&m2ptr),
-		unsafe.Pointer(&k1ptr),
-		unsafe.Pointer(&k2ptr),
-		unsafe.Pointer(&k3ptr),
-		unsafe.Pointer(&N1),
-		unsafe.Pointer(&N2)}
-
-	shmem := 0
-	kernMulRSymm2DyzCode := PTXLoad("kernmulRSymm2Dyz")
-	cu.LaunchKernel(kernMulRSymm2DyzCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
+	ptx.K_kernmulRSymm2Dyz(fftMy.Pointer(), fftMz.Pointer(), K11.Pointer(), K22.Pointer(), K12.Pointer(), N1, N2, gridDim, blockDim)
 }
 
 func kernMulRSymm2Dx(fftMx safe.Complex64s, K00 safe.Float32s, N1, N2 int, stream cu.Stream) {

22e55b99614a273c3849a79faad8102aa5877369
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-13 23:50:57 +0100

    new copypad wrapper

--- gpu/conv_copypad.go
+++ gpu/conv_copypad.go
@@ -2,40 +2,24 @@ package gpu
 
 import (
 	"code.google.com/p/mx3/core"
+	"code.google.com/p/mx3/gpu/ptx"
 	"github.com/barnex/cuda5/cu"
 	"github.com/barnex/cuda5/safe"
-	"unsafe"
 )
 
-// Copies src into dst (which is larger or smaller), at offset position.
+// Copies src into dst, which is larger or smaller.
+// The remainder of dst is not filled with zeros.
 func copyPad(dst, src safe.Float32s, dstsize, srcsize, offset [3]int, stream cu.Stream) {
-	//panic("need to loop for 3D, in kernel")
 	core.Assert(dst.Len() == core.Prod(dstsize))
 	core.Assert(src.Len() == core.Prod(srcsize))
-	// TODO: either remove offset or check offset
 
-	copyPadKern := PTXLoad("copypad")
-
-	dstptr := dst.Pointer()
-	srcptr := src.Pointer()
-
-	block := 16
+	const block = 16
 	gridJ := DivUp(IMin(dstsize[1], srcsize[1]), block)
 	gridK := DivUp(IMin(dstsize[2], srcsize[2]), block)
-	shmem := 0
-	args := []unsafe.Pointer{
-		unsafe.Pointer(&dstptr),
-		unsafe.Pointer(&dstsize[0]),
-		unsafe.Pointer(&dstsize[1]),
-		unsafe.Pointer(&dstsize[2]),
-		unsafe.Pointer(&srcptr),
-		unsafe.Pointer(&srcsize[0]),
-		unsafe.Pointer(&srcsize[1]),
-		unsafe.Pointer(&srcsize[2]),
-		unsafe.Pointer(&offset[0]),
-		unsafe.Pointer(&offset[1]),
-		unsafe.Pointer(&offset[2])}
+	gridSize := cu.Dim3{gridK, gridJ, 1}
+	blockSize := cu.Dim3{block, block, 1}
 
-	// launch config is called x, y, z, but for us this is z, y, x
-	cu.LaunchKernel(copyPadKern, gridK, gridJ, 1, block, block, 1, shmem, stream, args)
+	ptx.K_copypad(dst.Pointer(), dstsize[0], dstsize[1], dstsize[2],
+		src.Pointer(), srcsize[0], srcsize[1], srcsize[2],
+		gridSize, blockSize)
 }
--- gpu/ptx/copypad.cu
+++ gpu/ptx/copypad.cu
@@ -1,16 +1,12 @@
 // Copies src (3D array, size S0 x S1 x S2) to larger dst (3D array, size D0 x D1 x D2).
-// src data is offset by o0,o1,o2.
 // The remainder of dst is NOT zero-padded.
 // E.g.:
 //	a    ->  a x
 //	         x x
 //
-extern int kaka;
-
 extern "C" __global__ void 
 copypad(float* __restrict__  dst, int D0, int D1, int D2, 
-        float* __restrict__  src, int S0, int S1, int S2, 
-        int o0, int o1, int o2){
+        float* __restrict__  src, int S0, int S1, int S2){
 
 	int j = blockIdx.y * blockDim.y + threadIdx.y; // index in src slice
 	int k = blockIdx.x * blockDim.x + threadIdx.x;
@@ -19,13 +15,10 @@ copypad(float* __restrict__  dst, int D0, int D1, int D2,
  		return;	// out of  bounds
 	}
 
-	int J = j + o1;  // index in full src
-	int K = k + o2; 
 	// loop over N layers
 	int N = min(S0, D0);
 	for (int i=0; i<N; i++){
- 		int I = i + o0; // index in full src
-		dst[I*D1*D2 + J*D2 + K] = src[i*S1*S2 + j*S2 + k];
+		dst[i*D1*D2 + j*D2 + k] = src[i*S1*S2 + j*S2 + k];
 	}
 } 
 

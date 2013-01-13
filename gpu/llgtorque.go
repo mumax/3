fcd14f15270cfadf65f99431f69d7a142b5a8cb6
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-13 23:57:56 +0100

    new llgtorue wrapper

--- gpu/llgtorque.go
+++ gpu/llgtorque.go
@@ -2,10 +2,9 @@ package gpu
 
 import (
 	"code.google.com/p/mx3/core"
+	"code.google.com/p/mx3/gpu/ptx"
 	"code.google.com/p/mx3/nimble"
-	"github.com/barnex/cuda5/cu"
 	"github.com/barnex/cuda5/safe"
-	"unsafe"
 )
 
 // LLGTorque calculates the REDUCED torque:
@@ -14,15 +13,13 @@ type LLGTorque struct {
 	torque nimble.ChanN
 	m, b   nimble.RChanN
 	Alpha  float32
-	bExt   [3]float32
-	stream cu.Stream
 }
 
 func NewLLGTorque(tag string, m_, B_ nimble.ChanN, alpha float32) *LLGTorque {
 	m, B := m_.NewReader(), B_.NewReader()
 	core.Assert(B.Size() == m.Size())
 	torque := nimble.MakeChanN(3, tag, "T", m.Mesh(), m_.MemType(), 1)
-	tq := &LLGTorque{torque, m, B, alpha, [3]float32{0, 0, 0}, cu.StreamCreate()}
+	tq := &LLGTorque{torque, m, B, alpha}
 	nimble.Stack(tq)
 	return tq
 }
@@ -32,7 +29,7 @@ func (r *LLGTorque) Output() nimble.ChanN { return r.torque }
 func (r *LLGTorque) Run() {
 	LockCudaThread()
 	for {
-		r.Exec() // TODO: exec(t,m,b)
+		r.Exec()
 	}
 }
 
@@ -42,47 +39,20 @@ func (r *LLGTorque) Exec() {
 	B := Device3(r.b.ReadNext(n))
 	T := Device3(r.torque.WriteNext(n))
 
-	CalcLLGTorque(T, M, B, r.Alpha, r.bExt, r.stream)
+	CalcLLGTorque(T, M, B, r.Alpha)
 
 	r.torque.WriteDone()
 	r.m.ReadDone()
 	r.b.ReadDone()
 }
 
-func CalcLLGTorque(torque, m, B [3]safe.Float32s, alpha float32, bExt [3]float32, stream cu.Stream) {
-	core.Assert(bExt == [3]float32{0, 0, 0})
-
-	llgtorqueCode := PTXLoad("llgtorque")
-
+func CalcLLGTorque(torque, m, B [3]safe.Float32s, alpha float32) {
 	N := torque[0].Len()
 	gridDim, blockDim := Make1DConf(N)
-
-	t0ptr := torque[0].Pointer()
-	t1ptr := torque[1].Pointer()
-	t2ptr := torque[2].Pointer()
-	m0ptr := m[0].Pointer()
-	m1ptr := m[1].Pointer()
-	m2ptr := m[2].Pointer()
-	B0ptr := B[0].Pointer()
-	B1ptr := B[1].Pointer()
-	B2ptr := B[2].Pointer()
-
-	args := []unsafe.Pointer{
-		unsafe.Pointer(&t0ptr),
-		unsafe.Pointer(&t1ptr),
-		unsafe.Pointer(&t2ptr),
-		unsafe.Pointer(&m0ptr),
-		unsafe.Pointer(&m1ptr),
-		unsafe.Pointer(&m2ptr),
-		unsafe.Pointer(&B0ptr),
-		unsafe.Pointer(&B1ptr),
-		unsafe.Pointer(&B2ptr),
-		unsafe.Pointer(&alpha),
-		unsafe.Pointer(&N)}
-
-	shmem := 0
-	cu.LaunchKernel(llgtorqueCode, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
-	stream.Synchronize()
+	ptx.K_llgtorque(torque[0].Pointer(), torque[1].Pointer(), torque[2].Pointer(),
+		m[0].Pointer(), m[1].Pointer(), m[2].Pointer(),
+		B[0].Pointer(), B[1].Pointer(), B[2].Pointer(),
+		alpha, N, gridDim, blockDim)
 }
 
 func Device3(s []nimble.Slice) [3]safe.Float32s {

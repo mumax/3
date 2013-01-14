bba243f1f8786accf4ede1ca9eef14b08b6bf202
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-14 01:41:57 +0100

    cleanup conv_kernmulrsymm.go

--- gpu/conv_kernmulrsymm.go
+++ gpu/conv_kernmulrsymm.go
@@ -19,3 +19,11 @@ func kernMulRSymm2Dx(fftMx safe.Complex64s, K00 safe.Float32s, N1, N2 int) {
 	gridDim, blockDim := Make2DConf(N1, N2)
 	ptx.K_kernmulRSymm2Dx(fftMx.Pointer(), K00.Pointer(), N1, N2, gridDim, blockDim)
 }
+
+func kernMulRSymm3D(fftM [3]safe.Complex64s, K00, K11, K22, K12, K02, K01 safe.Float32s, N0, N1, N2 int) {
+	core.Assert(K11.Len() == N0*N1*N2)
+	gridDim, blockDim := Make2DConf(N1, N2)
+	ptx.K_kernmulRSymm3D(fftM[0].Pointer(), fftM[1].Pointer(), fftM[2].Pointer(),
+		K00.Pointer(), K11.Pointer(), K22.Pointer(), K12.Pointer(), K02.Pointer(), K01.Pointer(),
+		N0, N1, N2, gridDim, blockDim)
+}

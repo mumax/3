b446db058be3797f7648817eee23a79ef0bb8a88
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-14 01:39:59 +0100

    copypad: use make2Dconf

--- gpu/conv_copypad.go
+++ gpu/conv_copypad.go
@@ -3,7 +3,6 @@ package gpu
 import (
 	"code.google.com/p/mx3/core"
 	"code.google.com/p/mx3/gpu/ptx"
-	"github.com/barnex/cuda5/cu"
 	"github.com/barnex/cuda5/safe"
 )
 
@@ -13,11 +12,9 @@ func copyPad(dst, src safe.Float32s, dstsize, srcsize [3]int) {
 	core.Assert(dst.Len() == core.Prod(dstsize))
 	core.Assert(src.Len() == core.Prod(srcsize))
 
-	const block = 16
-	gridJ := DivUp(IMin(dstsize[1], srcsize[1]), block)
-	gridK := DivUp(IMin(dstsize[2], srcsize[2]), block)
-	gridSize := cu.Dim3{gridK, gridJ, 1}
-	blockSize := cu.Dim3{block, block, 1}
+	NO := IMin(dstsize[1], srcsize[1])
+	N1 := IMin(dstsize[2], srcsize[2])
+	gridSize, blockSize := Make2DConf(NO, N1)
 
 	ptx.K_copypad(dst.Pointer(), dstsize[0], dstsize[1], dstsize[2],
 		src.Pointer(), srcsize[0], srcsize[1], srcsize[2],

0b0bbe9ea444710c33ce8aaf9dd012abae3727b3
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-01-18 17:18:47 +0100

    cleanup reduce3

--- gpu/reduce.go
+++ gpu/reduce.go
@@ -48,6 +48,17 @@ func reduce2(in1, in2 safe.Float32s, init float32, f func(in1, in2, out cu.Devic
 	return copyback(out)
 }
 
+// general reduce wrapper for 3 input arrays
+func reduce3(in1, in2, in3 safe.Float32s, init float32, f func(in1, in2, in3, out cu.DevicePtr, init float32, N int, grid, block cu.Dim3)) float32 {
+	N := in1.Len()
+	core.Assert(in2.Len() == N && in3.Len() == N)
+	out := reduceBuf(init)
+	defer reduceRecycle(out)
+	gr, bl := reduceConf()
+	f(in1.Pointer(), in2.Pointer(), in3.Pointer(), out.Pointer(), init, N, gr, bl)
+	return copyback(out)
+}
+
 // Maximum difference between the two arrays.
 // 	max_i abs(a[i] - b[i])
 func MaxDiff(a, b safe.Float32s, stream cu.Stream) float32 {
@@ -57,7 +68,8 @@ func MaxDiff(a, b safe.Float32s, stream cu.Stream) float32 {
 // Maximum of the norms of all vectors (x[i], y[i], z[i]).
 // 	max_i sqrt( x[i]*x[i] + y[i]*y[i] + z[i]*z[i] )
 func MaxVecNorm(x, y, z safe.Float32s, stream cu.Stream) float64 {
-	return math.Sqrt(float64(reduce3("reducemaxvecnorm2", x, y, z, 0, stream)))
+	r := reduce3(x, y, z, 0, ptx.K_reducemaxvecnorm2)
+	return math.Sqrt(float64(r))
 }
 
 // Maximum of the norms of the difference between all vectors (x1,y1,z1) and (x2,y2,z2)
@@ -69,40 +81,6 @@ func MaxVecDiff(x1, y1, z1, x2, y2, z2 safe.Float32s, stream cu.Stream) float64 
 
 func Dot(x1, x2 safe.Float32s) {
 	panic("todo")
-}
-
-// 3-input reduce with arbitrary PTX code (op)
-func reduce3(op string, in1, in2, in3 safe.Float32s, init float32, stream cu.Stream) float32 {
-	out := reduceBuf(init)
-	defer reduceRecycle(out)
-
-	core.Assert(in1.Len() == in2.Len())
-	N := in1.Len()
-
-	blockDim := cu.Dim3{512, 1, 1}
-	gridDim := cu.Dim3{8, 1, 1} // 8 is typ. number of multiprocessors.
-
-	in1ptr := in1.Pointer()
-	in2ptr := in2.Pointer()
-	in3ptr := in3.Pointer()
-	outptr := out.Pointer()
-	args := []unsafe.Pointer{
-		unsafe.Pointer(&in1ptr),
-		unsafe.Pointer(&in2ptr),
-		unsafe.Pointer(&in3ptr),
-		unsafe.Pointer(&outptr),
-		unsafe.Pointer(&init),
-		unsafe.Pointer(&N)}
-
-	shmem := 0
-	code := PTXLoad(op)
-	cu.LaunchKernel(code, gridDim.X, gridDim.Y, gridDim.Z, blockDim.X, blockDim.Y, blockDim.Z, shmem, stream, args)
-	stream.Synchronize()
-
-	var result_ [1]float32
-	result := result_[:]
-	out.CopyDtoH(result) // async? register one arena block // , stream)
-	return result_[0]
 }
 
 // 6-input reduce with arbitrary PTX code (op)

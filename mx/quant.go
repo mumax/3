f56b2188d14c7d048eeaf62b3e74a5f40f0e7b1b
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-02-06 15:09:35 +0100

    mx/slice

--- mx/quant.go
+++ mx/quant.go
@@ -3,4 +3,160 @@ package mx
 // File: Quant stores a physical quantity.
 // Author: Arne Vansteenkiste
 
-import ()
+// shared by Quant and Reader
+type quant struct {
+	buffer    Slice           // stores the data
+	lock      [MAX_COMP]mutex // protects buffer. mutex can be read or write type
+	tag, unit string          // TODO: fuse with mesh into info
+	mesh      *Mesh
+}
+
+type Quant struct {
+	quant
+}
+
+type Reader struct {
+	quant
+}
+
+func NewQuant(nComp int, tag, unit string, m *Mesh) Quant {
+	bufBlocks := 0
+	N := bufferSize(m, bufBlocks)
+	buffer := gpuSlice(nComp, N)
+	var lock [MAX_COMP]mutex
+	for c := 0; c < nComp; c++ {
+		lock[c] = newRWMutex(N)
+	}
+	return Quant{quant{buffer, lock, tag, unit, m}}
+}
+
+//func (c *Quant) NewReader() Reader {
+//	reader := c.quant // copy
+//	for i := range reader.lock {
+//		reader.lock[i] = (reader.lock[i]).(*rwMutex).MakeRMutex()
+//	}
+//	return Reader{reader}
+//}
+//
+//// NComp returns the number of components.
+//// 	scalar: 1
+//// 	vector: 3
+//// 	...
+//func (c *quant) NComp() int {
+//	return int(c.buffer.nComp)
+//}
+//
+//func (c *quant) comp(i int) quant {
+//	return quant{c.buffer.Comp(i), [MAX_COMP]mutex{c.lock[i]}, c.tag, c.unit, c.mesh}
+//}
+//
+//func (c *Quant) Comp(i int) Quant {
+//	return Quant{c.comp(i)}
+//}
+//
+//func (c *Reader) Comp(i int) Reader {
+//	return Reader{c.comp(i)}
+//}
+//
+//// BufLen returns the number of buffered elements.
+//// This is the largest number of elements that can be read/written at once.
+//func (c *quant) BufLen() int {
+//	return c.buffer.Len()
+//}
+//
+//func (c *quant) Unit() string {
+//	return c.unit
+//}
+//
+//func (c *quant) Tag() string {
+//	return c.tag
+//}
+//
+//func (c *quant) Mesh() *Mesh {
+//	return c.mesh
+//}
+//
+//func (c *quant) MemType() MemType {
+//	return c.buffer.MemType
+//}
+//
+////func (c Quant) NBufferedBlocks() int { return c.comp[0].NBufferedBlocks() }
+////func (c Quant) MemType() MemType { return c.buffer.MemType }
+////func (c Quant) Quant() Quant     { return c } // implements Chan iface
+////func (c Quant) Comp(i int) Chan1 { return c.comp[i] }
+//
+//// UnsafeData returns the data buffer without locking.
+//// To be used with extreme care.
+//func (c *quant) UnsafeData() Slice {
+//	return c.buffer
+//}
+//
+//// WriteNext locks and returns a slice of length n for
+//// writing the next n elements to the Chan3.
+//// When done, WriteDone() should be called to "send" the
+//// slice down the Chan3. After that, the slice is not valid any more.
+//func (c *quant) next(n int) Slice {
+//	c.lock[0].lockNext(n)
+//	a, b := c.lock[0].lockedRange()
+//	for i := 1; i < len(c.lock); i++ {
+//		c.lock[i].lockNext(n)
+//		α, β := c.lock[i].lockedRange()
+//		if α != a || β != b {
+//			panic("chan: next: inconsistent state")
+//		}
+//	}
+//	next := c.buffer.Slice(a, b)
+//	return next
+//}
+//
+//func (c *quant) done() {
+//	for i := range c.lock {
+//		c.lock[i].unlock()
+//	}
+//}
+//
+//func (c *Quant) WriteNext(n int) Slice {
+//	return c.quant.next(n)
+//}
+//
+//func (c *Quant) WriteDone() {
+//	c.quant.done()
+//}
+//
+//func (c *Reader) ReadNext(n int) Slice {
+//	return c.quant.next(n)
+//}
+//
+//func (c *Reader) ReadDone() {
+//	c.quant.done()
+//}
+//
+//// WriteDone() signals a slice obtained by WriteNext() is fully
+//// written and can be sent down the Chan3.
+//
+////func (c Quant) WriteDelta(Δstart, Δstop int) [][]float32 {
+////	next := make([][]float32, c.NComp())
+////	for i := range c {
+////		c[i].WriteDelta(Δstart, Δstop)
+////		a, b := c[i].mutex.WRange()
+////		next[i] = c[i].slice.Slice(a, b).list
+////	}
+////	return next
+////}
+//
+//func bufferSize(m *Mesh, bufBlocks int) int {
+//	N := -666
+//	if bufBlocks < 1 { // means auto
+//		N = m.NCell() // buffer all
+//	} else {
+//		N = m.BlockLen() * bufBlocks
+//	}
+//	return N
+//}
+//
+//// safe integer division.
+//func idiv(a, b int) int {
+//	core.Assert(a%b == 0)
+//	return a / b
+//}
+//
---
+++ mx/slice.go
@@ -0,0 +1,148 @@
+package mx
+
+// File: Slice stores N-component GPU or host data.
+// Author: Arne Vansteenkiste
+
+import (
+	"fmt"
+	"github.com/barnex/cuda5/cu"
+	"unsafe"
+)
+
+const MAX_COMP = 3 // Maximum supported number of Slice components
+
+// Slice is like a [][]float32, but may be stored in GPU or host memory.
+type Slice struct {
+	ptr     [MAX_COMP]unsafe.Pointer
+	len_    int32
+	nComp   int8
+	memType int8
+}
+
+// Make a GPU Slice with nComp components each of size length.
+func MakeSlice(nComp, length int) Slice {
+	return gpuSlice(nComp, length)
+}
+
+// alloc slice on gpu
+func gpuSlice(nComp, length int) Slice {
+	bytes := int64(length) * cu.SIZEOF_FLOAT32
+	var ptrs [MAX_COMP]unsafe.Pointer
+	for c := 0; c < nComp; c++ {
+		ptrs[c] = unsafe.Pointer(cu.MemAlloc(bytes))
+	}
+	return Slice{ptrs, int32(length), int8(nComp), gpuMemory}
+}
+
+const (
+	cpuMemory     = 1 << 0
+	gpuMemory     = 1 << 1
+	unifiedMemory = cpuMemory | gpuMemory
+)
+
+// GPUAccess returns whether the Slice is accessible by the GPU.
+// true means it is either stored on GPU or in unified host memory.
+func (s *Slice) GPUAccess() bool {
+	return s.memType&gpuMemory != 0
+}
+
+// CPUAccess returns whether the Slice is accessible by the CPU.
+// true means it is either stored in host memory.
+func (s *Slice) CPUAccess() bool {
+	return s.memType&cpuMemory != 0
+}
+
+// NComp returns the number of components.
+func (s *Slice) NComp() int {
+	return int(s.nComp)
+}
+
+// Len returns the number of elements per component.
+func (s *Slice) Len() int {
+	return int(s.len_)
+}
+
+// Bytes returns the number of storage bytes per component.
+func (s *Slice) Bytes() int64 {
+	return int64(s.len_) * cu.SIZEOF_FLOAT32
+}
+
+// Comp returns a single component of the Slice.
+func (s *Slice) Comp(i int) Slice {
+	return Slice{[MAX_COMP]unsafe.Pointer{s.ptr[i]}, s.len_, 1, s.memType}
+}
+
+// Slice returns a slice sharing memory with the original.
+func (s *Slice) Slice(a, b int) Slice {
+	len_ := int(s.len_)
+	if a >= len_ || b > len_ || a > b || a < 0 || b < 0 {
+		panic(fmt.Errorf("slice range out of bounds: [%v:%v] (len=%v)", a, b, len_))
+	}
+	var slice Slice
+	for i := range s.ptr {
+		s.ptr[i] = unsafe.Pointer(uintptr(s.ptr[i]) + cu.SIZEOF_FLOAT32*uintptr(a))
+	}
+	slice.len_ = int32(b - a)
+	slice.nComp = s.nComp
+	return slice
+}
+
+//func unifiedSlice(N int) Slice {
+//	bytes := int64(N) * SizeofFloat32
+//	ptr := unsafe.Pointer(cu.MemAllocHost(bytes))
+//	ptrs := [MAX_COMP]unsafe.Pointer{ptr}
+//	return Slice{ptrs, N, 1, UnifiedMemory}
+//}
+//
+//func ToSlice(list []float32) Slice {
+//	ptr := unsafe.Pointer(&list[0])
+//	ptrs := [MAX_COMP]unsafe.Pointer{ptr}
+//	return Slice{ptrs, len(list), 1, CPUMemory}
+//}
+//
+//func UnsafeSlice(ptr unsafe.Pointer, len_ int, flag MemType) Slice {
+//	if len_ < 0 {
+//		panic("negative slice length")
+//	}
+//	ptrs := [MAX_COMP]unsafe.Pointer{ptr}
+//	return Slice{ptrs, len_, 1, flag}
+//}
+
+func (s *Slice) DevPtr(component int) cu.DevicePtr {
+	return cu.DevicePtr(s.ptr[:s.nComp][component])
+}
+
+// TODO: rm
+//func (s *Slice) Safe(component int) safe.Float32s {
+//	var f safe.Float32s
+//	f.UnsafeSet(unsafe.Pointer(s.DevPtr(component)), s.Len(), s.Len())
+//	return f
+//}
+
+//func (s *Slice) Host() []float32 {
+//	if s.nComp != 1 {
+//		panic("need to implement for components")
+//	}
+//
+//	if s.MemType&CPUMemory == 0 {
+//		core.Panicf("slice not accessible by CPU (memory type %v)", s.MemType)
+//	}
+//	var list []float32
+//	hdr := (*reflect.SliceHeader)(unsafe.Pointer(&list))
+//	hdr.Data = uintptr(s.ptr[0])
+//	hdr.Len = int(s.len_)
+//	hdr.Cap = hdr.Len
+//	return list
+//}
+//
+//func (s *Slice) Device() safe.Float32s {
+//	if s.nComp != 1 {
+//		panic(fmt.Errorf("slice.device: need 1 component, have %v", s.nComp))
+//	}
+//	if s.MemType&GPUMemory == 0 {
+//		panic(fmt.Errorf("slice not accessible by GPU (memory type %v)", s.MemType))
+//	}
+//	var floats safe.Float32s
+//	floats.UnsafeSet(s.ptr[0], s.Len(), s.Len())
+//	return floats
+//}

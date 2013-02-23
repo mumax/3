e7ae1526cc37cf735fa8a7e823bea1b2fce36e74
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-02-23 20:59:16 +0100

    import normalize

---
+++ cuda/normalize.go
@@ -0,0 +1,14 @@
+package cuda
+
+import (
+	"code.google.com/p/mx3/data"
+	"code.google.com/p/mx3/kernel"
+)
+
+// Normalize the vector field to unit length.
+// 0-length vectors are unaffected.
+func Normalize(vec *data.Slice) {
+	N := vec.Len()
+	gridDim, blockDim := Make1DConf(N)
+	kernel.K_normalize(vec.DevPtr(0), vec.DevPtr(1), vec.DevPtr(2), N, gridDim, blockDim)
+}

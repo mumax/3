3afc7af4a15069ed8cff3e5f5a3fad9761a696df
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-02-25 10:57:29 +0100

    fix dash.go, not sure where to place

--- util/dash.go
+++ util/dash.go
@@ -8,9 +8,9 @@ import (
 var lastdash = time.Now()
 
 func Dash(step, undone int, t, dt, err float64) {
-	if *Flag_silent {
-		return
-	}
+	//	if *Flag_silent {
+	//		return
+	//	}
 	if time.Since(lastdash) > 100*time.Millisecond {
 		lastdash = time.Now()
 		fmt.Printf("step: % 8d (%6d) t: % 12es Δt: % 12es ε:% 12e\u000D", step, undone, float32(t), float32(dt), float32(err))
@@ -18,8 +18,8 @@ func Dash(step, undone int, t, dt, err float64) {
 }
 
 func DashExit() {
-	if *Flag_silent {
-		return
-	}
+	//	if *Flag_silent {
+	//		return
+	//	}
 	fmt.Println()
 }

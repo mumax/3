2456b02bfbe524ea571a88bb067010f836ba6827
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-02-26 15:02:25 +0100

    improve madd performance by streaming

--- cuda/madd.go
+++ cuda/madd.go
@@ -7,42 +7,44 @@ import (
 )
 
 // multiply-add: dst[i] = src1[i] * factor1 + src2[i] * factor2
-// TODO: could be streamed
 func Madd2(dst, src1, src2 *data.Slice, factor1, factor2 float32) {
 	N := dst.Len()
 	nComp := dst.NComp()
 	util.Assert(src1.Len() == N && src2.Len() == N)
 	util.Assert(src1.NComp() == nComp && src2.NComp() == nComp)
-	gridDim, blockDim := Make1DConf(N)
+	gr, bl := Make1DConf(N)
+	str := kernel.Stream()
 	for c := 0; c < nComp; c++ {
-		kernel.K_madd2(dst.DevPtr(c), src1.DevPtr(c), factor1,
-			src2.DevPtr(c), factor2, N, gridDim, blockDim)
+		kernel.K_madd2_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
+			src2.DevPtr(c), factor2, N, gr, bl, str)
 	}
+	kernel.SyncAndRecycle(str)
 }
 
 // multiply-add: dst[i] = src1[i] * factor1 + src2[i] * factor2 + src3 * factor3
-// TODO: could be streamed
 func Madd3(dst, src1, src2, src3 *data.Slice, factor1, factor2, factor3 float32) {
 	N := dst.Len()
 	nComp := dst.NComp()
 	util.Assert(src1.Len() == N && src2.Len() == N && src3.Len() == N)
 	util.Assert(src1.NComp() == nComp && src2.NComp() == nComp && src3.NComp() == nComp)
-	gridDim, blockDim := Make1DConf(N)
+	gr, bl := Make1DConf(N)
+	str := kernel.Stream()
 	for c := 0; c < nComp; c++ {
-		kernel.K_madd3(dst.DevPtr(c), src1.DevPtr(c), factor1,
-			src2.DevPtr(c), factor2, src3.DevPtr(c), factor3,
-			N, gridDim, blockDim)
+		kernel.K_madd3_async(dst.DevPtr(c), src1.DevPtr(c), factor1,
+			src2.DevPtr(c), factor2, src3.DevPtr(c), factor3, N, gr, bl, str)
 	}
+	kernel.SyncAndRecycle(str)
 }
 
 // Adds a constant to each element of the slice.
 // 	dst[comp][index] += cnst[comp]
-// TODO: stream
 func AddConst(dst *data.Slice, cnst ...float32) {
 	util.Argument(len(cnst) == dst.NComp())
 	N := dst.Len()
 	gr, bl := Make1DConf(N)
+	str := kernel.Stream()
 	for c := 0; c < dst.NComp(); c++ {
-		kernel.K_madd2(dst.DevPtr(c), dst.DevPtr(c), 1, nil, cnst[c], N, gr, bl)
+		kernel.K_madd2_async(dst.DevPtr(c), dst.DevPtr(c), 1, nil, cnst[c], N, gr, bl, str)
 	}
+	kernel.SyncAndRecycle(str)
 }

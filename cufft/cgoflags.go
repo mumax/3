e929c0caca09af5bf2703ec05461244e2e99ddf5
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-01 14:12:32 +0100

    -lcudart flag

--- cufft/cgoflags.go
+++ cufft/cgoflags.go
@@ -2,7 +2,7 @@ package cufft
 
 // This file provides CGO flags.
 
-//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib -L/usr/lib64/nvidia/ -L/usr/lib/nvidia/ -L/usr/lib/nvidia-current/ -L/opt/cuda/lib64 -L/opt/cuda/lib -lcuda -lcufft
+//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib -L/usr/lib64/nvidia/ -L/usr/lib/nvidia/ -L/usr/lib/nvidia-current/ -L/opt/cuda/lib64 -L/opt/cuda/lib -lcudart -lcufft
 //#cgo CFLAGS:-I/usr/local/cuda/include/ -I/opt/cuda/include
 import "C"
 

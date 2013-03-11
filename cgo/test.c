a1019c2cc19e1297f08f86529cb1a3d172bc2053
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-11 12:46:48 +0100

    rm cgo/ (obsolete)

--- cgo/test.c
+++
@@ -1,6 +0,0 @@
-#include "test.h"
-#include "_cgo_export.h"
-
-int test(){
-	return 42;
-}
--- cgo/test.go
+++
@@ -1,8 +0,0 @@
-package cgo
-
-// #include "test.h"
-import "C"
-
-func Test() int {
-	return int(C.test())
-}

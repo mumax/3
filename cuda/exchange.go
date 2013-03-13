ff44ee75d9c865a8fddb416bd5d003ef5b671693
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-13 10:24:23 +0100

    exchange streamed concurrency

--- cuda/exchange.go
+++ cuda/exchange.go
@@ -3,6 +3,7 @@ package cuda
 import (
 	"code.google.com/p/mx3/data"
 	"code.google.com/p/mx3/mag"
+	"github.com/barnex/cuda5/cu"
 )
 
 // TODO: m should be normalized.
@@ -16,11 +17,15 @@ func Exchange(Hex *data.Slice, M *data.Slice, Aex float64) {
 	w1 := float32(Aex / (c[1] * c[1]))
 	w2 := float32(Aex / (c[2] * c[2]))
 	cfg := make2DConf(N[2], N[1])
-	str := stream()
+
+	str := [3]cu.Stream{stream(), stream(), stream()}
 	for c := 0; c < 3; c++ {
-		k_exchange1comp_async(Hex.DevPtr(c), M.DevPtr(c), w0, w1, w2, N[0], N[1], N[2], cfg, str)
+		k_exchange1comp_async(Hex.DevPtr(c), M.DevPtr(c), w0, w1, w2, N[0], N[1], N[2], cfg, str[c])
 	}
-	syncAndRecycle(str)
+	syncAndRecycle(str[0])
+	syncAndRecycle(str[1])
+	syncAndRecycle(str[2])
+
 	//	k_exchange(Hex.DevPtr(0), Hex.DevPtr(1), Hex.DevPtr(2),
 	//		M.DevPtr(0), M.DevPtr(1), M.DevPtr(2),
 	//		w0, w1, w2, N[0], N[1], N[2], cfg)

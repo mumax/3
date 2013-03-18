6b396cd30de0a97dc76f6bf9720ba1f2f145123f
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-18 14:58:39 +0100

    simplify common_stencil.h

--- cuda/common_stencil.h
+++ cuda/common_stencil.h
@@ -4,17 +4,13 @@
 #include "float3.h"
 
 // clamps i between 0 and N-1
-inline __device__ int clamp(int i, int N){
-	return min( max(i, 0) , N-1 );
-}
+#define clamp(i, N) min( max((i), 0), (N)-1 )
 
-inline __device__ int lclamp(int i){
-	return  max(i, 0);
-}
+// clamps i to positive values
+#define lclamp(i) max((i), 0)
 
-inline __device__ int hclamp(int i, int N){
-	return min(i, N-1);
-}
+// clamps i to < N
+#define hclamp(i, N) min((i), (N)-1)
 
 // 3D array indexing
 #define idx(i,j,k) ((i)*N1*N2 + (j)*N2 + (k))

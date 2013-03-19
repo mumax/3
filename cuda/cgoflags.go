456c71a1e35f6676bf73b4a5e791e51972e160a1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-19 14:44:19 +0100

    add some cuda runtime wrappers

---
+++ cuda/cgoflags.go
@@ -0,0 +1,9 @@
+package cuda
+
+// This file provides CGO flags.
+
+import "C"
+
+//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib -L/opt/cuda/lib -L/opt/cuda/lib64 -lcudart
+//#cgo CFLAGS:-I/usr/local/cuda/include/ -I/opt/cuda/include
+import "C"

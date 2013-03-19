8418a532533e2e25c63cb20b3c1e72fb2bdb5e4c
A.Vansteenkiste <A.Vansteenkiste@gmail.com>
2013-03-19 11:37:40 +0000

    exchange: use STENCIL_BLOCK_X

--- cuda/exchange.go
+++ cuda/exchange.go
@@ -6,6 +6,11 @@ import (
 	"github.com/barnex/cuda5/cu"
 )
 
+//#include "common_stencil.h"
+import "C"
+
+const STENCIL_BLOCKSIZE = C.STENCIL_BLOCK_X
+
 // Add exchange field to Beff.
 func AddExchange(Beff *data.Slice, m *data.Slice, Aex, Bsat float64) {
 	AddAnisoExchange(Beff, m, Aex, Aex, Aex, Bsat)
@@ -21,7 +26,7 @@ func AddAnisoExchange(Beff *data.Slice, m *data.Slice, AexX, AexY, AexZ, Bsat fl
 	w0 := float32(2 * AexX * mag.Mu0 / (Bsat * c[0] * c[0]))
 	w1 := float32(2 * AexY * mag.Mu0 / (Bsat * c[1] * c[1]))
 	w2 := float32(2 * AexZ * mag.Mu0 / (Bsat * c[2] * c[2]))
-	cfg := make2DConf(N[2], N[1])
+	cfg := make2DConf(N[2], N[1], STENCIL_BLOCKSIZE)
 
 	str := [3]cu.Stream{stream(), stream(), stream()}
 	for c := 0; c < 3; c++ {

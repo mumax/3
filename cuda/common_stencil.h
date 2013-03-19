c99631713da56b286ae723bae7a46f27e9630ec2
A.Vansteenkiste <A.Vansteenkiste@gmail.com>
2013-03-19 11:32:49 +0000

    define STENCIL_MAXTHREADS

--- cuda/common_stencil.h
+++ cuda/common_stencil.h
@@ -1,8 +1,6 @@
 #ifndef _COMMON_STENCIL_H_
 #define _COMMON_STENCIL_H_
 
-#include "float3.h"
-
 // clamps i between 0 and N-1
 #define clamp(i, N) min( max((i), 0), (N)-1 )
 
@@ -14,6 +12,10 @@
 
 // 3D array indexing
 #define idx(i,j,k) ((i)*N1*N2 + (j)*N2 + (k))
+
+// Maximum threads per block for stencil op
+#define STENCIL_BLOCKSIZE_X 16
+#define STENCIL_MAXTHREADS (STENCIL_BLOCKSIZE_X * STENCIL_BLOCKSIZE_X)
 
 #endif
 

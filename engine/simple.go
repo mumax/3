87a6a57ecec83c5c78b75c82d617268430978c70
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-26 17:02:58 +0100

    engine cleanup

--- engine/simple.go
+++ engine/simple.go
@@ -23,15 +23,13 @@ var (
 )
 
 var (
-	M, Torque *Buffered
-	B_demag   *Adder
+	M, Torque       *Buffered
+	B_demag, B_exch *Adder
 )
 
 func initialize() {
 
 	M = NewBuffered(3, "m")
-
-	Torque = NewBuffered(3, "torque")
 
 	demag_ := cuda.NewDemag(mesh)
 	vol := data.NilSlice(3, mesh)
@@ -41,29 +39,19 @@ func initialize() {
 		M.ReadDone()
 	})
 
+	B_exch = NewAdder("B_exch", func(dst *data.Slice) {
+		m := M.Read()
+		cuda.AddExchange(dst, m, Aex(), Mu0*Msat())
+		M.ReadDone()
+	})
+
+	Torque = NewBuffered(3, "torque")
+
 	Solver = cuda.NewHeun(mesh, TorqueFn, 1e-15, Gamma0, &Time)
-
-	//buffer = cuda.NewSlice(3, mesh)
-	//vol = data.NilSlice(1, mesh)
-
-	//demag_ := cuda.NewDemag(mesh)
-	//demag = func(dst *data.Slice) {
-	//	demag_.Exec(dst, m, vol, Mu0*Msat())
-	//}
-	//B_demag = newHandle("B_demag")
-
-	//exch = func(dst *data.Slice) {
-	//	cuda.AddExchange(dst, m, Aex(), Mu0*Msat())
-	//}
-	//B_exch = newHandle("B_exch")
-
-	//B_eff = newHandle("B_eff")
-
-	//Torque = newHandle("torque")
 }
 
 func TorqueFn() *data.Slice {
-	return nil
+
 }
 
 func SetMesh(Nx, Ny, Nz int, cellSizeX, cellSizeY, cellSizeZ float64) {

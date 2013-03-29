696b3287b92ca7296d33f3c043441766155cc185
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-03-29 09:33:58 +0100

    re-add mag/constants.go

---
+++ mag/constants.go
@@ -0,0 +1,8 @@
+package mag
+
+import "math"
+
+const (
+	Gamma0 = 1.7595e11          //   1.76085970839e11   // Gyromagnetic ratio of electron, in rad/Ts
+	Mu0    = 4 * math.Pi * 1e-7 // Permeability of vacuum in Tm/A
+)

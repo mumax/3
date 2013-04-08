f5cd5c810b23e93b2fe778d4ee1aa8fdc0a073a1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-08 19:55:33 +0200

    web: super basic renderer works

--- web/render.go
+++ web/render.go
@@ -1,6 +1,7 @@
 package web
 
 import (
+	"code.google.com/p/mx3/cuda"
 	"code.google.com/p/mx3/draw"
 	"code.google.com/p/mx3/engine"
 	"image/jpeg"
@@ -15,6 +16,7 @@ func render(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "render: unknown quantity: "+url, http.StatusNotFound)
 		return
 	} else {
+		cuda.LockThread()                               // TODO: for bootstrapping only, use dedicated thread
 		img := draw.Image(h.Download(), "auto", "auto") // TODO: not very concurrent
 		jpeg.Encode(w, img, &jpeg.Options{Quality: 100})
 	}

e25091600407c384605a89df6bc36607d0994f94
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-10 12:26:32 +0200

    implement DMI according to Bogdanov and Roessler.

--- cuda/dmi.cu
+++ cuda/dmi.cu
@@ -1,13 +1,13 @@
 #include "stencil.h"
 
-// Dzyaloshinskii-Moriya interaction.
-// m is normalized.
-
+// Dzyaloshinskii-Moriya interaction according to
+// Bagdanov and Röβler, PRL 87, 3, 2001. Eq. (6) and (8).
+// m: normalized magnetization
+// H: effective field in Tesla
 extern "C" __global__ void
 adddmi(float* __restrict__ Hx, float* __restrict__ Hy, float* __restrict__ Hz,
        float* __restrict__ mx, float* __restrict__ my, float* __restrict__ mz,
-       float dx, float dy, float dz, // DMI vector in Tesla / m
-       float cx, float cy, float cz, // cell size in m
+       float Dx, float Dy, float Dz, // DMI vector / cell size, in Tesla
        int N0, int N1, int N2){
 
 	int j = blockIdx.x * blockDim.x + threadIdx.x;
@@ -20,25 +20,27 @@ adddmi(float* __restrict__ Hx, float* __restrict__ Hy, float* __restrict__ Hz,
 	for(int i=0; i<N0; i++){
 
 		int I = idx(i, j, k);
+		float3 h = make_float3(Hx[I], Hy[I], Hz[I]); // add to H
 
-		if (dx != 0){
-			float dmzdy = diff(mz, 0, 1, 0, cy); // ∂mz / ∂y
-			float dmydz = diff(my, 0, 0, 1, cz); // ∂my / ∂z
-			Hx[I] += dx * (-dmzdy + dmydz); 
+		if (Dx != 0){
+			h.z += Dx * delta(mx, 1, 0, 0); // Dx * ∂mx / ∂x
+			h.x -= Dx * delta(mz, 1, 0, 0); // Dx * ∂mz / ∂x
 		}
 
-		if (dy != 0){
-			float dmzdx = diff(mz, 1, 0, 0, cx);
-			float dmxdz = diff(mx, 0, 0, 1, cz);
-			Hy[I] += dy * (dmzdx - dmxdz); 
+		if (Dy != 0){
+			h.z += Dy * delta(my, 0, 1, 0); 
+			h.y -= Dx * delta(mz, 0, 1, 0); 
 		}
 
-		if (dz != 0){
-			float dmydx = diff(my, 1, 0, 0, cx);
-			float dmxdy = diff(mx, 0, 1, 0, cy);
-			Hz[I] += dz * (-dmydx + dmxdy); 
+		if (Dz != 0){
+			h.x += Dx * delta(my, 0, 0, 1); 
+			h.y -= Dx * delta(mx, 0, 0, 1); 
 		}
-		// note: left-handed coordinate system.
+
+		// write back, result is H + Hdmi
+		Hx[I] = h.x;
+		Hy[I] = h.y;
+		Hz[I] = h.z;
 	}
 }
 

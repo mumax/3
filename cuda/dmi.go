79fedb9031a062a0f0200c960cf4549ba7705ce0
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-11 16:09:12 +0200

    DMI strength now scalar

--- cuda/dmi.go
+++ cuda/dmi.go
@@ -3,17 +3,18 @@ package cuda
 import "code.google.com/p/mx3/data"
 
 // Add effective field of Dzyaloshinskii-Moriya interaction to Beff (Tesla).
+// According to Bagdanov and Röβler, PRL 87, 3, 2001. eq.8 (out-of-plane symmetry breaking).
 // m: normalized
 // D: J/m²
-func AddDMI(Beff *data.Slice, m *data.Slice, Dx, Dy, Dz, Msat float64) {
+func AddDMI(Beff *data.Slice, m *data.Slice, D, Msat float64) {
 	// TODO: size check
 	mesh := Beff.Mesh()
 	N := mesh.Size()
 	c := mesh.CellSize()
 
-	dx := float32(Dx / (Msat * c[0])) // actually (2 * D) / (2 * Msat * c)
-	dy := float32(Dy / (Msat * c[1]))
-	dz := float32(Dz / (Msat * c[2]))
+	dx := float32(D / (Msat * c[0])) // actually (2*D) / (Msat * 2*c), 2*c disappears in kernel.
+	dy := float32(D / (Msat * c[1]))
+	dz := float32(D / (Msat * c[2]))
 
 	cfg := make2DConf(N[2], N[1])
 	k_adddmi(Beff.DevPtr(0), Beff.DevPtr(1), Beff.DevPtr(2),

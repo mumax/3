1506e9d04064f52af60fca3f0f23a37e5553ffdd
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-17 12:03:33 +0200

    mx3 main program starts web gui

--- main.go
+++ main.go
@@ -1,13 +1,22 @@
 package main
 
-// dummy imports to make go get fetch all of them.
+// main program starts a web gui.
+
 import (
-	_ "code.google.com/p/mx3/cuda"
-	_ "code.google.com/p/mx3/data"
-	_ "code.google.com/p/mx3/engine"
-	_ "code.google.com/p/mx3/test"
+	. "code.google.com/p/mx3/engine"
 )
 
 func main() {
+	Init()
+	defer Close()
+
+	SetMesh(32, 32, 1, 1e-9, 1e-9, 1e-9)
+
+	Msat = Const(1000e3)
+	Aex = Const(10e-12)
+	Alpha = Const(1)
+	SetMUniform(1, 1, 0)
+
+	Interactive()
 
 }

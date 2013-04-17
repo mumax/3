7afe4ae3a8d7acf0e1764e3f8f051df5fc278c16
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-17 13:22:20 +0200

    mx3: launch web gui in browser window

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ package main
 
 import (
 	. "code.google.com/p/mx3/engine"
+	"log"
+	"os/exec"
 )
 
 func main() {
@@ -17,6 +19,21 @@ func main() {
 	Alpha = Const(1)
 	SetMUniform(1, 1, 0)
 
+	go openbrowser("http://localhost" + *Flag_port)
 	Interactive()
-
 }
+
+// Try to open url in a browser. Instruct to do so if it fails.
+func openbrowser(url string) {
+	for _, cmd := range browsers {
+		err := exec.Command(cmd, url).Start()
+		if err == nil {
+			log.Println("\n ====\n openend web interface in", cmd, "\n ====\n")
+			return
+		}
+	}
+	log.Println("\n ===== \n Please open ", url, " in a browser \n ==== \n")
+}
+
+// list of browsers to try.
+var browsers = []string{"x-www-browser", "google-chrome", "chromium-browser", "firefox", "ie", "iexplore"}

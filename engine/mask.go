e305ba74efd6e29fa2d5fee0816042ab0cfe66ca
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-24 10:26:55 +0200

    cleanup staggered mask api

--- engine/mask.go
+++ engine/mask.go
@@ -12,17 +12,27 @@ type StaggeredMask struct {
 	mask *data.Slice
 }
 
+// Set the value of all cell faces with their normal along direction. E.g.:
+// 	SetAll(X, 1) // sets all faces in YZ plane to value 1.
 func (m *StaggeredMask) SetAll(direction int, value float64) {
 	m.init()
 	cuda.Memset(m.mask.Comp(swapIndex(direction, 3)), float32(value))
 }
 
-func (m *StaggeredMask) SetLeftOf(direction int, ix, iy, iz int, value float64) {
+// Sets the value at the "lower/left" face of cell(ix, iy, iz). E.g.:
+// 	SetSide1(X, i, j, k, value) // sets value on the left side of cell i,j,k.
+// 	SetSide1(Y, i, j, k, value) // sets value below (along Y) of cell i,j,k.
+// 	SetSide1(Z, i, j, k, value) // sets value below (along Z) of cell i,j,k.
+func (m *StaggeredMask) SetSide1(direction int, ix, iy, iz int, value float64) {
 	m.init()
 	cuda.SetCell(m.mask, swapIndex(direction, 3), iz, iy, ix, float32(value))
 }
 
-func (m *StaggeredMask) SetRightOf(direction int, ix, iy, iz int, value float64) {
+// Sets the value at the "upper/right" face of cell(ix, iy, iz). E.g.:
+// 	SetSide2(X, i, j, k, value) // sets value on the right side of cell i,j,k.
+// 	SetSide2(Y, i, j, k, value) // sets value above (along Y) of cell i,j,k.
+// 	SetSide2(Z, i, j, k, value) // sets value above (along Z) of cell i,j,k.
+func (m *StaggeredMask) SetSide2(direction int, ix, iy, iz int, value float64) {
 	m.init()
 	direction = swapIndex(direction, 3)
 	i := [3]int{iz, iy, ix}

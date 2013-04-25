e38bacd229c573d59a3a5acd010aa8e0d0bace56
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-04-25 09:18:52 +0200

    rm test-old

--- test-old/test4.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-go build test4.go || exit 2
-for CPU in 1 4; do
-	export GOMAXPROCS=$CPU;
-	for N in 9999999; do # 4096 1024 256 128; do
-		./test4 -maxblocklen $N || exit 2
-	done
-done
-rm -f ./test4

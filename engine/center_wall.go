101d40051aa610946ec0b9194d7847564f8f700d
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-05-14 10:31:40 +0200

    general wall center functions

--- engine/center_wall.go
+++ engine/center_wall.go
@@ -1,18 +1,52 @@
 package engine
 
-// Shift the magnetization to the left or right in order to keep mz close zero.
-// Thus moving an up-down domain wall to the center of the simulation box.
+import "fmt"
+
+// This post-step function centers the simulation window on a domain wall between up-down (or down-up) domains (like in perpendicular media). E.g.:
+// 	PostStep(CenterPMAWall)
 func CenterPMAWall() {
-	mz := M.Average()[Z] // TODO: optimize
-	if mz > 0.01 {
-		M.Shift(-1, 0, 0) // 1 cell to the left // TODO: shift others
+	mz := M.Average()[Z]           // TODO: optimize
+	tolerance := 4 / float64(Nx()) // 2 * expected <m> change for 1 cell shift
+
+	if mz < tolerance {
+		sign := signum(M.GetCell(Z, 0, Ny()/2, Nz()/2))
+		M.Shift(sign, 0, 0)
 		return
 	}
-	if mz < -0.01 {
-		M.Shift(1, 0, 0) // 1 cell to the right
+	if mz > tolerance {
+		sign := signum(M.GetCell(Z, 0, Ny()/2, Nz()/2))
+		M.Shift(-sign, 0, 0)
+	}
+}
+
+// This post-step function centers the simulation window on a domain wall between left-right (or right-left) domains (like in soft thin films). E.g.:
+// 	PostStep(CenterInplaneWall)
+func CenterInplaneWall() {
+	mz := M.Average()[X]           // TODO: optimize
+	tolerance := 4 / float64(Nx()) // 2 * expected <m> change for 1 cell shift
+
+	if mz < tolerance {
+		sign := signum(M.GetCell(X, 0, Ny()/2, Nz()/2))
+		M.Shift(sign, 0, 0)
+		return
+	}
+	if mz > tolerance {
+		sign := signum(M.GetCell(X, 0, Ny()/2, Nz()/2))
+		M.Shift(-sign, 0, 0)
 	}
 }
 
 func init() {
-	parser.AddFunc("centerPMAwall", CenterPMAWall)
+	parser.AddFunc("centerPMAWall", CenterPMAWall)
+	parser.AddFunc("centerInplaneWall", CenterInplaneWall)
+}
+
+func signum(x float64) int {
+	if x > 0.6 {
+		return 1
+	}
+	if x < -0.6 {
+		return -1
+	}
+	panic(fmt.Errorf("center wall: unclear in which direction to shift: magnetization at border=%v", x))
 }

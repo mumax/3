a446a70678398a758df1c9f86abbfcff8ceeebb1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-05-15 17:06:40 +0200

    cleanup web gui

--- web/make.bash
+++ web/make.bash
@@ -4,7 +4,7 @@ echo // THIS FILE IS AUTO GENERATED FROM webgui.js >> js.go
 echo // EDITING IS FUTILE >> js.go
 echo  >> js.go
 echo const templText = \` >> js.go
-cat gui.js >> js.go
+cat gui.html >> js.go
 echo \` >> js.go
 gofmt -w js.go
 go install -v

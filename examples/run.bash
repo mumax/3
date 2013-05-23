5113669d8d9ec0e07fe007cf3fba2439b8030f83
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-05-23 12:11:11 +0200

    remove .go examples in favor of scripts

--- examples/run.bash
+++ examples/run.bash
@@ -2,20 +2,9 @@
 
 scripts=*.txt
 for f in $scripts; do
-	mx3-vet $f|| exit 1;
+	../mx3 -vet $f|| exit 1;
 done;
 for f in $scripts; do
 	../mx3 -f $f|| exit 1;
-done;
-
-files=*.go
-files=$(echo $files | sed s/doc.go//g)
-
-./make.bash || exit 1
-
-for f in $files; do
-	a=$(echo $f | sed s/.go//g)
-	./$a -o $a.out -f -s || exit 1;
-	rm $a
 done;
 

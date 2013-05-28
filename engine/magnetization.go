8bff644c3ed8edc296abf3a5a6e9b99928784001
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-05-28 09:33:21 +0200

    magnetization.go to stencil geometry upon m=...

---
+++ engine/magnetization.go
@@ -0,0 +1,24 @@
+package engine
+
+import (
+	"code.google.com/p/mx3/cuda"
+	"code.google.com/p/mx3/data"
+)
+
+// special bufferedQuant to store magnetization.
+// Set is overridden to stencil m with geometry.
+type magnetization struct {
+	bufferedQuant
+}
+
+func (q *magnetization) init() {
+	q.bufferedQuant.buffer = cuda.NewSlice(3, Mesh())
+}
+
+func (b *magnetization) Set(src *data.Slice) {
+	if src.Mesh().Size() != b.buffer.Mesh().Size() {
+		src = data.Resample(src, b.buffer.Mesh().Size())
+	}
+	// stencil here
+	data.Copy(b.buffer, src)
+}

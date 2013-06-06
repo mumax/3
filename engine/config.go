cb1847803b2b7421e587f31b283bd96b313437f2
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-06-06 10:17:03 +0200

    re-implement twodomain config with smooth wall

--- engine/config.go
+++ engine/config.go
@@ -9,8 +9,8 @@ import (
 func init() {
 	world.Func("uniform", Uniform)
 	world.Func("vortex", Vortex)
-	//	world.Func("twodomain", TwoDomain)
-	//	world.Func("vortexwall", VortexWall)
+	world.Func("twodomain", TwoDomain)
+	//world.Func("vortexwall", VortexWall)
 }
 
 // magnetic configuration
@@ -25,6 +25,7 @@ func Uniform(mx, my, mz float64) Config {
 }
 
 // Make a vortex magnetization with given circulation and core polarization (+1 or -1).
+// The core is smoothed over a few exchange lengths and should easily relax to its ground state.
 func Vortex(circ, pol int) Config {
 	diam2 := 2 * (Aex() / (0.5 * Mu0 * Msat() * Msat())) // inverse core diam squared (roughly)
 	return func(x, y, z float64) [3]float64 {
@@ -45,57 +46,28 @@ func Vortex(circ, pol int) Config {
 //	SetRegion(m, nx/2-ny/2, 0, 0, nx/2+ny/2, ny, nz, Vortex(circ, pol)) // center
 //	return m
 //}
-//
-//// Make a 2-domain configuration with domain wall.
-//// (mx1, my1, mz1) and (mx2, my2, mz2) are the magnetizations in the left and right domain, respectively.
-//// (mxwall, mywall, mzwall) is the magnetization in the wall.
-//// E.g.:
-//// 	M.Set(TwoDomain(1,0,0,  0,1,0,  -1,0,0)) // head-to-head domains with transverse (Néel) wall
-//// 	M.Set(TwoDomain(1,0,0,  0,0,1,  -1,0,0)) // head-to-head domains with perpendicular (Bloch) wall
-//// 	M.Set(TwoDomain(0,0,1,  1,0,0,   0,0,-1))// up-down domains with Bloch wall
-//func TwoDomain(mx1, my1, mz1, mxwall, mywall, mzwall, mx2, my2, mz2 float64) *data.Slice {
-//	m1 := vec(mx1, my1, mz1)
-//	m2 := vec(mx2, my2, mz2)
-//	mw := vec(mxwall, mywall, mzwall)
-//	m := data.NewSlice(3, Mesh())
-//	Nz := Mesh().Size()[0]
-//	Ny := Mesh().Size()[1]
-//	Nx := Mesh().Size()[2]
-//	util.Argument(Nx >= 4)
-//	v := m.Vectors()
-//	for c := range mw {
-//		for i := 0; i < Nz; i++ {
-//			for j := 0; j < Ny; j++ {
-//				for k := 0; k < Nx/2; k++ {
-//					v[c][i][j][k] = m1[c]
-//				}
-//				for k := Nx / 2; k < Nx; k++ {
-//					v[c][i][j][k] = m2[c]
-//				}
-//				v[c][i][j][Nx/2-2] += mw[c]
-//				v[c][i][j][Nx/2-1] = mw[c]
-//				v[c][i][j][Nx/2] = mw[c]
-//				v[c][i][j][Nx/2+1] += mw[c]
-//			}
-//		}
-//	}
-//	return m
-//}
-//
-//
-//// Only sets the region between cells [x1, y1, z1] and [x2, y2, z2] (excl.) to the given configuration.
-//func SetRegion(dst *data.Slice, x1, y1, z1, x2, y2, z2 int, config *data.Slice) {
-//	v := dst.Vectors()
-//	src := config.Vectors()
-//
-//	for c := range v {
-//		for i := z1; i < z2; i++ {
-//			for j := y1; j < y2; j++ {
-//				for k := x1; k < x2; k++ {
-//					v[c][i][j][k] = src[c][i][j][k]
-//				}
-//			}
-//		}
-//	}
-//}
-//
+
+// Make a 2-domain configuration with domain wall.
+// (mx1, my1, mz1) and (mx2, my2, mz2) are the magnetizations in the left and right domain, respectively.
+// (mxwall, mywall, mzwall) is the magnetization in the wall. The wall is smoothed over a few cells so it will
+// easily relax to its ground state.
+// E.g.:
+// 	TwoDomain(1,0,0,  0,1,0,  -1,0,0) // head-to-head domains with transverse (Néel) wall
+// 	TwoDomain(1,0,0,  0,0,1,  -1,0,0) // head-to-head domains with perpendicular (Bloch) wall
+// 	TwoDomain(0,0,1,  1,0,0,   0,0,-1)// up-down domains with Bloch wall
+func TwoDomain(mx1, my1, mz1, mxwall, mywall, mzwall, mx2, my2, mz2 float64) Config {
+	ww := 2 * Mesh().CellSize()[2] // wall width in cells
+	return func(x, y, z float64) [3]float64 {
+		var m [3]float64
+		if x < 0 {
+			m = [3]float64{mx1, my1, mz1}
+		} else {
+			m = [3]float64{mx2, my2, mz2}
+		}
+		gauss := math.Exp(-sqr64(x / ww))
+		m[0] = (1-gauss)*m[0] + gauss*mxwall
+		m[1] = (1-gauss)*m[1] + gauss*mywall
+		m[2] = (1-gauss)*m[2] + gauss*mzwall
+		return m
+	}
+}

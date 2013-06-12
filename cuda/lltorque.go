170f193080c20764ec5429546aa82281ab0b0dee
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-06-12 09:06:34 +0200

    rm dampingtorque

--- cuda/lltorque.go
+++ cuda/lltorque.go
@@ -21,12 +21,3 @@ func LLTorque(torque, m, B *data.Slice, alpha LUTPtr, regions *Bytes) {
 		B.DevPtr(0), B.DevPtr(1), B.DevPtr(2),
 		unsafe.Pointer(alpha), regions.Ptr, N, cfg)
 }
-
-// Only the damping term of LLGTorque, with alpha 1. Useful for relaxation.
-//func DampingTorque(torque, m, B *data.Slice) {
-//	N := torque.Len()
-//	cfg := make1DConf(N)
-//
-//	k_dampingtorque(torque.DevPtr(0), torque.DevPtr(1), torque.DevPtr(2),
-//		m.DevPtr(0), m.DevPtr(1), m.DevPtr(2), B.DevPtr(0), B.DevPtr(1), B.DevPtr(2), N, cfg)
-//}

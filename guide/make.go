76eb7f131ba5a5eb252330d4313cda5dbcc132de
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-06-14 13:59:39 +0200

    guide: exec examples

--- guide/make.go
+++ guide/make.go
@@ -32,17 +32,30 @@ type State struct {
 
 func (s *State) Example(in string) string {
 
+	// extract example source
 	in = strings.Replace(in, "@", "\n", -1) // undo raw string hack
 	in = strings.Trim(in, "\n")
 
-	infile := fmt.Sprintf("example%v.txt", s.count)
-	check(ioutil.WriteFile(infile, []byte(in), 0666))
-	out, err := exec.Command("mx3", "-f", infile).CombinedOutput()
-	os.Stdout.Write(out)
-	check(err)
+	// exec input file
+	check(ioutil.WriteFile(s.infile(), []byte(in), 0666))
+	cmd("mx3", "-f", s.infile())
 
 	s.count++
 	return `<pre>` + in + `</pre>`
+}
+
+func (s *State) infile() string {
+	return fmt.Sprintf("example%v.txt", s.count)
+}
+
+func (s *State) outfile() string {
+	return fmt.Sprintf("example%v.out", s.count)
+}
+
+func cmd(cmd string, args ...string) {
+	out, err := exec.Command(cmd, args...).CombinedOutput()
+	os.Stdout.Write(out)
+	check(err)
 
 }
 

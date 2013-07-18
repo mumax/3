882c2b95f9a4c7da07f845176f0e58c10a6a8aa1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-07-18 11:08:52 +0200

    gui: solver control

--- gui/span.go
+++ gui/span.go
@@ -6,11 +6,16 @@ import (
 
 // {{.Span id value}} adds a piece of text ("label") to the document.
 func (d *Doc) Span(id string, value ...string) string {
+	val := cat(value)
+	e := newElem(id, "innerHTML", val)
+	d.add(e)
+	return fmt.Sprintf(`<span id=%v>%v</span>`, e.Id(), val)
+}
+
+func cat(value []string) string {
 	val := ""
 	for _, v := range value {
 		val += v + " "
 	}
-	e := newElem(id, "innerHTML", val)
-	d.add(e)
-	return fmt.Sprintf(`<span id=%v>%v</span>`, e.Id(), val)
+	return val
 }
--- gui/textbox.go
+++ gui/textbox.go
@@ -7,8 +7,9 @@ import (
 // {{.Textbox id value}} adds a textbox to the document.
 // value is the initial text in the box.
 // optional width?
-func (d *Doc) TextBox(id, value string) string {
-	e := newElem(id, "value", value)
+func (d *Doc) TextBox(id string, value ...string) string {
+	val := cat(value)
+	e := newElem(id, "value", val)
 	d.add(e)
-	return fmt.Sprintf(`<input type=textbox class=TextBox id=%v onchange="notifytextbox('%v')"/>`, id, id) // todo: onblur...
+	return fmt.Sprintf(`<input type=textbox class=TextBox id=%v value="%v" onchange="notifytextbox('%v')"/>`, id, val, id) // todo: onblur...
 }

bd85682685467e805a4b5dc47cb7c19524839cf0
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-08-27 14:43:28 +0200

    cleanup ptx load output

--- cuda/fatbin.go
+++ cuda/fatbin.go
@@ -16,6 +16,5 @@ func fatbinLoad(sm map[int]string, fn string) cu.Function {
 	if best == 0 {
 		log.Fatalln("Unsupported GPU compute capability:", cudaCC)
 	}
-	log.Println("load PTX", best, fn)
 	return cu.ModuleLoadData(sm[best]).GetFunction(fn)
 }

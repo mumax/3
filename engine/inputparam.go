276d622e6ffde72760edf38f7965b8b304754a04
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-11 21:05:03 +0200

    backup

--- engine/inputparam.go
+++ engine/inputparam.go
@@ -1,92 +1,98 @@
 package engine
 
-import (
-	"code.google.com/p/mx3/cuda"
-	"code.google.com/p/mx3/data"
-	"code.google.com/p/mx3/util"
-	"log"
-)
+//import (
+//	"code.google.com/p/mx3/cuda"
+//	"code.google.com/p/mx3/data"
+//	"code.google.com/p/mx3/util"
+//	"log"
+//)
 
 type inputParam struct {
-	param
-	upd_reg [NREGION]func() []float64
+	cpu_buf   [][NREGION]float32 // look-up table source
+	upd_reg   [NREGION]func() []float64
+	timestamp float64
+	uptodate  bool
 }
 
-func (p *inputParam) init(nComp int, name, unit string) {
-	p.param.init_(nComp, name, unit, p.update)
+func (p *inputParam) init(nComp int) {
+	p.cpu_buf = make([][NREGION]float32)
 }
 
-func (p *inputParam) update() {
-	if p.modtime < Time {
+func (p *inputParam) Cpu() [][NREGION]float64 {
+	if p.timestamp != Time || p.uptodate == false {
+
 		for r := 0; r < regions.maxreg; r++ {
-			if p.upd_reg[r] != nil {
-				p.modtime = Time // !
-				p.gpu_ok = false // !
-				log.Println("update", p.name, "reg=", r)
-				v := p.upd_reg[r]()
-				for c := range p.cpu_buf {
-					p.cpu_buf[c][r] = float32(v[c])
-				}
+			updFunc := p.upd_reg[r]
+			if updFunc == nil {
+				continue
+			}
+			v := updFunc[r]()
+			for c := range p.cpu_buf {
+				p.cpu_buf[c][r] = float32(v[c])
 			}
 		}
+
+		p.timestamp = Time
+		p.uptodate = true
 	}
+	return p.cpu_buf
 }
 
-func (p *inputParam) setRegion(region int, v ...float64) {
-	util.Argument(len(v) == p.NComp()) // note: also panics if param not initialized (nComp = 0)
+func (p *inputParam) setRegion(region int, v []float64) {
+	util.Argument(len(v) == len(p.cpu_buf))
 
 	p.upd_reg[region] = nil
-	p.update() // ! make sure everything has same time stamp
 
 	for c := range v {
 		p.cpu_buf[c][region] = float32(v[c])
 	}
-	p.modtime = Time
-	p.gpu_ok = false
+	p.uptodate = false
 }
 
-// set in all regions except 0
-// TODO: should region zero really have unset params?
-// TODO: check if we always start from 1
-func (p *inputParam) setUniform(v ...float64) {
-	for r := 1; r < NREGION; r++ {
-		p.setRegion(r, v...)
-	}
-}
-
-func (p *inputParam) getRegion(region int) []float64 {
-	cpu, _ := p.Cpu()
-	v := make([]float64, p.nComp)
-	for i := range v {
-		v[i] = float64(cpu[i][region])
-	}
-	return v
-}
-
-func (p *inputParam) GetVec() []float64 {
-	return p.getRegion(1) // TODO: revise
-}
-
-func (p *param) getUniform() []float64 {
-	cpu, _ := p.Cpu()
-	v := make([]float64, p.NComp())
-	for c := range v {
-		x := cpu[c][1]
-		for r := 2; r < regions.maxreg; r++ {
-			if p.cpu_buf[c][r] != x {
-				log.Panicf("%v is not uniform", p.name)
-			}
-		}
-		v[c] = float64(x)
-	}
-	return v
-}
-
-func (p *param) Get() (*data.Slice, bool) {
-	gpu := p.gpu()
-	b := cuda.GetBuffer(p.NComp(), p.Mesh())
-	for c := 0; c < p.NComp(); c++ {
-		cuda.RegionDecode(b.Comp(c), cuda.LUTPtr(gpu[c]), regions.Gpu())
-	}
-	return b, true
-}
+//
+//
+//// set in all regions except 0
+//// TODO: should region zero really have unset params?
+//// TODO: check if we always start from 1
+//func (p *inputParam) setUniform(v ...float64) {
+//	for r := 1; r < NREGION; r++ {
+//		p.setRegion(r, v...)
+//	}
+//}
+//
+//func (p *inputParam) getRegion(region int) []float64 {
+//	cpu, _ := p.Cpu()
+//	v := make([]float64, p.nComp)
+//	for i := range v {
+//		v[i] = float64(cpu[i][region])
+//	}
+//	return v
+//}
+//
+//func (p *inputParam) GetVec() []float64 {
+//	return p.getRegion(1) // TODO: revise
+//}
+//
+//func (p *param) getUniform() []float64 {
+//	cpu, _ := p.Cpu()
+//	v := make([]float64, p.NComp())
+//	for c := range v {
+//		x := cpu[c][1]
+//		for r := 2; r < regions.maxreg; r++ {
+//			if p.cpu_buf[c][r] != x {
+//				log.Panicf("%v is not uniform", p.name)
+//			}
+//		}
+//		v[c] = float64(x)
+//	}
+//	return v
+//}
+//
+//func (p *param) Get() (*data.Slice, bool) {
+//	gpu := p.gpu()
+//	b := cuda.GetBuffer(p.NComp(), p.Mesh())
+//	for c := 0; c < p.NComp(); c++ {
+//		cuda.RegionDecode(b.Comp(c), cuda.LUTPtr(gpu[c]), regions.Gpu())
+//	}
+//	return b, true
+//}
--- engine/param.go
+++ engine/param.go
@@ -10,81 +10,81 @@ import (
 	"unsafe"
 )
 
-// param stores a space-dependent material parameter,
-// by keeping a look-up table mapping region index to float value.
-type param struct {
-	gpu_ok  bool               // gpu cache up-to date with lut source
-	gpu_buf cuda.LUTPtrs       // gpu copy of lut, lazily transferred when needed
-	cpu_buf [][NREGION]float32 // look-up table source // TODO []*[NREGION]float32
-	update  func()             // updates cpu_buf, if needed
-	modtime float64            // timestamp for cpu data
-	doc
-}
-
-func (p *param) init_(nComp int, name, unit string, upd func()) {
-	util.Assert(nComp > 0)
-	p.cpu_buf = make([][NREGION]float32, nComp)
-	p.update = upd
-	p.doc = doc{nComp: nComp, name: name, unit: unit}
-	p.modtime = math.Inf(-1)
-}
-
-func (p *param) Cpu() (data [][NREGION]float32, modtime float64) {
-	p.update()
-	return p.cpu_buf, p.modtime
-}
-
-// Get a GPU mirror of the look-up table, copies to GPU if needed.
-func (p *param) gpu() cuda.LUTPtrs {
-	if !p.gpu_ok {
-		p.upload()
-	}
-	return p.gpu_buf
-}
-
-func (p *param) Gpu1() cuda.LUTPtr {
-	ptr := cuda.LUTPtr(p.gpu()[0])
-	return ptr
-}
-
-func (p *param) Gpu3() cuda.LUTPtrs { return p.gpu() }
-
-// upload cpu table to gpu
-func (p *param) upload() {
-	p.assureAlloc()
-	_, _ = p.Cpu() // assures cpu is up-to-date, but don't leak pointer to heap
-	for c2 := range p.gpu_buf {
-		c := util.SwapIndex(c2, p.NComp()) // XYZ swap here
-		cu.MemcpyHtoD(cu.DevicePtr(p.gpu_buf[c]), unsafe.Pointer(&p.cpu_buf[c2][0]), cu.SIZEOF_FLOAT32*NREGION)
-	}
-	p.gpu_ok = true
-	log.Println("upload", p.name)
-}
-
-// allocte if when needed
-func (p *param) assureAlloc() {
-	util.Assert(p.NComp() > 0)
-	if p.gpu_buf == nil {
-		log.Println("alloc", p.name)
-		p.gpu_buf = make(cuda.LUTPtrs, p.NComp())
-		for i := range p.gpu_buf {
-			p.gpu_buf[i] = cuda.MemAlloc(NREGION * cu.SIZEOF_FLOAT32)
-		}
-	}
-}
-
-func (p *param) zero() bool {
-	cpu, _ := p.Cpu()
-	for c := range cpu {
-		for r := 0; r < regions.maxreg; r++ {
-			if cpu[c][r] != 0 {
-				return false
-			}
-		}
-	}
-	return true
-}
-
-func (p *param) Mesh() *data.Mesh {
-	return &globalmesh
-}
+//// param stores a space-dependent material parameter,
+//// by keeping a look-up table mapping region index to float value.
+//type param struct {
+//	gpu_ok  bool               // gpu cache up-to date with lut source
+//	gpu_buf cuda.LUTPtrs       // gpu copy of lut, lazily transferred when needed
+//	cpu_buf [][NREGION]float32 // look-up table source // TODO []*[NREGION]float32
+//	update  func()             // updates cpu_buf, if needed
+//	modtime float64            // timestamp for cpu data
+//	doc
+//}
+//
+//func (p *param) init_(nComp int, name, unit string, upd func()) {
+//	util.Assert(nComp > 0)
+//	p.cpu_buf = make([][NREGION]float32, nComp)
+//	p.update = upd
+//	p.doc = doc{nComp: nComp, name: name, unit: unit}
+//	p.modtime = math.Inf(-1)
+//}
+//
+//func (p *param) Cpu() (data [][NREGION]float32, modtime float64) {
+//	p.update()
+//	return p.cpu_buf, p.modtime
+//}
+//
+//// Get a GPU mirror of the look-up table, copies to GPU if needed.
+//func (p *param) gpu() cuda.LUTPtrs {
+//	if !p.gpu_ok {
+//		p.upload()
+//	}
+//	return p.gpu_buf
+//}
+//
+//func (p *param) Gpu1() cuda.LUTPtr {
+//	ptr := cuda.LUTPtr(p.gpu()[0])
+//	return ptr
+//}
+//
+//func (p *param) Gpu3() cuda.LUTPtrs { return p.gpu() }
+//
+//// upload cpu table to gpu
+//func (p *param) upload() {
+//	p.assureAlloc()
+//	_, _ = p.Cpu() // assures cpu is up-to-date, but don't leak pointer to heap
+//	for c2 := range p.gpu_buf {
+//		c := util.SwapIndex(c2, p.NComp()) // XYZ swap here
+//		cu.MemcpyHtoD(cu.DevicePtr(p.gpu_buf[c]), unsafe.Pointer(&p.cpu_buf[c2][0]), cu.SIZEOF_FLOAT32*NREGION)
+//	}
+//	p.gpu_ok = true
+//	log.Println("upload", p.name)
+//}
+//
+//// allocte if when needed
+//func (p *param) assureAlloc() {
+//	util.Assert(p.NComp() > 0)
+//	if p.gpu_buf == nil {
+//		log.Println("alloc", p.name)
+//		p.gpu_buf = make(cuda.LUTPtrs, p.NComp())
+//		for i := range p.gpu_buf {
+//			p.gpu_buf[i] = cuda.MemAlloc(NREGION * cu.SIZEOF_FLOAT32)
+//		}
+//	}
+//}
+//
+//func (p *param) zero() bool {
+//	cpu, _ := p.Cpu()
+//	for c := range cpu {
+//		for r := 0; r < regions.maxreg; r++ {
+//			if cpu[c][r] != 0 {
+//				return false
+//			}
+//		}
+//	}
+//	return true
+//}
+//
+//func (p *param) Mesh() *data.Mesh {
+//	return &globalmesh
+//}

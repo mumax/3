032c8f2dc0b4c895a1ec51cc67bee858fca921c3
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-16 11:34:22 +0200

    add mx3-script toy interpreter

---
+++ tools/mx3-script/main.go
@@ -0,0 +1,79 @@
+/*
+	Toy interpreter executes scripts or stdin.
+*/
+package main
+
+import (
+	"bufio"
+	"code.google.com/p/mx3/script"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+)
+
+var debug = flag.Bool("g", false, "print debug output")
+
+var (
+	world *script.World
+	ps1   string
+)
+
+func main() {
+	log.SetFlags(0)
+	flag.Parse()
+	world = script.NewWorld()
+	script.Debug = *debug
+
+	if flag.NArg() > 1 {
+		check(fmt.Errorf("need 0 or 1 input files"))
+	}
+
+	if flag.NArg() == 1 {
+		src, err := os.Open(flag.Arg(0))
+		check(err)
+		ps1 = ">"
+		interpret(src)
+	} else {
+		ps1 = ""
+		interpret(os.Stdin)
+	}
+}
+
+func interpret(in io.Reader) {
+	scanner := bufio.NewScanner(in)
+	for scanner.Scan() {
+		safecall(scanner.Text())
+	}
+	check(scanner.Err())
+}
+
+func safecall(code string) {
+	if code == "" {
+		return
+	}
+	defer func() {
+		err := recover()
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "panic:", err)
+		}
+	}()
+	tree, err := world.Compile(code)
+	if err == nil {
+		ret := tree.Eval()
+		if ret != nil {
+			fmt.Println(ret)
+		}
+	} else {
+		fmt.Fprintln(os.Stderr, err)
+	}
+
+}
+
+func check(e error) {
+	if e != nil {
+		fmt.Fprintln(os.Stderr, e)
+		os.Exit(1)
+	}
+}

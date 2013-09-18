0bdc2997a9ce48d85cd0549f02e38670f5c0f58e
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-18 10:49:12 +0200

    remove fftpower

--- engine/fftm.go
+++ engine/fftm.go
@@ -37,35 +37,4 @@ func (f *fftm) NComp() int       { return 3 }
 func (f *fftm) Name() string     { return "mFFT" }
 func (f *fftm) Unit() string     { return "" }
 func (f *fftm) Mesh() *data.Mesh { return &demagConv().FFTMesh }
-func (f *fftm) Save()            { Save(f) } // rm
-
-//type fftmPower struct{}
-//
-//// Power of FFTM, used for display in web interface.
-//// Frequencies in y shifted to center at 0/m.
-//func (q *fftmPower) Get() (*data.Slice, bool) {
-//	fftm, _ := FFTM.Get()
-//	n := fftm.Mesh().Size()
-//	c := fftm.Mesh().CellSize()
-//	m1 := data.NewMesh(n[0], n[1], n[2]/2, c[0], c[1], c[2])
-//	n1 := n[1]
-//	n12 := n1 / 2
-//	m1.Unit = fftm.Mesh().Unit
-//	power := data.NewSlice(3, m1)
-//	f := fftm.Vectors()
-//	p := power.Vectors()
-//	for i := range p[0] {
-//		for j := range p[0][i] {
-//			for k := range p[0][i][j] {
-//				p[0][i][j][k] = sqrt(sqr(f[0][i][(j+n12)%n1][2*k]) + sqr(f[0][i][(j+n12)%n1][2*k+1]))
-//				p[1][i][j][k] = sqrt(sqr(f[1][i][(j+n12)%n1][2*k]) + sqr(f[1][i][(j+n12)%n1][2*k+1]))
-//				p[2][i][j][k] = sqrt(sqr(f[2][i][(j+n12)%n1][2*k]) + sqr(f[2][i][(j+n12)%n1][2*k+1]))
-//			}
-//		}
-//	}
-//	return power, false
-//}
-//
-//
-//func sqr(x float32) float32  { return x * x }
-//func sqrt(x float32) float32 { return float32(math.Sqrt(float64(x))) }
+func (f *fftm) Save()            { Save(f) } // TODO: rm

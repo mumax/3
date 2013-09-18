159b0bc2fb8dd1877bebcdf2677973c3b2ebcf4f
Arne Vansteenkiste <arne@porteus.example.net>
2013-09-18 19:54:44 +0200

    gui: fix textbox widths

--- gui/textbox.go
+++ gui/textbox.go
@@ -7,19 +7,19 @@ import (
 // {{.Textbox id value}} adds a textbox to the document.
 // value is the initial text in the box.
 func (t *Templ) TextBox(id string, value string) string {
-	return t.textbox("text", id, nil, value, "")
+	return t.textbox("text", id, nil, value, "size=12")
 }
 
 // {{.Numbox id value}} adds a textbox for numbers to the document.
 // value is the initial text in the box.
 func (t *Templ) NumBox(id string, value float64) string {
-	return t.textbox("text", id, &floatData{value}, value, "size=10")
+	return t.textbox("text", id, &floatData{value}, value, "size=12")
 }
 
 // {{.IntBox id value}} adds a textbox for integer numbers to the document.
 // value is the initial text in the box.
 func (t *Templ) IntBox(id string, value int) string {
-	return t.textbox("number", id, &intData{value}, value, "size=10")
+	return t.textbox("number", id, &intData{value}, value, "min=0 max=99999") // hack to get width right
 }
 
 // general textbox with data model (nil = default interfaceData)

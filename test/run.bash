c2ebd67ac4d29a302490a5fb349fb4f57abfbb61
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-23 13:23:48 +0200

    tests pass

--- test/run.bash
+++ test/run.bash
@@ -2,10 +2,10 @@
 
 scripts=*.txt
 
-mx3 -vet $scripts || exit 1;
+../3 -vet $scripts || exit 1;
 
 for f in $scripts; do
-	mx3 -f -http "" $f || exit 1;
+	../3 -f -http "" $f || exit 1;
 	echo ""
 done;
 

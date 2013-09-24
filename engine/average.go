fe65b266a37a8c41a5aefa1c6dc0612f5a55c286
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-24 09:38:48 +0200

    average: take into account magnet volume

---
+++ engine/average.go
@@ -0,0 +1,38 @@
+package engine
+
+import (
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/data"
+	"github.com/mumax/3/util"
+)
+
+func init() {
+	DeclFunc("average", Average, "Average of space-dependent quantity over magnet volume")
+}
+
+// average in userspace XYZ order
+// does not yet take into account volume.
+// pass volume parameter, possibly nil?
+func Average(s Slicer) []float64 {
+	b, recycle := s.Slice()
+	if recycle {
+		defer cuda.Recycle(b)
+	}
+	return avg(b)
+}
+
+// userspace average
+func avg(b *data.Slice) []float64 {
+	nComp := b.NComp()
+	nCell := float64(b.Mesh().NCell())
+	avg := make([]float64, nComp)
+	for i := range avg {
+		I := util.SwapIndex(i, nComp)
+		if vol.IsNil() {
+			avg[i] = float64(cuda.Sum(b.Comp(I))) / nCell
+		} else {
+			avg[i] = float64(cuda.Dot(b.Comp(I), vol)) / (spaceFill * nCell)
+		}
+	}
+	return avg
+}

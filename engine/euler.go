d5edc7d5910cee06dece69d679863506d2d00a2a
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-09-27 11:17:57 +0200

    add euler solver

---
+++ engine/euler.go
@@ -0,0 +1,24 @@
+package engine
+
+import (
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/util"
+)
+
+// can be used as solver.step
+func EulerStep(e *solver) {
+	dy0 := cuda.Buffer(3, e.y.Mesh())
+	defer cuda.Recycle(dy0)
+
+	dt := float32(e.Dt_si * e.dt_mul)
+	util.Assert(dt > 0)
+
+	e.torqueFn(dy0)
+	e.NEval++
+
+	cuda.Madd2(e.y, e.y, dy0, 1, dt) // y = y + dt * dy
+
+	Time += e.Dt_si
+	e.postStep(e.y)
+	e.NSteps++
+}

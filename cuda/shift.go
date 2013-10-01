125ce57fbe3284521125b61c44899c04a614f2d1
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-10-01 13:03:58 +0200

    implement shiftbytes

--- cuda/shift.go
+++ cuda/shift.go
@@ -17,3 +17,11 @@ func Shift(dst, src *data.Slice, shift [3]int) {
 
 	k_shift(dst.DevPtr(0), src.DevPtr(0), N[0], N[1], N[2], shift[0], shift[1], shift[2], cfg)
 }
+
+// Like Shift, but for bytes
+func ShiftBytes(dst, src *Bytes, m *data.Mesh, shift [3]int) {
+	N := m.Size()
+	cfg := make3DConf(N)
+
+	k_shiftbytes(dst.Ptr, src.Ptr, N[0], N[1], N[2], shift[0], shift[1], shift[2], cfg)
+}
---
+++ cuda/shiftbytes.cu
@@ -0,0 +1,20 @@
+#include "stencil.h"
+
+// Copies src to dst, shifting elements by (u, v, w).
+// Clamps at the boundaries.
+extern "C" __global__ void
+shiftbytes(int8_t* __restrict__  dst, int8_t* __restrict__  src,
+           int N0,  int N1,  int N2,
+           int sh0, int sh1, int sh2) {
+
+    int i = blockIdx.z * blockDim.z + threadIdx.z;
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    int k = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if(i >= N0 || j>=N1 || k>=N2) {
+        return;	// out of  bounds
+    }
+
+    dst[idx(i, j, k)] = src[idxclamp(i-sh0, j-sh1, k-sh2)];
+}
+

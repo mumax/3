19b2d11b1ef6560fd5aefb4beb32c437d5a0f594
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-04 16:49:41 +0200

    prepare draw to re-utilize image buffer

--- draw/image.go
+++ draw/image.go
@@ -10,12 +10,19 @@ import (
 
 // Renders an image of slice. fmin, fmax = "auto" or a number to set the min/max color scale.
 func Image(f *data.Slice, fmin, fmax string) *image.NRGBA {
+	img := new(image.NRGBA)
+	On(img, f, fmin, fmax)
+	return img
+}
+
+// Render on existing image buffer. Resize it if needed
+func On(img *image.NRGBA, f *data.Slice, fmin, fmax string) {
 	dim := f.NComp()
 	switch dim {
 	default:
 		log.Fatalf("unsupported number of components: %v", dim)
 	case 3:
-		return drawVectors(f.Vectors())
+		drawVectors(img, f.Vectors())
 	case 1:
 		min, max := extrema(f.Host()[0])
 		if fmin != "auto" {
@@ -32,18 +39,17 @@ func Image(f *data.Slice, fmin, fmax string) *image.NRGBA {
 			min -= 1
 			max += 1 // make it gray instead of black
 		}
-		return drawFloats(f.Scalars(), min, max)
+		drawFloats(img, f.Scalars(), min, max)
 	}
-	panic("unreachable")
 }
 
 // Draws rank 4 tensor (3D vector field) as image
 // averages data over X (usually thickness of thin film)
-func drawVectors(arr [3][][][]float32) *image.NRGBA {
+func drawVectors(img *image.NRGBA, arr [3][][][]float32) {
 	h, w := len(arr[0][0]), len(arr[0][0][0])
 	d := len(arr[0])
 	norm := float32(d)
-	img := image.NewNRGBA(image.Rect(0, 0, w, h))
+	*img = *recycle(img, w, h)
 	for i := 0; i < h; i++ {
 		for j := 0; j < w; j++ {
 			var x, y, z float32 = 0., 0., 0.
@@ -58,7 +64,6 @@ func drawVectors(arr [3][][][]float32) *image.NRGBA {
 			img.Set(j, (h-1)-i, HSLMap(z, y, x))
 		}
 	}
-	return img
 }
 
 func extrema(data []float32) (min, max float32) {
@@ -77,11 +82,11 @@ func extrema(data []float32) (min, max float32) {
 
 // Draws rank 3 tensor (3D scalar field) as image
 // averages data over X (usually thickness of thin film)
-func drawFloats(arr [][][]float32, min, max float32) *image.NRGBA {
+func drawFloats(img *image.NRGBA, arr [][][]float32, min, max float32) {
 
 	h, w := len(arr[0]), len(arr[0][0])
 	d := len(arr)
-	img := image.NewNRGBA(image.Rect(0, 0, w, h))
+	*img = *recycle(img, w, h)
 
 	for i := 0; i < h; i++ {
 		for j := 0; j < w; j++ {
@@ -93,6 +98,13 @@ func drawFloats(arr [][][]float32, min, max float32) *image.NRGBA {
 			x /= float32(d)
 			img.Set(j, (h-1)-i, GreyMap(min, max, x))
 		}
+	}
+}
+
+// recycle image if it has right size
+func recycle(img *image.NRGBA, w, h int) *image.NRGBA {
+	if img == nil || img.Bounds().Size().X != w || img.Bounds().Size().Y != h {
+		img = image.NewNRGBA(image.Rect(0, 0, w, h))
 	}
 	return img
 }

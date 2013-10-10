7d46c55c75cc1f6bb5c20e6ab236f578bf1f5d50
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-10 16:25:26 +0200

    svg 3D arrows

--- draw/svg.go
+++ draw/svg.go
@@ -12,7 +12,7 @@ func SVG(out io.Writer, arr [3][][][]float32) {
 
 	Mx := arr[2][slice]
 	My := arr[1][slice]
-	//Mz := arr[0][slice]
+	Mz := arr[0][slice]
 
 	h, w := len(Mx), len(Mx[0])
 
@@ -30,11 +30,14 @@ func SVG(out io.Writer, arr [3][][][]float32) {
 		for j := 0; j < w; j++ {
 			x := S*j + S/2
 
-			mx := Mx[i][j]
-			my := My[i][j]
-			theta := math.Atan2(float64(my), float64(mx))
+			mx := float64(Mx[i][j])
+			my := float64(My[i][j])
+			mz := float64(Mz[i][j])
+
+			theta := math.Atan2(my, mx)
 			c := math.Cos(theta)
 			s := math.Sin(theta)
+			r1 := r1 * math.Cos(math.Asin(mz))
 
 			xs := []int{int(r1*c) + x, int(r2*s-r1*c) + x, int(-r2*s-r1*c) + x}
 			ys := []int{int(r1*s) + y, int(-r2*c-r1*s) + y, int(r2*c-r1*s) + y}

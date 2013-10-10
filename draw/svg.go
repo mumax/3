f8ea28ce7de0209eaea87b8f754afdbee60e82f0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-10 16:20:15 +0200

    svg arrows

--- draw/svg.go
+++ draw/svg.go
@@ -3,15 +3,45 @@ package draw
 import (
 	"github.com/ajstarks/svgo"
 	"io"
+	"math"
 )
 
 func SVG(out io.Writer, arr [3][][][]float32) {
 
-	h, w := len(arr[0][0]), len(arr[0][0][0])
+	slice := 0 // todo
+
+	Mx := arr[2][slice]
+	My := arr[1][slice]
+	//Mz := arr[0][slice]
+
+	h, w := len(Mx), len(Mx[0])
+
+	const (
+		S  = 100 // scale
+		r1 = S / 2
+		r2 = S / 4
+	)
 
 	canvas := svg.New(out)
-	canvas.Start(w, h)
-	canvas.Circle(w/2, h/2, 100)
-	canvas.Text(w/2, h/2, "Hello, SVG", "text-anchor:middle;font-size:30px;fill:white")
+	canvas.Start(S*w, S*h)
+
+	for i := 0; i < h; i++ {
+		y := S*i + S/2
+		for j := 0; j < w; j++ {
+			x := S*j + S/2
+
+			mx := Mx[i][j]
+			my := My[i][j]
+			theta := math.Atan2(float64(my), float64(mx))
+			c := math.Cos(theta)
+			s := math.Sin(theta)
+
+			xs := []int{int(r1*c) + x, int(r2*s-r1*c) + x, int(-r2*s-r1*c) + x}
+			ys := []int{int(r1*s) + y, int(-r2*c-r1*s) + y, int(r2*c-r1*s) + y}
+
+			canvas.Polygon(xs, ys)
+		}
+	}
+
 	canvas.End()
 }

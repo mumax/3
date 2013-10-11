2009a0d10d0118f60baaf54b58b052e69b27a597
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-10-11 09:31:46 +0200

    svg: fix colors

--- draw/svg.go
+++ draw/svg.go
@@ -1,24 +1,18 @@
 package draw
 
 import (
+	"fmt"
 	"github.com/ajstarks/svgo"
 	"io"
 	"math"
-	"strconv"
 )
 
 func SVG(out io.Writer, arr [3][][][]float32) {
 
-	slice := 0 // todo
-
-	Mx := arr[2][slice]
-	My := arr[1][slice]
-	Mz := arr[0][slice]
-
-	h, w := len(Mx), len(Mx[0])
+	h, w := len(arr[0][0]), len(arr[0][0][0])
 
 	const (
-		S  = 100 // scale
+		S  = 256 // scale
 		r1 = S / 2
 		r2 = S / 4
 	)
@@ -26,33 +20,48 @@ func SVG(out io.Writer, arr [3][][][]float32) {
 	canvas := svg.New(out)
 	canvas.Start(S*w, S*h)
 
-	for i := 0; i < h; i++ {
-		y := S*i + S/2
-		for j := 0; j < w; j++ {
-			x := S*j + S/2
+	for slice := 0; slice < len(arr[0]); slice++ {
+		Mx := arr[2][slice]
+		My := arr[1][slice]
+		Mz := arr[0][slice]
 
-			mx := Mx[i][j]
-			my := My[i][j]
-			mz := Mz[i][j]
+		for i := 0; i < h; i++ {
+			y := S*i + S/2
+			for j := 0; j < w; j++ {
+				x := S*j + S/2
 
-			if mx*mx+my*my+mz*mz == 0 {
-				continue
+				mx := Mx[i][j]
+				my := My[i][j]
+				mz := Mz[i][j]
+
+				// skip zero-length vectors
+				if mx*mx+my*my+mz*mz == 0 {
+					continue
+				}
+
+				theta := math.Atan2(float64(my), float64(mx))
+				c := math.Cos(theta)
+				s := math.Sin(theta)
+				r1 := r1 * math.Cos(math.Asin(float64(mz)))
+
+				xs := []int{int(r1*c) + x, int(r2*s-r1*c) + x, int(-r2*s-r1*c) + x}
+				ys := []int{int(r1*s) + y, int(-r2*c-r1*s) + y, int(r2*c-r1*s) + y}
+
+				col := HSLMap(mx, my, mz)
+				style := "fill:#" + hex(col.R) + hex(col.G) + hex(col.B)
+
+				canvas.Polygon(xs, ys, style)
 			}
-
-			theta := math.Atan2(float64(my), float64(mx))
-			c := math.Cos(theta)
-			s := math.Sin(theta)
-			r1 := r1 * math.Cos(math.Asin(float64(mz)))
-
-			xs := []int{int(r1*c) + x, int(r2*s-r1*c) + x, int(-r2*s-r1*c) + x}
-			ys := []int{int(r1*s) + y, int(-r2*c-r1*s) + y, int(r2*c-r1*s) + y}
-
-			col := HSLMap(mx, my, mz)
-			style := "stroke:black;fill:#" + strconv.FormatInt(int64(col.R), 16) + strconv.FormatInt(int64(col.G), 16) + strconv.FormatInt(int64(col.B), 16)
-
-			canvas.Polygon(xs, ys, style)
 		}
 	}
 
 	canvas.End()
+}
+
+func hex(i uint8) string {
+	j := int(i) - 32 // make it a bit darker
+	if j < 0 {
+		j = 0
+	}
+	return fmt.Sprintf("%02X", j)
 }

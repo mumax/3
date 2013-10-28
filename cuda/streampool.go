4e1be4441c4a6e2033d438f387a590d8435896fe
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-10-28 10:18:40 +0100

    cuda cleanup

--- cuda/streampool.go
+++ cuda/streampool.go
@@ -1,28 +1,8 @@
 package cuda
 
-// Management of a pool of re-usable CUDA streams.
-
 import "github.com/barnex/cuda5/cu"
 
-var stream [3]cu.Stream
-
-//var streamPool chan cu.Stream // global pool of streams.
-//const streamPoolSize = 64     // number of streams in global pool.
-//
-//// Returns a CUDA stream from a global pool.
-//// After use it should be recycled with SyncAndRecycle.
-//func stream() cu.Stream {
-//	return <-streamPool
-//}
-//
-//// SyncAndRecycle takes a CUDA stream obtained by Stream(),
-//// the stream is synchronized and returned to the pool.
-//// Not recycling streams will cause deadlock quickly as
-//// the pool drains.
-//func syncAndRecycle(str cu.Stream) {
-//	str.Synchronize()
-//	streamPool <- str
-//}
+var stream [3]cu.Stream // 3 general-purpose CUDA streams, one per vector component
 
 func initStreampool() {
 	for i := range stream {

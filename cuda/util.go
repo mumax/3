8e79dbfc832616fd2f2bf0f7e560a52b5df0a807
Arne Vansteenkiste <Arne.Vansteenkiste@UGent.be>
2013-10-31 12:34:29 +0100

    minor cleanup

--- cuda/util.go
+++ cuda/util.go
@@ -18,10 +18,10 @@ type config struct {
 func make1DConf(N int) *config {
 	bl := cu.Dim3{X: BlockSize, Y: 1, Z: 1}
 
-	N2 := divUp(N, BlockSize) // N2 blocks left
-	NX := divUp(N2, MaxGridSize)
-	NY := divUp(N2, NX)
-	gr := cu.Dim3{X: NX, Y: NY, Z: 1}
+	n2 := divUp(N, BlockSize) // N2 blocks left
+	nx := divUp(n2, MaxGridSize)
+	ny := divUp(n2, nx)
+	gr := cu.Dim3{X: nx, Y: ny, Z: 1}
 
 	return &config{gr, bl}
 }
@@ -30,12 +30,10 @@ func make1DConf(N int) *config {
 func make3DConf(N [3]int) *config {
 	bl := cu.Dim3{X: TileX, Y: TileY, Z: 1}
 
-	NZ := N[Z]
-	NY := divUp(N[Y], TileY)
-	NX := divUp(N[X], TileX)
-	gr := cu.Dim3{X: NX, Y: NY, Z: NZ}
+	nx := divUp(N[X], TileX)
+	ny := divUp(N[Y], TileY)
+	gr := cu.Dim3{X: nx, Y: ny, Z: N[Z]}
 
-	//dbg("3dconf", N, "=", config{gr, bl})
 	return &config{gr, bl}
 }
 
@@ -52,7 +50,6 @@ func divUp(x, y int) int {
 	return ((x - 1) / y) + 1
 }
 
-// vector components
 const (
 	X = 0
 	Y = 1

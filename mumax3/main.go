f03095b74f8428051c0422550710dbc2753f1225
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-07 15:46:51 +0100

    remove obsolete main flags

--- mumax3/main.go
+++ mumax3/main.go
@@ -14,20 +14,15 @@ import (
 )
 
 var (
-	flag_silent   = flag.Bool("s", false, "Don't generate any log info")
-	flag_vet      = flag.Bool("vet", false, "Check input files for errors, but don't run them")
-	flag_od       = flag.String("o", "", "Override output directory")
-	flag_force    = flag.Bool("f", false, "Force start, clean existing output directory")
-	flag_port     = flag.String("http", ":35367", "Port to serve web gui")
-	flag_cpuprof  = flag.Bool("cpuprof", false, "Record gopprof CPU profile")
-	flag_memprof  = flag.Bool("memprof", false, "Recored gopprof memory profile")
-	flag_blocklen = flag.Int("bl", 512, "CUDA 1D thread block length")
-	flag_blockX   = flag.Int("bx", 32, "CUDA 2D thread block size X")
-	flag_blockY   = flag.Int("by", 32, "CUDA 2D thread block size Y")
-	flag_gpu      = flag.Int("gpu", 0, "specify GPU")
-	flag_sched    = flag.String("sched", "yield", "CUDA scheduling: auto|spin|yield|sync")
-	flag_sync     = flag.Bool("sync", false, "synchronize all CUDA calls (debug)")
-	//flag_pagelock = flag.Bool("pagelock", true, "enable CUDA memeory page-locking")
+	flag_silent  = flag.Bool("s", false, "Don't generate any log info")
+	flag_vet     = flag.Bool("vet", false, "Check input files for errors, but don't run them")
+	flag_od      = flag.String("o", "", "Override output directory")
+	flag_force   = flag.Bool("f", false, "Force start, clean existing output directory")
+	flag_port    = flag.String("http", ":35367", "Port to serve web gui")
+	flag_cpuprof = flag.Bool("cpuprof", false, "Record gopprof CPU profile")
+	flag_memprof = flag.Bool("memprof", false, "Recored gopprof memory profile")
+	flag_gpu     = flag.Int("gpu", 0, "specify GPU")
+	flag_sync    = flag.Bool("sync", false, "synchronize all CUDA calls (debug)")
 )
 
 func main() {
@@ -65,10 +60,10 @@ func main() {
 	}
 
 	runtime.GOMAXPROCS(runtime.NumCPU())
-	cuda.BlockSize = *flag_blocklen
-	cuda.TileX = *flag_blockX
-	cuda.TileY = *flag_blockY
-	cuda.Init(*flag_gpu, *flag_sched, *flag_sync)
+	cuda.BlockSize = 512
+	cuda.TileX = 32
+	cuda.TileY = 32
+	cuda.Init(*flag_gpu, "yield", *flag_sync)
 	cuda.LockThread()
 
 	if *flag_cpuprof {

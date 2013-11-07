65fb6402e3734f57388a6ad6b32da6e79f1087b8
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-07 10:35:30 +0100

    cleanup demag

--- engine/demag.go
+++ engine/demag.go
@@ -12,43 +12,46 @@ var (
 	Bsat        derivedParam
 	M_full      setter
 	B_demag     setter
-	E_demag     = NewGetScalar("E_demag", "J", "Magnetostatic energy", getDemagEnergy)
+	E_demag     *GetScalar
 	EnableDemag = true                 // enable/disable demag field
 	demagconv_  *cuda.DemagConvolution // does the heavy lifting and provides FFTM
 )
 
 func init() {
 	Msat.init("Msat", "A/m", "Saturation magnetization", []derived{&Bsat, &lex2, &ku1_red, &kc1_red, &temp_red})
-
-	M_full.init(VECTOR, &globalmesh, "m_full", "A/m", "Unnormalized magnetization", func(dst *data.Slice) {
-		msat, r := Msat.Slice()
-		if r {
-			defer cuda.Recycle(msat)
-		}
-		for c := 0; c < 3; c++ {
-			cuda.Mul(dst.Comp(c), M.buffer.Comp(c), msat)
-		}
-	})
-
+	M_full.init(VECTOR, &globalmesh, "m_full", "A/m", "Unnormalized magnetization", SetMFull)
 	DeclVar("EnableDemag", &EnableDemag, "Enables/disables demag (default=true)")
+	B_demag.init(VECTOR, &globalmesh, "B_demag", "T", "Magnetostatic field", SetDemagField)
+	E_demag = NewGetScalar("E_demag", "J", "Magnetostatic energy", getDemagEnergy)
+	registerEnergy(getDemagEnergy)
 
+	//Bsat = Msat * mu0
 	Bsat.init(SCALAR, []updater{&Msat}, func(p *derivedParam) {
-		//Bsat = Msat * mu0
 		Ms := Msat.cpuLUT()
 		for i, ms := range Ms[0] {
 			p.cpu_buf[0][i] = mag.Mu0 * ms
 		}
 	})
+}
 
-	B_demag.init(VECTOR, &globalmesh, "B_demag", "T", "Magnetostatic field", func(b *data.Slice) {
-		if EnableDemag {
-			demagConv().Exec(b, M.buffer, vol(), Bsat.gpuLUT1(), regions.Gpu())
-		} else {
-			cuda.Zero(b) // will ADD other terms to it
-		}
-	})
+// Sets dst to the current demag field
+func SetDemagField(dst *data.Slice) {
+	if EnableDemag {
+		demagConv().Exec(dst, M.buffer, vol(), Bsat.gpuLUT1(), regions.Gpu())
+	} else {
+		cuda.Zero(dst) // will ADD other terms to it
+	}
+}
 
-	registerEnergy(getDemagEnergy)
+// Sets dst to the full (unnormalized) magnetization in A/m
+func SetMFull(dst *data.Slice) {
+	msat, r := Msat.Slice()
+	if r {
+		defer cuda.Recycle(msat)
+	}
+	for c := 0; c < 3; c++ {
+		cuda.Mul(dst.Comp(c), M.buffer.Comp(c), msat)
+	}
 }
 
 // returns demag convolution, making sure it's initialized

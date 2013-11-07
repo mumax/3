fda59560534d9db25d0bcb92a8304e359b7aa902
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-07 11:23:43 +0100

    cleanup zeeman

--- engine/zeeman.go
+++ engine/zeeman.go
@@ -2,11 +2,12 @@ package engine
 
 var (
 	B_ext    excitation
-	E_Zeeman = NewGetScalar("E_Zeeman", "J", "Zeeman energy", getZeemanEnergy)
+	E_Zeeman *GetScalar
 )
 
 func init() {
 	B_ext.init("B_ext", "T", "Externally applied field")
+	E_Zeeman = NewGetScalar("E_Zeeman", "J", "Zeeman energy", getZeemanEnergy)
 	registerEnergy(getZeemanEnergy)
 }
 

a2eb5a0e2902ea183e9adcdee2b9e90d7d77cd24
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-20 08:00:26 +0100

    gui: remove Span & Button varargs

--- gui/span.go
+++ gui/span.go
@@ -5,13 +5,12 @@ import (
 )
 
 // {{.Span id value}} adds a piece of text ("label") to the document.
-func (t *Templ) Span(id string, value ...string) string {
+func (t *Templ) Span(id string, value string) string {
 	d := (*Doc)(t)
-	val := cat(value)
 	el := d.addElem(id)
-	el.setValue(val)
+	el.setValue(value)
 	el.update = func(id string) jsCall {
 		return jsCall{F: "setAttr", Args: []interface{}{id, "innerHTML", el.value()}}
 	}
-	return fmt.Sprintf(`<span id=%v>%v</span>`, id, val)
+	return fmt.Sprintf(`<span id=%v>%v</span>`, id, value)
 }

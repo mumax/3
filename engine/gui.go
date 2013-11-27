0933fb1d1e35b9980b9a2a196e26ff6f1bfb3588
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-27 15:56:28 +0100

    fix steps / cachebreaker race condition

--- engine/gui.go
+++ engine/gui.go
@@ -73,14 +73,16 @@ func (g *guistate) PrepareServer() {
 	GUI.OnUpdate(func() {
 		updateKeepAlive() // keep track of when browser was last seen alive
 
-		// solver
-		GUI.Set("steps", Solver.NSteps)
+		InjectAndWait(func() {
+			// solver
+			GUI.Set("steps", Solver.NSteps)
 
-		// display
-		quant := GUI.StringValue("renderQuant")
-		comp := GUI.StringValue("renderComp")
-		cachebreaker := "?" + GUI.StringValue("steps") + "_" + GUI.StringValue("renderScale") // scale needed if we zoom while paused
-		GUI.Set("display", "/render/"+quant+"/"+comp+cachebreaker)
+			// display
+			quant := GUI.StringValue("renderQuant")
+			comp := GUI.StringValue("renderComp")
+			cachebreaker := "?" + GUI.StringValue("steps") + "_" + GUI.StringValue("renderScale") // scale needed if we zoom while paused
+			GUI.Set("display", "/render/"+quant+"/"+comp+cachebreaker)
+		})
 	})
 }
 

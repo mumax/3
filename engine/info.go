5037b80828235b395e0e8c83dcf4ea2364419e7c
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2013-11-28 09:08:32 +0100

    remove mesh from info to prepare for consitent mesh checks

--- engine/info.go
+++ engine/info.go
@@ -1,30 +1,14 @@
 package engine
 
-import "github.com/mumax/3/data"
-
-type descr struct {
-	name, unit string
-}
-
-type doc struct {
-	nComp int // number of components (scalar, vector, ...)
-	descr
-}
-
-func Doc(nComp int, name, unit string) doc {
-	return doc{nComp, descr{name, unit}}
+func Info(nComp int, name, unit string) info {
+	return info{nComp: nComp, name: name, unit: unit}
 }
 
 type info struct {
-	doc
-	mesh *data.Mesh // nil means use global mesh
+	nComp      int // number of components (scalar, vector, ...)
+	name, unit string
 }
 
-func Info(nComp int, name, unit string, m *data.Mesh) info {
-	return info{doc{nComp, descr{name, unit}}, m}
-}
-
-func (d *descr) Name() string    { return d.name }
-func (d *descr) Unit() string    { return d.unit }
-func (i *doc) NComp() int        { return i.nComp }
-func (i *info) Mesh() *data.Mesh { return i.mesh }
+func (i *info) Name() string { return i.name }
+func (i *info) Unit() string { return i.unit }
+func (i *info) NComp() int   { return i.nComp }

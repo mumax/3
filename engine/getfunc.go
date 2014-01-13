ba0d47c386a204239b04a7605354134b4b9d818e
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-01-13 12:04:08 +0100

    fixup getfuncs, corepos test

--- engine/getfunc.go
+++ engine/getfunc.go
@@ -1,12 +1,17 @@
 package engine
 
+import (
+	"github.com/mumax/3/data"
+)
+
+// wraps a func to make it a quantity
 // unifies getScalar and getVector
 type getFunc struct {
 	info
 	get func() []float64
 }
 
-func (g *getFunc) TableData() []float64 { return g.get() }
+func (g *getFunc) average() []float64 { return g.get() }
 
 func newGetfunc_(nComp int, name, unit, doc_ string, get func() []float64) getFunc {
 	return getFunc{Info(nComp, name, unit), get}
@@ -15,8 +20,11 @@ func newGetfunc_(nComp int, name, unit, doc_ string, get func() []float64) getFu
 type GetScalar struct{ getFunc }
 type GetVector struct{ getFunc }
 
-func (g *GetScalar) Get() float64    { return g.get()[0] }
-func (g *GetVector) Get() [3]float64 { return unslice(g.get()) }
+func (g *GetScalar) Get() float64     { return g.get()[0] }
+func (g *GetScalar) Average() float64 { return g.Get() }
+
+func (g *GetVector) Get() data.Vector     { return unslice(g.get()) }
+func (g *GetVector) Average() data.Vector { return g.Get() }
 
 // INTERNAL
 func NewGetScalar(name, unit, doc string, get func() float64) *GetScalar {

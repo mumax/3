302b6a3247bb8d20ed833f15c6405543d55ccb78
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-01-20 15:29:16 +0100

    fix examples

--- make.bash
+++ make.bash
@@ -9,5 +9,5 @@ ln -sf $(pwd)/post-commit .git/hooks/post-commit
 #(cd tools/mumax3-convert && go build && go install) || exit 1
 #(cd tools/mumax3-plot && go build && go install)    || exit 1
 (cd test && mumax3 -vet *.txt)                   || exit 1
-(cd doc && mumax3 -vet *.txt)               || exit 1
+(cd doc && mumax3 -vet *.in)               || exit 1
 

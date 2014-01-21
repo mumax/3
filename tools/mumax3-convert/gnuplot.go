6bb623b2ea5c97f1e07892d24b482feb8c3f04b8
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-01-21 11:35:17 +0100

    fix mumax3-convert gnuplot output

--- tools/mumax3-convert/gnuplot.go
+++ tools/mumax3-convert/gnuplot.go
@@ -1,7 +1,6 @@
 package main
 
 // Output for gnuplot's "splot"
-// Author: Arne Vansteenkiste
 
 import (
 	"bufio"
@@ -15,7 +14,6 @@ func dumpGnuplot(out io.Writer, f *data.Slice, m data.Meta) (err error) {
 	defer buf.Flush()
 
 	data := f.Tensors()
-	gridsize := f.Size()
 	cellsize := m.CellSize
 	// If no cell size is set, use generic cell index.
 	if cellsize == [3]float64{0, 0, 0} {
@@ -23,17 +21,15 @@ func dumpGnuplot(out io.Writer, f *data.Slice, m data.Meta) (err error) {
 	}
 	ncomp := f.NComp()
 
-	// Here we loop over X,Y,Z, not Z,Y,X, because
-	// internal in C-order == external in Fortran-order
-	for i := 0; i < gridsize[0]; i++ {
-		x := float64(i) * cellsize[0]
-		for j := 0; j < gridsize[1]; j++ {
-			y := float64(j) * cellsize[1]
-			for k := 0; k < gridsize[2]; k++ {
-				z := float64(k) * cellsize[2]
-				_, err = fmt.Fprint(buf, z, " ", y, " ", x, "\t")
+	for iz := range data[0] {
+		z := float64(iz) * cellsize[Z]
+		for iy := range data[0][iz] {
+			y := float64(iy) * cellsize[Y]
+			for ix := range data[0][iz][iy] {
+				x := float64(ix) * cellsize[X]
+				_, err = fmt.Fprint(buf, x, " ", y, " ", z, "\t")
 				for c := 0; c < ncomp; c++ {
-					_, err = fmt.Fprint(buf, data[c][i][j][k], " ") // converts to user space.
+					_, err = fmt.Fprint(buf, data[c][iz][iy][ix], " ")
 				}
 				_, err = fmt.Fprint(buf, "\n")
 			}

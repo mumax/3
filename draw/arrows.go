659a6e42caf61f5dd4515ffb192ba730dba8f60d
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-02-21 11:36:46 +0100

    stash draw/arrows.go

--- draw/arrows.go
+++ draw/arrows.go
@@ -1,11 +1,13 @@
 package draw
 
 import (
+	"github.com/mumax/3/data"
 	"image"
 )
 
-func drawArrows(img *image.NRGBA, arr [3][][][]float32) {
-
+func drawArrows(img *image.NRGBA, arr [3][][][]float32, subsample int) {
+	//smaller := data.SizeOf(arr[0])
+	//in := data.Downsample(arr[:], smaller)
 	//	w, h := len(arr[X][0][0]), len(arr[X][0])
 	//	d := len(arr[X])
 	//	norm := float32(d)

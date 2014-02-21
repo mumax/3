mumax/3

commit 88b578ab2b0cd8894500ba7589a44963a8cbaf9a

Update make.bash

cuda/make.bash
@@ -5,7 +5,7 @@ go build cuda2go.go || exit 1
 NVCC='nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx'
 
 for f in *.cu; do
-	g=$(basename -s .cu $f)
+ g=$(echo $f | sed s/\.cu//)
 	for cc in 20 30 35; do
 		if [[ $f -nt $g'_'$cc.ptx ]]; then
 			echo $NVCC -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx

commit 19e2ed8663c66dac5f5f2ac685f48802ba9550bf

cuda/make.bash: fix wrong use of sed introduced in 88b578ab2b0cd8894500ba7589a44963a8cbaf9a

cuda/make.bash
@@ -5,7 +5,7 @@ go build cuda2go.go || exit 1
 NVCC='nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx'
 
 for f in *.cu; do
-	g=$(echo $f | sed s/\.cu//)
+	g=$(echo $f | sed 's/\.cu$//') # file basename
 	for cc in 20 30 35; do
 		if [[ $f -nt $g'_'$cc.ptx ]]; then
 			echo $NVCC -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx
@@ -13,7 +13,6 @@ for f in *.cu; do
 		fi
 	done
 	if [[ $f -nt $g'_wrapper.go' ]]; then
-		astyle $f || echo " "
 		./cuda2go $f || exit 1
 	fi
 done
b4f4d49d4dfd0b9e2d49978403fc9291bf598dc0
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-04 10:13:51 +0100

    automated output format/mumax3-convert test

--- test/run.bash
+++ test/run.bash
@@ -1,21 +1,22 @@
 #! /bin/bash
 
+set -e
+
 scripts=*.txt
 
-mumax3 -vet $scripts || exit 1;
+mumax3 -vet $scripts
+
+mumax3 outputformat.txt
+mumax3-convert -png testdata/*.omf outputformat.out/*.ovf
 
 for g in *.go; do
-	go run $g || exit 1;
+	go run $g;
 done
 
 time (
 for f in $scripts; do
-	mumax3 -f -http "" $f || exit 1;
+	mumax3 -f -http "" $f;
 	echo ""
 done;)
 
-mumax3-convert -png testdata/*.ovf testdata/*.dump
-
-#	mumax3 -sync -f -http "" $f || exit 1;
-#	mumax3 -bx 16 -by 16 -bl 1024  -f -http "" $f || exit 1;
 

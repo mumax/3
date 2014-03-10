058fdac7f0b4e452556e4fe41e8f603f343b1bd6
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-10 11:31:29 +0100

    mark v3.5 alpha 1

--- engine/engine.go
+++ engine/engine.go
@@ -6,7 +6,7 @@ import (
 	"time"
 )
 
-const VERSION = "mumax3.5 BETA"
+const VERSION = "mumax3.5 ALPHA1"
 
 var UNAME = VERSION + " " + runtime.GOOS + "_" + runtime.GOARCH + " " + runtime.Version() + " (" + runtime.Compiler + ")"
 

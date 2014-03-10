c9c52bdaf44cdb21e0e6eae30d3cd180e4ef2029
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-10 09:39:31 +0100

    rk23: allow resize

--- engine/rk23.go
+++ engine/rk23.go
@@ -16,7 +16,6 @@ import (
 // 	y{n+1}  = yn + 2/9 h k1 + 1/3 h k2 + 4/9 h k3            // 3rd order
 // 	k4 = f(tn + h, y{n+1})
 // 	z{n+1} = yn + 7/24 h k1 + 1/4 h k2 + 1/3 h k3 + 1/8 h k4 // 2nd order
-
 type RK23 struct {
 	k1 *data.Slice // torque at end of step is kept for beginning of next step
 }
@@ -27,6 +26,12 @@ func (rk *RK23) Step(unused *data.Slice) {
 
 	if FixDt != 0 {
 		Dt_si = FixDt
+	}
+
+	// upon resize: remove wrongly sized k1
+	if rk.k1.Size() != m.Size() {
+		rk.k1.Free()
+		rk.k1 = nil // will be re-allocated
 	}
 
 	// first step ever: one-time k1 init and eval
@@ -106,82 +111,3 @@ func madd4(dst, src1, src2, src3, src4 *data.Slice, w1, w2, w3, w4 float32) {
 	cuda.Madd3(dst, src1, src2, src3, w1, w2, w3)
 	cuda.Madd2(dst, dst, src4, 1, w4)
 }
-
-//func RK23Step(s *solver, y *data.Slice) {
-//
-//	h := float32(s.Dt_si * *s.dt_mul)
-//	util.Assert(dt > 0)
-//	N := y.Size()
-//
-//	k1 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(k1)
-//
-//	y2 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(y2)
-//	k2 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(k2)
-//
-//	y3 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(y3)
-//	k3 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(k3)
-//
-//
-//	// stage 1
-//	t0 := Time
-//	s.torqueFn(k1);  s.NEval++
-//
-//	// stage 2
-//	cuda.Madd2(y2, y, k1, 1, 0.5 * h) // y2 = 1 y + (0.5 * h) k1
-//	Time = t0 + 0.5 * h
-//	s.torqueFn(k2); s.NEval++
-//
-//	// stage 3
-//	cuda.Madd2(y3, y, k2, 1, 3./4. * h) // y3 = 1 y + (3/4 * h) k2
-//	Time = t0 + 3./4. * h
-//	s.torqueFn(k3); s.NEval++
-//
-//	// low-order torque
-//	kLow := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(kLow)
-//	cuda.Madd3(kLow, k1, k2, k3, 2./9., 1./3., 4./9)
-//	yLow := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(yLow)
-//	cuda.Madd3(yLow, y
-//
-//
-//	k4 := cuda.Buffer(VECTOR, N)
-//	defer cuda.Recycle(k4)
-//
-//
-//	// stage 2
-//	dy := cuda.Buffer(3, y.Size())
-//	defer cuda.Recycle(dy)
-//	Time += s.Dt_si
-//	s.torqueFn(dy)
-//	s.NEval++
-//
-//	// determine error
-//	err := 0.0
-//	if s.FixDt == 0 { // time step not fixed
-//		err = cuda.MaxVecDiff(dy0, dy) * float64(dt)
-//	}
-//
-//	// adjust next time step
-//	if err < s.MaxErr || s.Dt_si <= s.MinDt { // mindt check to avoid infinite loop
-//		// step OK
-//		cuda.Madd3(y, y, dy, dy0, 1, 0.5*dt, -0.5*dt)
-//		s.postStep()
-//		s.NSteps++
-//		s.adaptDt(math.Pow(s.MaxErr/err, 1./2.))
-//		s.LastErr = err
-//	} else {
-//		// undo bad step
-//		util.Assert(s.FixDt == 0)
-//		Time -= s.Dt_si
-//		cuda.Madd2(y, y, dy0, 1, -dt)
-//		s.NUndone++
-//		s.adaptDt(math.Pow(s.MaxErr/err, 1./3.))
-//	}
-//
-//}

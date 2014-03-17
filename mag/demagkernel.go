6eac4dddb4de6f4a897ff47c77d6659a05926d7a
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-17 14:41:43 +0100

    parallelize demag kernel over components, 3x speed-up

--- mag/demagkernel.go
+++ mag/demagkernel.go
@@ -52,116 +52,125 @@ func DemagKernel(inputSize, pbc [3]int, cellsize [3]float64, accuracy float64) (
 	// Start brute integration
 	// 9 nested loops, does that stress you out?
 	// Fortunately, the 5 inner ones usually loop over just one element.
-	var (
-		R, R2  [3]float64 // field and source cell center positions
-		pole   [3]float64 // position of point charge on the surface
-		points int        // counts used integration points
-	)
-	progress, progmax := 0, (1+r2[Y]-r1[Y])*(1+r2[Z]-r1[Z])
+	progress, progmax := 0, (1+(r2[Y]-r1[Y])/2)*(1+(r2[Z]-r1[Z])/2)
 
+	done := make(chan struct{}, 3)
 	for s := 0; s < 3; s++ { // source index Ksdxyz
-		u, v, w := s, (s+1)%3, (s+2)%3 // u = direction of source (s), v & w are the orthogonal directions
+		go func(s int) {
+			u, v, w := s, (s+1)%3, (s+2)%3 // u = direction of source (s), v & w are the orthogonal directions
+			var (
+				R, R2  [3]float64 // field and source cell center positions
+				pole   [3]float64 // position of point charge on the surface
+				points int        // counts used integration points
+			)
 
-		for z := r1[Z]; z <= r2[Z]; z++ {
-			zw := wrap(z, size[Z])
-			R[Z] = float64(z) * cellsize[Z]
-			// skip one half, reconstruct from symmetry later
-			// check on wrapped index instead of loop range so it also works for PBC
-			if zw > size[Z]/2 {
-				continue
-			}
-
-			for y := r1[Y]; y <= r2[Y]; y++ {
-				progress++
-				util.Progress(progress, progmax, "Calculating demag kernel")
-
-				yw := wrap(y, size[Y])
-				R[Y] = float64(y) * cellsize[Y]
-				if yw > size[Y]/2 {
+			for z := r1[Z]; z <= r2[Z]; z++ {
+				zw := wrap(z, size[Z])
+				R[Z] = float64(z) * cellsize[Z]
+				// skip one half, reconstruct from symmetry later
+				// check on wrapped index instead of loop range so it also works for PBC
+				if zw > size[Z]/2 {
 					continue
 				}
 
-				for x := r1[X]; x <= r2[X]; x++ {
-					xw := wrap(x, size[X])
-					R[X] = float64(x) * cellsize[X]
-					if xw > size[X]/2 {
+				for y := r1[Y]; y <= r2[Y]; y++ {
+
+					yw := wrap(y, size[Y])
+					R[Y] = float64(y) * cellsize[Y]
+					if yw > size[Y]/2 {
 						continue
 					}
-
-					// choose number of integration points depending on how far we are from source.
-					dx, dy, dz := delta(x)*cellsize[X], delta(y)*cellsize[Y], delta(z)*cellsize[Z]
-					d := math.Sqrt(dx*dx + dy*dy + dz*dz)
-					if d == 0 {
-						d = L
+					if s == 0 {
+						progress++
+						util.Progress(progress, progmax, "Calculating demag kernel")
 					}
-					maxSize := d / accuracy // maximum acceptable integration size
 
-					nv := int(math.Max(cellsize[v]/maxSize, 1) + 0.5)
-					nw := int(math.Max(cellsize[w]/maxSize, 1) + 0.5)
-					nx := int(math.Max(cellsize[X]/maxSize, 1) + 0.5)
-					ny := int(math.Max(cellsize[Y]/maxSize, 1) + 0.5)
-					nz := int(math.Max(cellsize[Z]/maxSize, 1) + 0.5)
-					// Stagger source and destination grids.
-					// Massively improves accuracy, see note.
-					nv *= 2
-					nw *= 2
+					for x := r1[X]; x <= r2[X]; x++ {
+						xw := wrap(x, size[X])
+						R[X] = float64(x) * cellsize[X]
+						if xw > size[X]/2 {
+							continue
+						}
 
-					util.Assert(nv > 0 && nw > 0 && nx > 0 && ny > 0 && nz > 0)
+						// choose number of integration points depending on how far we are from source.
+						dx, dy, dz := delta(x)*cellsize[X], delta(y)*cellsize[Y], delta(z)*cellsize[Z]
+						d := math.Sqrt(dx*dx + dy*dy + dz*dz)
+						if d == 0 {
+							d = L
+						}
+						maxSize := d / accuracy // maximum acceptable integration size
 
-					scale := 1 / float64(nv*nw*nx*ny*nz)
-					surface := cellsize[v] * cellsize[w] // the two directions perpendicular to direction s
-					charge := surface * scale
-					pu1 := cellsize[u] / 2. // positive pole center
-					pu2 := -pu1             // negative pole center
+						nv := int(math.Max(cellsize[v]/maxSize, 1) + 0.5)
+						nw := int(math.Max(cellsize[w]/maxSize, 1) + 0.5)
+						nx := int(math.Max(cellsize[X]/maxSize, 1) + 0.5)
+						ny := int(math.Max(cellsize[Y]/maxSize, 1) + 0.5)
+						nz := int(math.Max(cellsize[Z]/maxSize, 1) + 0.5)
+						// Stagger source and destination grids.
+						// Massively improves accuracy, see note.
+						nv *= 2
+						nw *= 2
 
-					// Do surface integral over source cell, accumulate  in B
-					var B [3]float64
-					for i := 0; i < nv; i++ {
-						pv := -(cellsize[v] / 2.) + cellsize[v]/float64(2*nv) + float64(i)*(cellsize[v]/float64(nv))
-						pole[v] = pv
-						for j := 0; j < nw; j++ {
-							pw := -(cellsize[w] / 2.) + cellsize[w]/float64(2*nw) + float64(j)*(cellsize[w]/float64(nw))
-							pole[w] = pw
+						util.Assert(nv > 0 && nw > 0 && nx > 0 && ny > 0 && nz > 0)
 
-							// Do volume integral over destination cell
-							for α := 0; α < nx; α++ {
-								rx := R[X] - cellsize[X]/2 + cellsize[X]/float64(2*nx) + (cellsize[X]/float64(nx))*float64(α)
+						scale := 1 / float64(nv*nw*nx*ny*nz)
+						surface := cellsize[v] * cellsize[w] // the two directions perpendicular to direction s
+						charge := surface * scale
+						pu1 := cellsize[u] / 2. // positive pole center
+						pu2 := -pu1             // negative pole center
 
-								for β := 0; β < ny; β++ {
-									ry := R[Y] - cellsize[Y]/2 + cellsize[Y]/float64(2*ny) + (cellsize[Y]/float64(ny))*float64(β)
+						// Do surface integral over source cell, accumulate  in B
+						var B [3]float64
+						for i := 0; i < nv; i++ {
+							pv := -(cellsize[v] / 2.) + cellsize[v]/float64(2*nv) + float64(i)*(cellsize[v]/float64(nv))
+							pole[v] = pv
+							for j := 0; j < nw; j++ {
+								pw := -(cellsize[w] / 2.) + cellsize[w]/float64(2*nw) + float64(j)*(cellsize[w]/float64(nw))
+								pole[w] = pw
 
-									for γ := 0; γ < nz; γ++ {
-										rz := R[Z] - cellsize[Z]/2 + cellsize[Z]/float64(2*nz) + (cellsize[Z]/float64(nz))*float64(γ)
-										points++
+								// Do volume integral over destination cell
+								for α := 0; α < nx; α++ {
+									rx := R[X] - cellsize[X]/2 + cellsize[X]/float64(2*nx) + (cellsize[X]/float64(nx))*float64(α)
 
-										pole[u] = pu1
-										R2[X], R2[Y], R2[Z] = rx-pole[X], ry-pole[Y], rz-pole[Z]
-										r := math.Sqrt(R2[X]*R2[X] + R2[Y]*R2[Y] + R2[Z]*R2[Z])
-										qr := charge / (4 * math.Pi * r * r * r)
-										bx := R2[X] * qr
-										by := R2[Y] * qr
-										bz := R2[Z] * qr
+									for β := 0; β < ny; β++ {
+										ry := R[Y] - cellsize[Y]/2 + cellsize[Y]/float64(2*ny) + (cellsize[Y]/float64(ny))*float64(β)
 
-										pole[u] = pu2
-										R2[X], R2[Y], R2[Z] = rx-pole[X], ry-pole[Y], rz-pole[Z]
-										r = math.Sqrt(R2[X]*R2[X] + R2[Y]*R2[Y] + R2[Z]*R2[Z])
-										qr = -charge / (4 * math.Pi * r * r * r)
-										B[X] += (bx + R2[X]*qr) // addition ordered for accuracy
-										B[Y] += (by + R2[Y]*qr)
-										B[Z] += (bz + R2[Z]*qr)
+										for γ := 0; γ < nz; γ++ {
+											rz := R[Z] - cellsize[Z]/2 + cellsize[Z]/float64(2*nz) + (cellsize[Z]/float64(nz))*float64(γ)
+											points++
 
+											pole[u] = pu1
+											R2[X], R2[Y], R2[Z] = rx-pole[X], ry-pole[Y], rz-pole[Z]
+											r := math.Sqrt(R2[X]*R2[X] + R2[Y]*R2[Y] + R2[Z]*R2[Z])
+											qr := charge / (4 * math.Pi * r * r * r)
+											bx := R2[X] * qr
+											by := R2[Y] * qr
+											bz := R2[Z] * qr
+
+											pole[u] = pu2
+											R2[X], R2[Y], R2[Z] = rx-pole[X], ry-pole[Y], rz-pole[Z]
+											r = math.Sqrt(R2[X]*R2[X] + R2[Y]*R2[Y] + R2[Z]*R2[Z])
+											qr = -charge / (4 * math.Pi * r * r * r)
+											B[X] += (bx + R2[X]*qr) // addition ordered for accuracy
+											B[Y] += (by + R2[Y]*qr)
+											B[Z] += (bz + R2[Z]*qr)
+
+										}
 									}
 								}
 							}
 						}
-					}
-					for d := s; d < 3; d++ { // destination index Ksdxyz
-						array[s][d][zw][yw][xw] += float32(B[d]) // += needed in case of PBC
+						for d := s; d < 3; d++ { // destination index Ksdxyz
+							array[s][d][zw][yw][xw] += float32(B[d]) // += needed in case of PBC
+						}
 					}
 				}
 			}
-		}
+			done <- struct{}{}
+		}(s)
 	}
+	<-done
+	<-done
+	<-done
 
 	// Reconstruct skipped parts from symmetry (X)
 	for z := 0; z < size[Z]; z++ {

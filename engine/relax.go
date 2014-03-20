63bbc4be24f83179b1fb246bbf3b52d3bba925e9
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-03-20 15:07:44 +0100

    better relax based on average torque

--- engine/relax.go
+++ engine/relax.go
@@ -7,7 +7,7 @@ import (
 )
 
 func init() {
-	DeclFunc("Relax", Relax, "Try to minimize the total energy until maxTorque limit")
+	DeclFunc("Relax", Relax, "Try to minimize the total energy")
 }
 
 func Relax() {
@@ -27,8 +27,11 @@ func Relax() {
 	}()
 
 	solver := stepper.(*RK23)
-	maxTorque := func() float64 {
-		return cuda.MaxVecNorm(solver.k1)
+	//	maxTorque := func() float64 {
+	//		return cuda.MaxVecNorm(solver.k1)
+	//	}
+	avgTorque := func() float64 {
+		return math.Sqrt(float64(cuda.Dot(solver.k1, solver.k1)))
 	}
 
 	const N = 3
@@ -42,16 +45,16 @@ func Relax() {
 	}
 
 	T0 := 0.
-	T1 := maxTorque()
+	T1 := avgTorque()
 
 	for MaxErr > 1e-9 {
 		MaxErr /= math.Sqrt2
 		util.Println(MaxErr)
 		Steps(N)
-		T0, T1 = T1, maxTorque()
+		T0, T1 = T1, avgTorque()
 		for T1 < T0 {
 			Steps(1)
-			T0, T1 = T1, maxTorque()
+			T0, T1 = T1, avgTorque()
 		}
 	}
 

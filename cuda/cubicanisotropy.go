326522e25a564191953c033b86fa46c9697795eb
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-04-14 20:24:04 +0200

    update cubicanisotropy wrappers for Colin's 2nd order term.

--- cuda/cubicanisotropy.go
+++ cuda/cubicanisotropy.go
@@ -8,7 +8,7 @@ import (
 
 // Adds cubic anisotropy field to Beff.
 // see cubicanisotropy.cu
-func AddCubicAnisotropy(Beff, m *data.Slice, k1_red LUTPtr, c1, c2 LUTPtrs, regions *Bytes) {
+func AddCubicAnisotropy(Beff, m *data.Slice, k1_red, k2_red LUTPtr, c1, c2 LUTPtrs, regions *Bytes) {
 	util.Argument(Beff.Size() == m.Size())
 
 	N := Beff.Len()
@@ -17,7 +17,7 @@ func AddCubicAnisotropy(Beff, m *data.Slice, k1_red LUTPtr, c1, c2 LUTPtrs, regi
 	k_addcubicanisotropy_async(
 		Beff.DevPtr(X), Beff.DevPtr(Y), Beff.DevPtr(Z),
 		m.DevPtr(X), m.DevPtr(Y), m.DevPtr(Z),
-		unsafe.Pointer(k1_red),
+		unsafe.Pointer(k1_red), unsafe.Pointer(k2_red),
 		c1[X], c1[Y], c1[Z],
 		c2[X], c2[Y], c2[Z],
 		regions.Ptr, N, cfg)

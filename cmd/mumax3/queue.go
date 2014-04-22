0dd9e4b0755320bf2fbbfcfba33c68e539ad9134
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-04-22 14:42:37 +0200

    use same css for gui and queue

--- cmd/mumax3/queue.go
+++ cmd/mumax3/queue.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"github.com/barnex/cuda5/cu"
+	"github.com/mumax/3/engine"
 	"io"
 	"log"
 	"net/http"
@@ -140,22 +141,7 @@ func (s *stateTab) RenderHTML(w io.Writer) {
 <!DOCTYPE html> <html> <head> 
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<meta http-equiv="refresh" content="1">
-
-	<style media="all" type="text/css">
-		body  { margin-left: 5%; margin-right:5%; font-family: sans-serif; font-size: 14px; }
-		img   { margin: 10px; }
-		table { border-collapse: collapse; }
-		tr:nth-child(even) { background-color: white; }
-		tr:nth-child(odd)  { background-color: white; }
-		td        { padding: 1px 5px; }
-		hr        { border-style: none; border-top: 1px solid #CCCCCC; }
-		a         { color: #375EAB; text-decoration: none; }
-		div       { margin-left: 20px; margin-top: 5px; margin-bottom: 20px; }
-		div#footer{ color:gray; font-size:14px; border:none; }
-		.ErrorBox { color: red; font-weight: bold; font-size: 1.5em; } 
-		.TextBox  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; }
-		textarea  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; color:gray; font-size: 1em; }
-	</style>
+`+engine.CSS+`
 	</head><body>
 	<span style="color:gray; font-weight:bold; font-size:1.5em"> mumax<sup>3</sup> queue status </span><br/>
 	<hr/>
--- engine/html.go
+++ engine/html.go
@@ -10,22 +10,7 @@ const templText = `
 
 	<title> mumax3 </title>
 
-	<style media="all" type="text/css">
-
-		body  { margin-left: 5%; margin-right:5%; font-family: sans-serif; font-size: 14px; }
-		img   { margin: 10px; }
-		table { border-collapse: collapse; }
-		tr:nth-child(even) { background-color: white; }
-		tr:nth-child(odd)  { background-color: white; }
-		td        { padding: 1px 5px; }
-		hr        { border-style: none; border-top: 1px solid #CCCCCC; }
-		a         { color: #375EAB; text-decoration: none; }
-		div       { margin-left: 20px; margin-top: 5px; margin-bottom: 20px; }
-		div#footer{ color:gray; font-size:14px; border:none; }
-		.ErrorBox { color: red; font-weight: bold; font-size: 1.5em; } 
-		.TextBox  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; }
-		textarea  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; color:gray; font-size: 1em; }
-	</style>
+` + CSS + `
 
 	{{.JS}}
 
@@ -228,4 +213,23 @@ Region: {{.Number "region" -1 255 -1}} </br>
 
 </body>
 </html>
+`
+
+const CSS = `
+	<style media="all" type="text/css">
+
+		body  { margin-left: 5%; margin-right:5%; font-family: sans-serif; font-size: 14px; }
+		img   { margin: 10px; }
+		table { border-collapse: collapse; }
+		tr:nth-child(even) { background-color: white; }
+		tr:nth-child(odd)  { background-color: white; }
+		td        { padding: 1px 5px; }
+		hr        { border-style: none; border-top: 1px solid #CCCCCC; }
+		a         { color: #375EAB; text-decoration: none; }
+		div       { margin-left: 20px; margin-top: 5px; margin-bottom: 20px; }
+		div#footer{ color:gray; font-size:14px; border:none; }
+		.ErrorBox { color: red; font-weight: bold; font-size: 1.5em; } 
+		.TextBox  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; }
+		textarea  { border:solid; border-color:#BBBBBB; border-width:1px; padding-left:4px; color:gray; font-size: 1em; }
+	</style>
 `

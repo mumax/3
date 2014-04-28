9580a820e334cfc98daf16e56ba853508890477a
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-04-28 10:02:30 +0200

    update cgoflags to support cuda 6.0

--- cuda/cgoflags.go
+++ cuda/cgoflags.go
@@ -4,6 +4,25 @@ package cuda
 
 import "C"
 
-//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib -L/opt/cuda/lib -L/opt/cuda/lib64 -lcudart
-//#cgo CFLAGS:-I/usr/local/cuda/include/ -I/opt/cuda/include
+//#cgo LDFLAGS:-lcudart
+//
+////default location:
+//#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib
+//#cgo CFLAGS: -I/usr/local/cuda/include/
+//
+////default location if not properly symlinked:
+//#cgo LDFLAGS:-L/usr/local/cuda-6.0/lib64 -L/usr/local/cuda-6.0/lib
+//#cgo LDFLAGS:-L/usr/local/cuda-5.5/lib64 -L/usr/local/cuda-5.5/lib
+//#cgo LDFLAGS:-L/usr/local/cuda-5.0/lib64 -L/usr/local/cuda-5.0/lib
+//#cgo CFLAGS: -I/usr/local/cuda-6.0/include/
+//#cgo CFLAGS: -I/usr/local/cuda-5.5/include/
+//#cgo CFLAGS: -I/usr/local/cuda-5.0/include/
+//
+////arch linux:
+//#cgo LDFLAGS:-L/opt/cuda/lib64 -L/opt/cuda/lib
+//#cgo CFLAGS: -I/opt/cuda/include
+//
+////WINDOWS:
+//#cgo windows LDFLAGS:-LC:/cuda/v5.0/lib/x64 -LC:/cuda/v5.5/lib/x64 -LC:/cuda/v6.0/lib/x64
+//#cgo windows CFLAGS: -IC:/cuda/v5.0/include -IC:/cuda/v5.5/include -IC:/cuda/v6.0/include
 import "C"
--- curand/cgoflags.go
+++ curand/cgoflags.go
@@ -2,24 +2,25 @@ package curand
 
 // This file provides CGO flags to find CUDA libraries and headers.
 
+//#cgo LDFLAGS:-lcurand
+//
 ////default location:
 //#cgo LDFLAGS:-L/usr/local/cuda/lib64 -L/usr/local/cuda/lib
+//#cgo CFLAGS: -I/usr/local/cuda/include/
 //
 ////default location if not properly symlinked:
+//#cgo LDFLAGS:-L/usr/local/cuda-6.0/lib64 -L/usr/local/cuda-6.0/lib
+//#cgo LDFLAGS:-L/usr/local/cuda-5.5/lib64 -L/usr/local/cuda-5.5/lib
 //#cgo LDFLAGS:-L/usr/local/cuda-5.0/lib64 -L/usr/local/cuda-5.0/lib
-//
-////ubuntu provided driver:
-//#cgo LDFLAGS:-L/usr/lib -L/usr/lib/nvidia-current/ -L/usr/lib/nvidia-experimental -L/usr/lib/nvidia-304 -L/usr/lib/nvidia-310 -L/usr/lib/nvidia-313 -L/usr/lib/nvidia-319
+//#cgo CFLAGS: -I/usr/local/cuda-6.0/include/
+//#cgo CFLAGS: -I/usr/local/cuda-5.5/include/
+//#cgo CFLAGS: -I/usr/local/cuda-5.0/include/
 //
 ////arch linux:
 //#cgo LDFLAGS:-L/opt/cuda/lib64 -L/opt/cuda/lib
-//
-//#cgo LDFLAGS:-lcurand
-//
-//#cgo CFLAGS:-I/usr/local/cuda/include/ -I/opt/cuda/include
+//#cgo CFLAGS: -I/opt/cuda/include
 //
 ////WINDOWS:
-//
-//#cgo LDFLAGS:-LC:/cuda/v5.0/lib/x64 -LC:/cuda/v5.5/lib/x64
-//#cgo CFLAGS:-IC:/cuda/v5.0/include -IC:/cuda/v5.5/include 
+//#cgo windows LDFLAGS:-LC:/cuda/v5.0/lib/x64 -LC:/cuda/v5.5/lib/x64 -LC:/cuda/v6.0/lib/x64
+//#cgo windows CFLAGS: -IC:/cuda/v5.0/include -IC:/cuda/v5.5/include -IC:/cuda/v6.0/include
 import "C"

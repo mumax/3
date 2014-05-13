e79ae6d9bbac433e65ffe0453d1cbd66aac05657
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-05-13 09:33:21 +0200

    gui: fix CLI box loss of content after disconnect

--- gui/clibox.go
+++ gui/clibox.go
@@ -7,11 +7,12 @@ type clibox struct {
 }
 
 func (e *clibox) update(id string) []jsCall {
-	return []jsCall{{F: "setAttr", Args: []interface{}{id, "value", e.value()}}}
+	return []jsCall{} // We never set the value of the CLI box, only the user does
 }
 
+// Command-line interface textbox where user types commands.
 func (d *Page) CliBox(id string, value interface{}, extra ...string) string {
 	e := &clibox{data: data{value}}
 	d.addElem(id, e)
-	return fmt.Sprintf(`<input type=%v class=TextBox id=%v  onfocus="notifyfocus('%v')" onblur="notifyblur('%v')" %v />`, "text", id, id, id, cat(extra))
+	return fmt.Sprintf(`<input type=%v class=TextBox id=%v  %v />`, "text", id, cat(extra))
 }

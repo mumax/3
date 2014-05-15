71ac9ead8faadfde598766502dad89e40530fbc9
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-05-15 14:25:00 +0200

    graph: axes

--- graph/graph.go
+++ graph/graph.go
@@ -2,28 +2,31 @@
 package graph
 
 import (
+	"fmt"
 	"github.com/mumax/3/svgo"
 	"io"
 )
 
 type Graph struct {
-	canvas           *svg.SVG
-	w, h             float64 // overall canvas size in pixels
-	xMin, xMax       float64 // x ranges of graphing area
-	yMin, yMax       float64 // y ranges of graphing area
-	marginL, marginR float64 // whitespace around graphing area, in pixels
-	marginB, marginT float64 // whitespace around graphing area, in pixels
-	LineStyle        string  //
-	TicSize          float64 // length of axis tics, in pixels
+	canvas                          *svg.SVG
+	w, h                            float64 // overall canvas size in pixels
+	xMin, xMax                      float64 // x ranges of graphing area
+	yMin, yMax                      float64 // y ranges of graphing area
+	marginL, marginR                float64 // whitespace around graphing area, in pixels
+	marginB, marginT                float64 // whitespace around graphing area, in pixels
+	LineStyle, FillStyle, TextStyle string  //
+	TicSize                         float64 // length of axis tics, in pixels
 }
 
 func New(out io.Writer, width, height int) *Graph {
 	canvas := svg.New(out)
 	canvas.Start(width, height)
 	g := &Graph{canvas: canvas, w: float64(width), h: float64(height)}
-	g.SetMargins(width/16, width/16, height/16, height/16)
+	g.SetMargins(32, 24, 24, 16) // suited margin for DrawAxes
 	g.SetRanges(0, 1, 0, 1)
 	g.LineStyle = `style="fill:none;stroke:black"`
+	g.FillStyle = `style="fill:black"`
+	g.TextStyle = `font-family="sans-serif"`
 	g.TicSize = 8
 	return g
 }
@@ -47,16 +50,19 @@ func (g *Graph) SetRanges(xMin, xMax, yMin, yMax float64) {
 }
 
 func (g *Graph) DrawAxes(xTic, yTic float64) {
+	const vpad = 4 // pixels between axes and text
 	g.Rect(g.xMin, g.yMin, g.xMax, g.yMax)
 	for x := g.xMin; x <= g.xMax; x += xTic {
-		x2, y2 := g.transf(x, g.yMin)
-		g.canvas.Line(x2, y2, x2, y2-g.TicSize, g.LineStyle)
-		x2, y2 = g.transf(x, g.yMax)
+		x2, y2 := g.transf(x, g.yMax)
 		g.canvas.Line(x2, y2, x2, y2+g.TicSize, g.LineStyle)
+		x2, y2 = g.transf(x, g.yMin)
+		g.canvas.Line(x2, y2, x2, y2-g.TicSize, g.LineStyle)
+		g.canvas.Text(int(x2), int(y2)+vpad, fmt.Sprintf("%.5g", x), g.TextStyle, `dominant-baseline="hanging"`, `text-anchor="middle"`)
 	}
 	for y := g.yMin; y <= g.yMax; y += yTic {
 		x2, y2 := g.transf(g.xMin, y)
 		g.canvas.Line(x2, y2, x2+g.TicSize, y2, g.LineStyle)
+		g.canvas.Text(int(x2)-vpad, int(y2), fmt.Sprintf("%.5g", y), g.TextStyle, `dominant-baseline="middle"`, `style="text-anchor:end"`)
 		x2, y2 = g.transf(g.xMax, y)
 		g.canvas.Line(x2, y2, x2-g.TicSize, y2, g.LineStyle)
 	}
@@ -85,7 +91,7 @@ func (g *Graph) Rect(x1, y1, x2, y2 float64) {
 }
 
 func (g *Graph) transf(x, y float64) (x2, y2 float64) {
-	x2 = g.marginL + x*(g.w-g.marginL-g.marginR)/(g.xMax-g.xMin)
-	y2 = g.h - g.marginT - y*(g.h-g.marginB-g.marginT)/(g.yMax-g.yMin)
+	x2 = g.marginL + (x-g.xMin)*(g.w-g.marginL-g.marginR)/(g.xMax-g.xMin)
+	y2 = g.h - g.marginB - (y-g.yMin)*(g.h-g.marginB-g.marginT)/(g.yMax-g.yMin)
 	return
 }

033dd7fea1d3922c6b51a82458fda6248858c282
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-07-30 09:44:22 +0200

    enable vet in make.bash

--- make.bash
+++ make.bash
@@ -5,7 +5,7 @@ ln -sf $(pwd)/post-commit .git/hooks/post-commit || echo ""
 
 (cd cuda && ./make.bash)  || exit 1
 go install -v github.com/mumax/3/cmd/... || exit 1
-#go vet github.com/mumax/3/... || exit 1
+go vet github.com/mumax/3/... || echo ""
 (cd test && mumax3 -vet *.mx3) || exit 1
 (cd doc && mumax3 -vet *.mx3)  || exit 1
 

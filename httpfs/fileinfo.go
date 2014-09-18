0568d2d7dbf746fea20fd6b6d7ae43c5fe2dff44
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-09-18 11:20:35 +0200

    httpfs cleanup

--- httpfs/fileinfo.go
+++ httpfs/fileinfo.go
@@ -1,20 +1,20 @@
 package httpfs
 
-import (
-	"os"
-	"time"
-)
-
-type fileInfo struct {
-	Nm   string      // base name of the file
-	Sz   int64       // length in bytes for regular files; system-dependent for others
-	Md   os.FileMode // file mode bits
-	MdTm time.Time   // modification time
-}
-
-func (f *fileInfo) Name() string       { return f.Nm }
-func (f *fileInfo) Size() int64        { return f.Sz }
-func (f *fileInfo) Mode() os.FileMode  { return f.Md }
-func (f *fileInfo) ModTime() time.Time { return f.MdTm }
-func (f *fileInfo) IsDir() bool        { return f.Mode().IsDir() }
-func (f *fileInfo) Sys() interface{}   { return nil }
+//import (
+//	"os"
+//	"time"
+//)
+//
+//type fileInfo struct {
+//	Nm   string      // base name of the file
+//	Sz   int64       // length in bytes for regular files; system-dependent for others
+//	Md   os.FileMode // file mode bits
+//	MdTm time.Time   // modification time
+//}
+//
+//func (f *fileInfo) Name() string       { return f.Nm }
+//func (f *fileInfo) Size() int64        { return f.Sz }
+//func (f *fileInfo) Mode() os.FileMode  { return f.Md }
+//func (f *fileInfo) ModTime() time.Time { return f.MdTm }
+//func (f *fileInfo) IsDir() bool        { return f.Mode().IsDir() }
+//func (f *fileInfo) Sys() interface{}   { return nil }

816bfc3c6fd41311bc8371b49d73f75235e09ef6
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2014-10-15 10:36:52 +0200

    improve test script

--- test.bash
+++ test.bash
@@ -2,10 +2,8 @@
 
 source ./make.bash || exit 1
 
-PKGS=$(echo github.com/mumax/3/{cmd/mumax3,cmd/mumax3-convert,cmd/mumax3-plot,data,draw,prof,engine,mag,script,util,cuda})
-
-go test -i $PKGS || exit 1
-go test $PKGS  || exit 1
+go test -i github.com/mumax/3/... || exit 1
+go test $PKGS  github.com/mumax/3/... || exit 1
 
 (cd test && ./run.bash) || exit 1
 

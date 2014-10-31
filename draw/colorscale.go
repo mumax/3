7ebd4ff334207bd05abf5669ebac6d755cafc74a
Arne Vansteenkiste <barnex@gmail.com>
2014-10-31 10:47:28 +0100

    fix colormap

--- draw/colorscale.go
+++ draw/colorscale.go
@@ -4,55 +4,55 @@ import "image/color"
 import "fmt"
 
 func ColorMap(min, max, value float32, colormap ...color.RGBA) color.RGBA {
-	if len(colormap) == 0 {
+	// default colormap: black-white
+	if len(colormap) < 1 {
 		colormap = []color.RGBA{{0, 0, 0, 255}, {255, 255, 255, 255}}
 	}
 
-	col := (value - min) / (max - min)
-	if col > 1. {
-		col = 1.
+	// map value to interval [O,1]
+	val := float64((value - min) / (max - min))
+	if val > 1 {
+		val = 1
 	}
-	if col < 0. {
-		col = 0.
-	}
-
-	N := float64(len(colormap)) - 1
-	lower := int(float64(col) * N)
-	upper := lower + 1
-
-	if lower < 0 {
-		panic(fmt.Sprint("lower=", lower))
+	if val < 0 {
+		val = 0
 	}
 
-	if upper >= len(colormap) {
-		return colormap[lower]
+	// find index of color below our value
+	maxIndex := float64(len(colormap) - 1)
+	index := val * maxIndex
+	// corner case val==max:
+	if index == maxIndex {
+		index--
 	}
 
-	c1 := colormap[lower]
-	c2 := colormap[upper]
+	// get two neighboring colors
+	i := int(index)
+	c1 := colormap[i]
+	c2 := colormap[i+1]
 
-	x := (float64(col) - float64(lower)/N)
-
-	fmt.Println(x)
-
+	// location between two neighboring colors [0..1]
+	x := (val - float64(i)/maxIndex) * maxIndex
 	if x < 0 || x > 1 {
 		panic(fmt.Sprint("x=", x))
 	}
 
+	// interpolate between colors
 	r := (1-x)*float64(c1.R) + x*float64(c2.R)
 	g := (1-x)*float64(c1.G) + x*float64(c2.G)
 	b := (1-x)*float64(c1.B) + x*float64(c2.B)
 	a := (1-x)*float64(c1.A) + x*float64(c2.A)
 
 	return color.RGBA{bte(r), bte(g), bte(b), bte(a)}
+
 }
 
 func bte(x float64) uint8 {
 	if x < 0 {
 		return 0
 	}
-	if x > 1 {
+	if x > 255 {
 		return 255
 	}
-	return uint8(255 * x)
+	return uint8(x)
 }

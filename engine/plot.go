6f888a86a5ce43974a89c25078c9e0ecb6031d7f
Arne Vansteenkiste <barnex@gmail.com>
2015-03-10 11:41:30 +0100

    limit max number of gnuplot processes, check httpfs errors

--- engine/plot.go
+++ engine/plot.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"bytes"
+	"errors"
 	"fmt"
 	"github.com/mumax/3/httpfs"
 	"image"
@@ -9,32 +10,82 @@ import (
 	"io/ioutil"
 	"net/http"
 	"os/exec"
+	"sync/atomic"
 )
 
+var nPlots int32       // counts number of active gnuplot processes
+const MAX_GNUPLOTS = 5 // maximum allowed number of gnuplot processes
+
 func (g *guistate) servePlot(w http.ResponseWriter, r *http.Request) {
+
+	out := []byte{}
+
+	// handle error and return wheter err != nil.
+	handle := func(err error) bool {
+		if err != nil {
+			w.Write(emptyIMG())
+			g.Set("plotErr", err.Error()+string(out))
+			return true
+		} else {
+			return false
+		}
+	}
+
+	// limit max processes
+	atomic.AddInt32(&nPlots, 1)
+	defer atomic.AddInt32(&nPlots, -1)
+	if atomic.LoadInt32(&nPlots) > MAX_GNUPLOTS {
+		handle(errors.New("too many gnuplot processes"))
+		return
+	}
+
 	a := g.StringValue("usingx")
 	b := g.StringValue("usingy")
 
 	cmd := "gnuplot"
 	args := []string{"-e", fmt.Sprintf(`set format x "%%g"; set key off; set format y "%%g"; set term svg size 480,320 fsize 10; plot "-" u %v:%v w li; set output;exit;`, a, b)}
 	excmd := exec.Command(cmd, args...)
-	stdin, _ := excmd.StdinPipe()
-	stdout, _ := excmd.StdoutPipe()
-	data, _ := httpfs.Read(fmt.Sprintf(`%vtable.txt`, OD()))
-	err := excmd.Start()
-	defer excmd.Wait()
-	stdin.Write(data)
-	stdin.Close()
-	out, err := ioutil.ReadAll(stdout)
-	if err != nil {
-		w.Write(emptyIMG())
-		g.Set("plotErr", string(out))
+
+	stdin, err := excmd.StdinPipe()
+	if handle(err) {
 		return
-	} else {
-		w.Header().Set("Content-Type", "image/svg+xml")
-		w.Write(out)
-		g.Set("plotErr", "")
 	}
+
+	stdout, err := excmd.StdoutPipe()
+	if handle(err) {
+		return
+	}
+
+	data, err := httpfs.Read(fmt.Sprintf(`%vtable.txt`, OD()))
+	if handle(err) {
+		return
+	}
+
+	err = excmd.Start()
+	if handle(err) {
+		return
+	}
+	defer excmd.Wait()
+
+	_, err = stdin.Write(data)
+	if handle(err) {
+		return
+	}
+	err = stdin.Close()
+	if handle(err) {
+		return
+	}
+
+	out, err = ioutil.ReadAll(stdout)
+	if handle(err) {
+		return
+	}
+
+	w.Header().Set("Content-Type", "image/svg+xml")
+	w.Write(out)
+	g.Set("plotErr", "")
+	return
+
 }
 
 var empty_img []byte

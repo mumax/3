c890266b9416a6e20f24abaffa6159594ffb9bb0
Arne Vansteenkiste <barnex@gmail.com>
2015-03-19 14:02:24 +0100

    smarter fatbinload

--- cuda/fatbin.go
+++ cuda/fatbin.go
@@ -8,20 +8,35 @@ import (
 
 // load PTX code for function name, find highest SM that matches our card.
 func fatbinLoad(sm map[int]string, fn string) cu.Function {
+	cc := determineCC()
+	return cu.ModuleLoadData(sm[cc]).GetFunction(fn)
+}
+
+var ccCache = 0
+
+func determineCC() int {
+	if ccCache != 0 {
+		return ccCache
+	}
+
+	for k, _ := range madd2_map {
+		if k > ccCache && okCC(k) {
+			ccCache = k
+		}
+	}
+	if ccCache == 0 {
+		log.Fatalln("Unsupported GPU compute capability:", ccCache)
+	}
+	return ccCache
+}
+
+func okCC(cc int) (ok bool) {
 	defer func() {
 		if err := recover(); err == cu.ERROR_NO_BINARY_FOR_GPU {
-			log.Fatalln(err, ": most likely your nvidia driver is out-of-date.")
+			ok = false
 		}
+		log.Println("OK CC", cc, ok)
 	}()
-	best := 0
-	for k, _ := range sm {
-		if k <= cudaCC && k > best {
-			best = k
-		}
-	}
-	if best == 0 {
-		log.Fatalln("Unsupported GPU compute capability:", cudaCC)
-	}
-	//log.Println(fn, best)
-	return cu.ModuleLoadData(sm[best]).GetFunction(fn)
+	cu.ModuleLoadData(madd2_map[cc]).GetFunction("madd2")
+	return true
 }

de653fd6647fac80c487a6dfc136c788697fae07
Arne Vansteenkiste <barnex@gmail.com>
2015-04-02 14:18:02 +0200

    adaptive time step for euler solver

--- engine/euler.go
+++ engine/euler.go
@@ -13,17 +13,25 @@ func (_ *Euler) Step() {
 	dy0 := cuda.Buffer(VECTOR, y.Size())
 	defer cuda.Recycle(dy0)
 
-	Dt_si = FixDt
-	dt := float32(Dt_si * GammaLL)
-	util.AssertMsg(dt > 0, "Euler solver requires fixed time step > 0")
-
 	torqueFn(dy0)
-	cuda.Madd2(y, y, dy0, 1, dt) // y = y + dt * dy
 	setMaxTorque(dy0)
-	setLastErr(0) // unknown
 
-	Time += Dt_si
+	// Adaptive time stepping: treat MaxErr as the maximum magnetization delta
+	// (proportional to the error, but an overestimation for sure)
+	var dt float32
+	if FixDt != 0 {
+		Dt_si = FixDt
+		dt = float32(Dt_si * GammaLL)
+	} else {
+		dt = float32(MaxErr / LastTorque)
+		Dt_si = float64(dt) / GammaLL
+	}
+	util.AssertMsg(dt > 0, "Euler solver requires fixed time step > 0")
+	setLastErr(float64(dt) * LastTorque)
+
+	cuda.Madd2(y, y, dy0, 1, dt) // y = y + dt * dy
 	M.normalize()
+	Time += Dt_si
 	NSteps++
 }
 

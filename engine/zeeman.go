94dd610421447ba8b9786f4cbf20c58d0e999c3d
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2015-12-25 14:40:34 +0000

    refactor Excitation

--- engine/zeeman.go
+++ engine/zeeman.go
@@ -1,18 +1,17 @@
 package engine
 
 var (
-	B_ext        excitation
+	B_ext        = NewExcitation("B_ext", "T", "Externally applied field")
 	Edens_zeeman = NewScalarField("Edens_Zeeman", "J/m3", "Zeeman energy density", AddEdens_zeeman)
 	E_Zeeman     = NewScalarValue("E_Zeeman", "J", "Zeeman energy", GetZeemanEnergy)
 )
 
-var AddEdens_zeeman = makeEdensAdder(&B_ext, -1)
+var AddEdens_zeeman = makeEdensAdder(B_ext, -1)
 
 func init() {
-	B_ext.init("B_ext", "T", "Externally applied field")
 	registerEnergy(GetZeemanEnergy, AddEdens_zeeman)
 }
 
 func GetZeemanEnergy() float64 {
-	return -1 * cellVolume() * dot(&M_full, &B_ext)
+	return -1 * cellVolume() * dot(&M_full, B_ext)
 }

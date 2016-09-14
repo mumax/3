addfdc561000c3e192d8150177eec84a5ad34624
Arne Vansteenkiste <a.vansteenkiste@gmail.com>
2016-09-14 13:37:27 +0200

    lltorque2

--- cuda/lltorque2.cu
+++ cuda/lltorque2.cu
@@ -1,19 +1,20 @@
-#include <stdint.h>
+#include "amul.h"
 #include "float3.h"
+#include <stdint.h>
 
 // Landau-Lifshitz torque.
 extern "C" __global__ void
-lltorque(float* __restrict__  tx, float* __restrict__  ty, float* __restrict__  tz,
-         float* __restrict__  mx, float* __restrict__  my, float* __restrict__  mz,
-         float* __restrict__  hx, float* __restrict__  hy, float* __restrict__  hz,
-         float* alphaLUT, uint8_t* regions, int N) {
+lltorque2(float* __restrict__  tx, float* __restrict__  ty, float* __restrict__  tz,
+          float* __restrict__  mx, float* __restrict__  my, float* __restrict__  mz,
+          float* __restrict__  hx, float* __restrict__  hy, float* __restrict__  hz,
+          float* __restrict__  alpha_, float alpha_mul, int N) {
 
 	int i =  ( blockIdx.y*gridDim.x + blockIdx.x ) * blockDim.x + threadIdx.x;
 	if (i < N) {
 
 		float3 m = {mx[i], my[i], mz[i]};
 		float3 H = {hx[i], hy[i], hz[i]};
-		float alpha = alphaLUT[regions[i]];
+		float alpha = amul(alpha_, alpha_mul, i);
 
 		float3 mxH = cross(m, H);
 		float gilb = -1.0f / (1.0f + alpha * alpha);

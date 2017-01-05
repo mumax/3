ecee0beabc61780a90ec846b45d9d83274bd559b
JLeliaert <jonathan.leliaert@gmail.com>
2017-01-05 16:49:05 +0100

    added centerbubble feature (untested)

--- engine/shift.go
+++ engine/shift.go
@@ -7,7 +7,7 @@ import (
 
 var (
 	TotalShift                      float64                        // accumulated window shift (X) in meter
-	ShiftMagL, ShiftMagR            data.Vector                    // when shifting m, put these value at the left/right edge.
+	ShiftMagL, ShiftMagR , ShiftMagU , ShiftMagD          data.Vector                    // when shifting m, put these value at the left/right edge.
 	ShiftM, ShiftGeom, ShiftRegions bool        = true, true, true // should shift act on magnetization, geometry, regions?
 )
 
@@ -15,6 +15,8 @@ func init() {
 	DeclFunc("Shift", Shift, "Shifts the simulation by +1/-1 cells along X")
 	DeclVar("ShiftMagL", &ShiftMagL, "Upon shift, insert this magnetization from the left")
 	DeclVar("ShiftMagR", &ShiftMagR, "Upon shift, insert this magnetization from the right")
+	DeclVar("ShiftMagU", &ShiftMagU, "Upon shift, insert this magnetization from the top")
+	DeclVar("ShiftMagD", &ShiftMagD, "Upon shift, insert this magnetization from the bottom")
 	DeclVar("ShiftM", &ShiftM, "Whether Shift() acts on magnetization")
 	DeclVar("ShiftGeom", &ShiftGeom, "Whether Shift() acts on geometry")
 	DeclVar("ShiftRegions", &ShiftRegions, "Whether Shift() acts on regions")
@@ -45,6 +47,32 @@ func shiftMag(m *data.Slice, dx int) {
 	for c := 0; c < m.NComp(); c++ {
 		comp := m.Comp(c)
 		cuda.ShiftX(m2, comp, dx, float32(ShiftMagL[c]), float32(ShiftMagR[c]))
+		data.Copy(comp, m2) // str0 ?
+	}
+}
+
+
+// shift the simulation window over dy cells in Y direction
+//TODO add regions and geoms
+func YShift(dy int) {
+	if ShiftM {
+		shiftMagY(M.Buffer(), dy)
+	}
+	if ShiftRegions {
+		regions.shiftY(dy)
+	}
+	//if ShiftGeom {
+	//	geometry.shift(dx)
+	//}
+	M.normalize()
+}
+
+func shiftMagY(m *data.Slice, dy int) {
+	m2 := cuda.Buffer(1, m.Size())
+	defer cuda.Recycle(m2)
+	for c := 0; c < m.NComp(); c++ {
+		comp := m.Comp(c)
+		cuda.ShiftY(m2, comp, dy, float32(ShiftMagU[c]), float32(ShiftMagD[c]))
 		data.Copy(comp, m2) // str0 ?
 	}
 }

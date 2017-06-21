676da9f9c48fae57d97ecafe5a709f0c0303dd13
Mykola Dvornik <mykola.dvornik@gmail.com>
2017-06-21 22:41:19 +0200

    cuda: update makefile

--- cuda/make.bash
+++ cuda/make.bash
@@ -6,7 +6,7 @@ NVCC='nvcc --compiler-options -Werror --compiler-options -Wall -Xptxas -O3 -ptx'
 
 for f in *.cu; do
 	g=$(echo $f | sed 's/\.cu$//') # file basename
-	for cc in 20 30 35 50 52 53; do
+	for cc in 30 35 50 52 53 60 61; do
 		if [[ $f -nt $g'_'$cc.ptx ]]; then
 			echo $NVCC -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx
 			$NVCC -I/usr/local/cuda/include -gencode arch=compute_$cc,code=sm_$cc $f -o $g'_'$cc.ptx # error can be ignored

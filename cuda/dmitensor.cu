b3fdc765ce7e18a317b6bcbd1b72bb0025625041
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2017-07-25 12:09:32 +0200

    combine two neighbors in cuda/dmitensor.cu

--- cuda/dmitensor.cu
+++ cuda/dmitensor.cu
@@ -58,118 +58,81 @@ adddmitensor(float* __restrict__ Hx, float* __restrict__ Hy, float* __restrict__
     }
 
 
-    // left neighbor
+    // x direction
     {
-        float3 m1 = make_float3(0.0f, 0.0f, 0.0f);     // left neighbor
-        int i_ = idx(lclampx(ix-1), iy, iz);           // load neighbor m if inside grid, keep 0 otherwise
-        if (ix-1 >= 0 || PBCx) {
-            m1 = make_float3(mx[i_], my[i_], mz[i_]);
-        }
-        if (!is0(m1)){
-            // bulk origin
-            h.x += ((D[XXY]-D[XYX])*m1.y+(D[XXZ]-D[XZX])*m1.z)/(2*cx) ;
-            h.y += ((D[XYZ]-D[XZY])*m1.z+(D[XYX]-D[XXY])*m1.x)/(2*cx) ;
-            h.z += ((D[XZX]-D[XXZ])*m1.x+(D[XZY]-D[XYZ])*m1.y)/(2*cx) ;
-            // boundary induced (cancels out if right neighbor exists as well)
-            h.x -= ((D[XXX]+D[XXX])*m0.x+(D[XXY]+D[XYX])*m0.y+(D[XXZ]+D[XZX])*m0.z)/(2*cx) ;
-            h.y -= ((D[XYY]+D[XYY])*m0.y+(D[XYZ]+D[XZY])*m0.z+(D[XYX]+D[XXY])*m0.x)/(2*cx) ;
-            h.z -= ((D[XZZ]+D[XZZ])*m0.z+(D[XZX]+D[XXZ])*m0.x+(D[XZY]+D[XYZ])*m0.y)/(2*cx) ;
-        }
-    }
-
-    // right neighbor
-    {
+        // left neighbor
         float3 m1 = make_float3(0.0f, 0.0f, 0.0f);
-        int i_ = idx(hclampx(ix+1), iy, iz);
+        if (ix-1 >= 0 || PBCx) {                      // check if neighbor is in simulation box
+            int i_ = idx(lclampx(ix-1), iy, iz);          // index of neighbor
+            m1 = make_float3(mx[i_], my[i_], mz[i_]); // magnetization of neighbor
+        }
+        // right neighbor
+        float3 m2 = make_float3(0.0f, 0.0f, 0.0f);
         if (ix+1 <Nx || PBCx) {
-            m1 = make_float3(mx[i_], my[i_], mz[i_]);
+            int i_ = idx(hclampx(ix+1), iy, iz);
+            m2 = make_float3(mx[i_], my[i_], mz[i_]);
         }
-        if (!is0(m1)){
-            // bulk origin
-            h.x -= ((D[XXY]-D[XYX])*m1.y+(D[XXZ]-D[XZX])*m1.z)/(2*cx) ;
-            h.y -= ((D[XYZ]-D[XZY])*m1.z+(D[XYX]-D[XXY])*m1.x)/(2*cx) ;
-            h.z -= ((D[XZX]-D[XXZ])*m1.x+(D[XZY]-D[XYZ])*m1.y)/(2*cx) ;
-            // boundary induced (cancels out if left neighbor exists as well)
-            h.x += ((D[XXX]+D[XXX])*m0.x+(D[XXY]+D[XYX])*m0.y+(D[XXZ]+D[XZX])*m0.z)/(2*cx) ;
-            h.y += ((D[XYY]+D[XYY])*m0.y+(D[XYZ]+D[XZY])*m0.z+(D[XYX]+D[XXY])*m0.x)/(2*cx) ;
-            h.z += ((D[XZZ]+D[XZZ])*m0.z+(D[XZX]+D[XXZ])*m0.x+(D[XZY]+D[XYZ])*m0.y)/(2*cx) ;
-        }
+        // bulk DMI
+        h.x += ((D[XXY]-D[XYX])*(m1.y-m2.y)+(D[XXZ]-D[XZX])*(m1.z-m2.z))/(2*cx);
+        h.y += ((D[XYZ]-D[XZY])*(m1.z-m2.z)+(D[XYX]-D[XXY])*(m1.x-m2.x))/(2*cx);
+        h.z += ((D[XZX]-D[XXZ])*(m1.x-m2.x)+(D[XZY]-D[XYZ])*(m1.y-m2.y))/(2*cx);
+        // boundary induced DMI
+        int sign = !is0(m1) - !is0(m2);
+        h.x += sign*((D[XXX]+D[XXX])*m0.x+(D[XXY]+D[XYX])*m0.y+(D[XXZ]+D[XZX])*m0.z)/(2*cx);
+        h.y += sign*((D[XYY]+D[XYY])*m0.y+(D[XYZ]+D[XZY])*m0.z+(D[XYX]+D[XXY])*m0.x)/(2*cx);
+        h.z += sign*((D[XZZ]+D[XZZ])*m0.z+(D[XZX]+D[XXZ])*m0.x+(D[XZY]+D[XYZ])*m0.y)/(2*cx);
     }
 
-    // back neighbor
+
+    // y direction
     {
+        // back neighbor
         float3 m1 = make_float3(0.0f, 0.0f, 0.0f);
-        int i_ = idx(ix, lclampy(iy-1), iz);
         if (iy-1 >= 0 || PBCy) {
+            int i_ = idx(ix, lclampy(iy-1), iz);
             m1 = make_float3(mx[i_], my[i_], mz[i_]);
         }
-        if (!is0(m1)){
-            // bulk origin
-            h.x += ((D[YXY]-D[YYX])*m1.y+(D[YXZ]-D[YZX])*m1.z)/(2*cy) ;
-            h.y += ((D[YYZ]-D[YZY])*m1.z+(D[YYX]-D[YXY])*m1.x)/(2*cy) ;
-            h.z += ((D[YZX]-D[YXZ])*m1.x+(D[YZY]-D[YYZ])*m1.y)/(2*cy) ;
-            // boundary induced (cancels out if front neighbor exists as well)
-            h.x -= ((D[YXX]+D[YXX])*m0.x+(D[YXY]+D[YYX])*m0.y+(D[YXZ]+D[YZX])*m0.z)/(2*cx) ;
-            h.y -= ((D[YYY]+D[YYY])*m0.y+(D[YYZ]+D[YZY])*m0.z+(D[YYX]+D[YXY])*m0.x)/(2*cx) ;
-            h.z -= ((D[YZZ]+D[YZZ])*m0.z+(D[YZX]+D[YXZ])*m0.x+(D[YZY]+D[YYZ])*m0.y)/(2*cx) ;
-        }
-    }
-
-    // front neighbor
-    {
-        float3 m1 = make_float3(0.0f, 0.0f, 0.0f);
-        int i_ = idx(ix, hclampy(iy+1), iz);
+        // front neighbor
+        float3 m2 = make_float3(0.0f, 0.0f, 0.0f);
         if (iy+1 <Ny || PBCy) {
-            m1 = make_float3(mx[i_], my[i_], mz[i_]);
+            int i_ = idx(ix, hclampy(iy+1), iz);
+            m2 = make_float3(mx[i_], my[i_], mz[i_]);
         }
-        if (!is0(m1)){
-            // bulk origin
-            h.x -= ((D[YXY]-D[YYX])*m1.y+(D[YXZ]-D[YZX])*m1.z)/(2*cy) ;
-            h.y -= ((D[YYZ]-D[YZY])*m1.z+(D[YYX]-D[YXY])*m1.x)/(2*cy) ;
-            h.z -= ((D[YZX]-D[YXZ])*m1.x+(D[YZY]-D[YYZ])*m1.y)/(2*cy) ;
-            // boundary induced (cancels out if back neighbor exists as well)
-            h.x += ((D[YXX]+D[YXX])*m0.x+(D[YXY]+D[YYX])*m0.y+(D[YXZ]+D[YZX])*m0.z)/(2*cx) ;
-            h.y += ((D[YYY]+D[YYY])*m0.y+(D[YYZ]+D[YZY])*m0.z+(D[YYX]+D[YXY])*m0.x)/(2*cx) ;
-            h.z += ((D[YZZ]+D[YZZ])*m0.z+(D[YZX]+D[YXZ])*m0.x+(D[YZY]+D[YYZ])*m0.y)/(2*cx) ;
-        }
+        // bulk DMI
+        h.x += ((D[YXY]-D[YYX])*(m1.y-m2.y)+(D[YXZ]-D[YZX])*(m1.z-m2.z))/(2*cy);
+        h.y += ((D[YYZ]-D[YZY])*(m1.z-m2.z)+(D[YYX]-D[YXY])*(m1.x-m2.x))/(2*cy);
+        h.z += ((D[YZX]-D[YXZ])*(m1.x-m2.x)+(D[YZY]-D[YYZ])*(m1.y-m2.y))/(2*cy);
+        // boundary induced DMI
+        int sign = !is0(m2) - !is0(m1);
+        h.x += sign*((D[YXX]+D[YXX])*m0.x+(D[YXY]+D[YYX])*m0.y+(D[YXZ]+D[YZX])*m0.z)/(2*cy);
+        h.y += sign*((D[YYY]+D[YYY])*m0.y+(D[YYZ]+D[YZY])*m0.z+(D[YYX]+D[YXY])*m0.x)/(2*cy);
+        h.z += sign*((D[YZZ]+D[YZZ])*m0.z+(D[YZX]+D[YXZ])*m0.x+(D[YZY]+D[YYZ])*m0.y)/(2*cy);
     }
 
-    // bottom neighbor
+
+    // z direction
     {
+        // bottom neighbor
         float3 m1 = make_float3(0.0f, 0.0f, 0.0f);
-        int i_ = idx(ix, iy, lclampz(iz-1));
         if (iz-1 >= 0 || PBCz) {
+            int i_ = idx(ix, iy, lclampz(iz-1));
             m1 = make_float3(mx[i_], my[i_], mz[i_]);
         }
-        if (!is0(m1)){
-            // bulk origin
-            h.x += ((D[ZXY]-D[ZYX])*m1.y+(D[ZXZ]-D[ZZX])*m1.z)/(2*cz) ;
-            h.y += ((D[ZYZ]-D[ZZY])*m1.z+(D[ZYX]-D[ZXY])*m1.x)/(2*cz) ;
-            h.z += ((D[ZZX]-D[ZXZ])*m1.x+(D[ZZY]-D[ZYZ])*m1.y)/(2*cz) ;
-            // boundary induced (cancels out if top neighbor exists as well)
-            h.x -= ((D[ZXX]+D[ZXX])*m0.x+(D[ZXY]+D[ZYX])*m0.y+(D[ZXZ]+D[ZZX])*m0.z)/(2*cx) ;
-            h.y -= ((D[ZYY]+D[ZYY])*m0.y+(D[ZYZ]+D[ZZY])*m0.z+(D[ZYX]+D[ZXY])*m0.x)/(2*cx) ;
-            h.z -= ((D[ZZZ]+D[ZZZ])*m0.z+(D[ZZX]+D[ZXZ])*m0.x+(D[ZZY]+D[ZYZ])*m0.y)/(2*cx) ;
-        }
-    }
-
-    // top neighbor
-    {
-        float3 m1 = make_float3(0.0f, 0.0f, 0.0f);
-        int i_ = idx(ix, iy, hclampz(iz+1));
+        // top neighbor
+        float3 m2 = make_float3(0.0f, 0.0f, 0.0f);
         if (iz+1 <Nz || PBCz) {
-            m1 = make_float3(mx[i_], my[i_], mz[i_]);
+            int i_ = idx(ix, iy, hclampz(iz+1));
+            m2 = make_float3(mx[i_], my[i_], mz[i_]);
         }
-        if (!is0(m1)){
-            // bulk origin
-            h.x -= ((D[ZXY]-D[ZYX])*m1.y+(D[ZXZ]-D[ZZX])*m1.z)/(2*cz) ;
-            h.y -= ((D[ZYZ]-D[ZZY])*m1.z+(D[ZYX]-D[ZXY])*m1.x)/(2*cz) ;
-            h.z -= ((D[ZZX]-D[ZXZ])*m1.x+(D[ZZY]-D[ZYZ])*m1.y)/(2*cz) ;
-            // boundary induced (cancels out if bottom neighbor exists as well)
-            h.x += ((D[ZXX]+D[ZXX])*m0.x+(D[ZXY]+D[ZYX])*m0.y+(D[ZXZ]+D[ZZX])*m0.z)/(2*cx) ;
-            h.y += ((D[ZYY]+D[ZYY])*m0.y+(D[ZYZ]+D[ZZY])*m0.z+(D[ZYX]+D[ZXY])*m0.x)/(2*cx) ;
-            h.z += ((D[ZZZ]+D[ZZZ])*m0.z+(D[ZZX]+D[ZXZ])*m0.x+(D[ZZY]+D[ZYZ])*m0.y)/(2*cx) ;
-        }
+        // bulk DMI
+        h.x += ((D[ZXY]-D[ZYX])*(m1.y-m2.y)+(D[ZXZ]-D[ZZX])*(m1.z-m2.z))/(2*cz);
+        h.y += ((D[ZYZ]-D[ZZY])*(m1.z-m2.z)+(D[ZYX]-D[ZXY])*(m1.x-m2.x))/(2*cz);
+        h.z += ((D[ZZX]-D[ZXZ])*(m1.x-m2.x)+(D[ZZY]-D[ZYZ])*(m1.y-m2.y))/(2*cz);
+        // boundary induced DMI
+        int sign = !is0(m1) - !is0(m2);
+        h.x += sign*((D[ZXX]+D[ZXX])*m0.x+(D[ZXY]+D[ZYX])*m0.y+(D[ZXZ]+D[ZZX])*m0.z)/(2*cz);
+        h.y += sign*((D[ZYY]+D[ZYY])*m0.y+(D[ZYZ]+D[ZZY])*m0.z+(D[ZYX]+D[ZXY])*m0.x)/(2*cz);
+        h.z += sign*((D[ZZZ]+D[ZZZ])*m0.z+(D[ZZX]+D[ZXZ])*m0.x+(D[ZZY]+D[ZYZ])*m0.y)/(2*cz);
     }
 
 

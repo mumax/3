89bfc0921f75d4f91f1adf44bc1897da7b068b2f
JLeliaert <jonathan.leliaert@gmail.com>
2017-08-05 17:24:30 +0200

    added rk56 solver

---
+++ engine/rk56.go
@@ -0,0 +1,137 @@
+package engine
+
+import (
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/data"
+	"github.com/mumax/3/util"
+	"math"
+)
+
+type RK56 struct {
+}
+
+func (rk *RK56) Step() {
+
+	m := M.Buffer()
+	size := m.Size()
+
+	if FixDt != 0 {
+		Dt_si = FixDt
+	}
+
+	t0 := Time
+	// backup magnetization
+	m0 := cuda.Buffer(3, size)
+	defer cuda.Recycle(m0)
+	data.Copy(m0, m)
+
+	k1, k2, k3, k4, k5, k6, k7, k8 := cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size), cuda.Buffer(3, size)
+	defer cuda.Recycle(k1)
+	defer cuda.Recycle(k2)
+	defer cuda.Recycle(k3)
+	defer cuda.Recycle(k4)
+	defer cuda.Recycle(k5)
+	defer cuda.Recycle(k6)
+	defer cuda.Recycle(k7)
+	defer cuda.Recycle(k8)
+	//k2 will be recyled as k9
+
+	h := float32(Dt_si * GammaLL) // internal time step = Dt * gammaLL
+
+	// stage 1
+	torqueFn(k1)
+
+	// stage 2
+	Time = t0 + (1./6.)*Dt_si
+	cuda.Madd2(m, m, k1, 1, (1./6.)*h) // m = m*1 + k1*h/6
+	M.normalize()
+	torqueFn(k2)
+
+	// stage 3
+	Time = t0 + (4./15.)*Dt_si
+	cuda.Madd3(m, m0, k1, k2, 1, (4./75.)*h, (16./75.)*h)
+	M.normalize()
+	torqueFn(k3)
+
+	// stage 4
+	Time = t0 + (2./3.)*Dt_si
+	madd4(m, m0, k1, k2, k3, 1, (5./6.)*h, (-8./3.)*h, (5./2.)*h)
+	M.normalize()
+	torqueFn(k4)
+
+	// stage 5
+	Time = t0 + (4./5.)*Dt_si
+	madd5(m, m0, k1, k2, k3, k4, 1, (-8./5.)*h, (144./25.)*h, (-4.)*h, (16./25.)*h)
+	M.normalize()
+	torqueFn(k5)
+
+	// stage 6
+	Time = t0 + (1.)*Dt_si
+	madd6(m, m0, k1, k2, k3, k4, k5, 1, (361./320.)*h, (-18./5.)*h, (407./128.)*h, (-11./80.)*h, (55./128.)*h)
+	M.normalize()
+	torqueFn(k6)
+
+	// stage 7
+	Time = t0
+	madd5(m, m0, k1, k3, k4, k5, 1, (-11./640.)*h, (11./256.)*h, (-11/160.)*h, (11./256.)*h)
+	M.normalize()
+	torqueFn(k7)
+
+	// stage 8
+	Time = t0 + (1.)*Dt_si
+	madd7(m, m0, k1, k2, k3, k4, k5, k7, 1, (93./640.)*h, (-18./5.)*h, (803./256.)*h, (-11./160.)*h, (99./256.)*h, (1.)*h)
+	M.normalize()
+	torqueFn(k8)
+
+
+	// stage 9: 6th order solution
+	Time = t0 + (1.)*Dt_si
+	madd6(m, m0, k1, k3, k4, k5, k6, 1, (31./384.)*h, (1125./2816.)*h, (9./32.)*h, (125./768.)*h, (5./66.)*h)
+	M.normalize()
+	torqueFn(k2) // re-use k2
+
+	// error estimate
+	Err := cuda.Buffer(3, size)
+	defer cuda.Recycle(Err)
+	madd5(Err, k2, k1, k6, k7, k8, 1, (-5./66.), (-5./66.), (5./66.), (5./66.))
+
+	// determine error
+	err := cuda.MaxVecNorm(Err) * float64(h)
+
+	// adjust next time step
+	if err < MaxErr || Dt_si <= MinDt || FixDt != 0 { // mindt check to avoid infinite loop
+		// step OK
+		setLastErr(err)
+		setMaxTorque(k7)
+		NSteps++
+		Time = t0 + Dt_si
+		adaptDt(math.Pow(MaxErr/err, 1./6.))
+	} else {
+		// undo bad step
+		//util.Println("Bad step at t=", t0, ", err=", err)
+		util.Assert(FixDt == 0)
+		Time = t0
+		data.Copy(m, m0)
+		NUndone++
+		adaptDt(math.Pow(MaxErr/err, 1./7.))
+	}
+}
+
+func (rk *RK56) Free() {
+}
+
+// TODO: into cuda
+func madd5(dst, src1, src2, src3, src4, src5 *data.Slice, w1, w2, w3, w4, w5 float32) {
+	cuda.Madd3(dst, src1, src2, src3, w1, w2, w3)
+	cuda.Madd3(dst, dst, src4, src5, 1, w4, w5)
+}
+
+func madd6(dst, src1, src2, src3, src4, src5, src6 *data.Slice, w1, w2, w3, w4, w5, w6 float32) {
+	madd5(dst, src1, src2, src3, src4, src5, w1, w2, w3, w4, w5)
+	cuda.Madd2(dst, dst, src6, 1, w6)
+}
+
+func madd7(dst, src1, src2, src3, src4, src5, src6, src7 *data.Slice, w1, w2, w3, w4, w5, w6, w7 float32) {
+	madd5(dst, src1, src2, src3, src4, src5, w1, w2, w3, w4, w5)
+	cuda.Madd3(dst, dst, src6, src7, 1, w6, w7)
+}

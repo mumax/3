ebfe7de2cb77e8e2ac1a01fe00d57b69ae47a482
JLeliaert <jonathan.leliaert@gmail.com>
2017-08-05 17:19:44 +0200

    added adaptive timestep for nonzero temperatures

--- engine/temperature.go
+++ engine/temperature.go
@@ -70,9 +70,21 @@ func (b *thermField) update() {
 		return
 	}
 
-	if FixDt == 0 {
-		util.Fatal("Finite temperature requires fixed time step. Set FixDt != 0.")
-	}
+
+        // after a bad step the timestep is rescaled and the noise should be rescaled accordingly
+        if NSteps == b.step && Dt_si != b.dt {
+                for c := 0; c < 3; c++ {
+                        cuda.Madd2(b.noise.Comp(c), b.noise.Comp(c), b.noise.Comp(c), float32(math.Sqrt(b.dt/Dt_si)),0.)
+                }
+                b.dt = Dt_si
+                return
+        }
+
+        //uncomment to not allow adaptive step
+        //if FixDt == 0 {
+        //      util.Fatal("Finite temperature requires fixed time step. Set FixDt != 0.")
+        //}
+
 
 	N := Mesh().NCell()
 	k2_VgammaDt := 2 * mag.Kb / (GammaLL * cellVolume() * Dt_si)

27f2efe46036da599603c4f7ce54694f9328cf5c
Mykola Dvornik <mykola.dvornik@gmail.com>
2017-08-18 15:32:08 +0200

    engine: introduce 'FixedLayerPosition' user-defined variable that allows one to instruct mumax3 how the fixed layer is placed with respect to the free one. If FIXEDLAYER_TOP (default) value is used, then nothing is changed, i.e. one needs to apply negative current to reverse parallel layers. If FIXEDLAYER_BOTTOM value is used, then the sign of the Slonczewski torque is reversed, i.e. one needs to apply positive current to reverse parallel layers.

--- engine/torque.go
+++ engine/torque.go
@@ -4,16 +4,16 @@ import (
 	"github.com/mumax/3/cuda"
 	"github.com/mumax/3/data"
 	"github.com/mumax/3/util"
+	"reflect"
 )
 
 var (
-	Alpha        = NewScalarParam("alpha", "", "Landau-Lifshitz damping constant")
-	Xi           = NewScalarParam("xi", "", "Non-adiabaticity of spin-transfer-torque")
-	Pol          = NewScalarParam("Pol", "", "Electrical current polarization")
-	Lambda       = NewScalarParam("Lambda", "", "Slonczewski Λ parameter")
-	EpsilonPrime = NewScalarParam("EpsilonPrime", "", "Slonczewski secondairy STT term ε'")
-	FrozenSpins  = NewScalarParam("frozenspins", "", "Defines spins that should be fixed") // 1 - frozen, 0 - free. TODO: check if it only contains 0/1 values
-
+	Alpha                            = NewScalarParam("alpha", "", "Landau-Lifshitz damping constant")
+	Xi                               = NewScalarParam("xi", "", "Non-adiabaticity of spin-transfer-torque")
+	Pol                              = NewScalarParam("Pol", "", "Electrical current polarization")
+	Lambda                           = NewScalarParam("Lambda", "", "Slonczewski Λ parameter")
+	EpsilonPrime                     = NewScalarParam("EpsilonPrime", "", "Slonczewski secondairy STT term ε'")
+	FrozenSpins                      = NewScalarParam("frozenspins", "", "Defines spins that should be fixed") // 1 - frozen, 0 - free. TODO: check if it only contains 0/1 values
 	FixedLayer                       = NewExcitation("FixedLayer", "", "Slonczewski fixed layer polarization")
 	Torque                           = NewVectorField("torque", "T", "Total torque/γ0", SetTorque)
 	LLTorque                         = NewVectorField("LLtorque", "T", "Landau-Lifshitz torque/γ0", SetLLTorque)
@@ -24,6 +24,7 @@ var (
 	Precess                          = true
 	DisableZhangLiTorque             = false
 	DisableSlonczewskiTorque         = false
+	fixedLayerPosition               = FIXEDLAYER_TOP // instructs mumax3 how free and fixed layers are stacked along +z direction
 )
 
 func init() {
@@ -33,6 +34,9 @@ func init() {
 	DeclVar("DisableZhangLiTorque", &DisableZhangLiTorque, "Disables Zhang-Li torque (default=false)")
 	DeclVar("DisableSlonczewskiTorque", &DisableSlonczewskiTorque, "Disables Slonczewski torque (default=false)")
 	DeclVar("DoPrecess", &Precess, "Enables LL precession (default=true)")
+	DeclLValue("FixedLayerPosition", &flposition{}, "Position of the fixed layer: FIXEDLAYER_TOP, FIXEDLAYER_BOTTOM (default=FIXEDLAYER_TOP)")
+	DeclROnly("FIXEDLAYER_TOP", FIXEDLAYER_TOP, "FixedLayerPosition = FIXEDLAYER_TOP instructs mumax3 that fixed layer is on top of the free layer")
+	DeclROnly("FIXEDLAYER_BOTTOM", FIXEDLAYER_BOTTOM, "FixedLayerPosition = FIXEDLAYER_BOTTOM instructs mumax3 that fixed layer is underneath of the free layer")
 }
 
 // Sets dst to the current total torque
@@ -97,7 +101,9 @@ func AddSTTorque(dst *data.Slice) {
 		epsPrime := EpsilonPrime.MSlice()
 		defer epsPrime.Recycle()
 		cuda.AddSlonczewskiTorque2(dst, M.Buffer(),
-			msat, j, fixedP, alpha, pol, lambda, epsPrime, Mesh())
+			msat, j, fixedP, alpha, pol, lambda, epsPrime,
+			CurrentSignFromFixedLayerPosition[fixedLayerPosition],
+			Mesh())
 	}
 }
 
@@ -112,3 +118,23 @@ func GetMaxTorque() float64 {
 	defer cuda.Recycle(torque)
 	return cuda.MaxVecNorm(torque)
 }
+
+type FixedLayerPosition int
+
+const (
+	FIXEDLAYER_TOP FixedLayerPosition = iota + 1
+	FIXEDLAYER_BOTTOM
+)
+
+var (
+	CurrentSignFromFixedLayerPosition = map[FixedLayerPosition]float64{
+		FIXEDLAYER_TOP:    1.0,
+		FIXEDLAYER_BOTTOM: -1.0,
+	}
+)
+
+type flposition struct{}
+
+func (*flposition) Eval() interface{}      { return fixedLayerPosition }
+func (*flposition) SetValue(v interface{}) { drainOutput(); fixedLayerPosition = v.(FixedLayerPosition) }
+func (*flposition) Type() reflect.Type     { return reflect.TypeOf(FixedLayerPosition(FIXEDLAYER_TOP)) }

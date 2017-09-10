3dbe7a05fb8295f91324115ec99c43f50ff63703
JLeliaert <jonathan.leliaert@gmail.com>
2017-09-10 12:52:35 +0200

    added ext_centerbubble

--- engine/ext_centerbubble.go
+++ engine/ext_centerbubble.go
@@ -2,6 +2,7 @@ package engine
 
 import (
 	"github.com/mumax/3/data"
+	"math"
 )
 
 func init() {
@@ -12,37 +13,35 @@ func centerBubble() {
 	M := &M
 	n := Mesh().Size()
 
-	//TODO This part was copied from ext_bubblepos
 	m := M.Buffer()
 	mz := m.Comp(Z).HostCopy().Scalars()[0]
-	posx, posy := 0, 0
+
+	posx, posy := 0., 0.
 
 	{
-		max := float32(-1e32)
+		var magsum float32
+		var weightedsum float32
+
 		for iy := range mz {
-			var sum float32
-			for ix := range mz[iy] {
-				sum += mz[iy][ix]
-			}
-			if sum > max {
-				posy = iy
-				max = sum
+			for ix := range mz[0] {
+				magsum += ((mz[iy][ix] + 1.) / 2.)
+				weightedsum += ((mz[iy][ix] + 1.) / 2.) * float32(iy)
 			}
 		}
+		posy = float64(weightedsum / magsum)
 	}
 
 	{
-		max := float32(-1e32)
+		var magsum float32
+		var weightedsum float32
+
 		for ix := range mz[0] {
-			var sum float32
 			for iy := range mz {
-				sum += mz[iy][ix]
-			}
-			if sum > max {
-				posx = ix
-				max = sum
+				magsum += ((mz[iy][ix] + 1.) / 2.)
+				weightedsum += ((mz[iy][ix] + 1.) / 2.) * float32(ix)
 			}
 		}
+		posx = float64(weightedsum / magsum)
 	}
 
 	zero := data.Vector{0, 0, 0}
@@ -53,8 +52,8 @@ func centerBubble() {
 		ShiftMagD[Z] = float64(sign)
 		ShiftMagU[Z] = float64(sign)
 	}
-	dx := n[X]/2 - posx
-	dy := n[Y]/2 - posy
+	dx := int(math.Floor(float64(n[X]/2) - posx))
+	dy := int(math.Floor(float64(n[Y]/2) - posy))
 
 	//put bubble to center
 	if dx != 0 {

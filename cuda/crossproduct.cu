abcf521ce48f65de127d5971bd91a4005c79afa8
Jonathan Leliaert <jonathan.leliaert@gmail.com>
2017-09-26 09:39:41 +0200

    Update crossproduct.cu
    Improved performance as suggested by @godsic

--- cuda/crossproduct.cu
+++ cuda/crossproduct.cu
@@ -10,9 +10,10 @@ crossproduct(float* __restrict__  dstx, float* __restrict__  dsty, float* __rest
     if (i < N) {
         float3 A = {ax[i], ay[i], az[i]};
         float3 B = {bx[i], by[i], bz[i]};
-        dstx[i] = cross(A, B).x;
-        dsty[i] = cross(A, B).y;
-        dstz[i] = cross(A, B).z;
+        float3 AxB = cross(A, B);
+        dstx[i] = AxB.x;
+        dsty[i] = AxB.y;
+        dstz[i] = AxB.z;
     }
 }
 

b2e4698fb87a9de3939ce5e667ff61ddf7f71fc1
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2017-09-29 13:38:03 +0200

    use bubblepos in centerbubble to avoid duplicate code

--- engine/ext_centerbubble.go
+++ engine/ext_centerbubble.go
@@ -10,41 +10,15 @@ func init() {
 }
 
 func centerBubble() {
-	M := &M
 	n := Mesh().Size()
+	c := Mesh().CellSize()
 
-	m := M.Buffer()
-	mz := m.Comp(Z).HostCopy().Scalars()[0]
+	position := bubblePos()
+	var centerIdx [2]int
+	centerIdx[X] = int(math.Floor((position[X] - GetShiftPos()) / c[X]))
+	centerIdx[Y] = int(math.Floor((position[Y] - GetShiftYPos()) / c[Y]))
 
-	posx, posy := 0., 0.
 	sign := magsign(M.GetCell(0, n[Y]/2, n[Z]/2)[Z]) //TODO make more robust with temperature?
-
-	{
-		var magsum float32
-		var weightedsum float32
-
-		for iy := range mz {
-			for ix := range mz[0] {
-				magsum += ((mz[iy][ix]*float32(-1*sign) + 1.) / 2.)
-				weightedsum += ((mz[iy][ix]*float32(-1*sign) + 1.) / 2.) * float32(iy)
-			}
-		}
-		posy = float64(weightedsum / magsum)
-	}
-
-	{
-		var magsum float32
-		var weightedsum float32
-
-		for ix := range mz[0] {
-			for iy := range mz {
-				magsum += ((mz[iy][ix]*float32(-1*sign) + 1.) / 2.)
-				weightedsum += ((mz[iy][ix]*float32(-1*sign) + 1.) / 2.) * float32(ix)
-			}
-		}
-		posx = float64(weightedsum / magsum)
-	}
-
 	zero := data.Vector{0, 0, 0}
 	if ShiftMagL == zero || ShiftMagR == zero || ShiftMagD == zero || ShiftMagU == zero {
 		ShiftMagL[Z] = float64(sign)
@@ -52,15 +26,13 @@ func centerBubble() {
 		ShiftMagD[Z] = float64(sign)
 		ShiftMagU[Z] = float64(sign)
 	}
-	dx := int(math.Floor(float64(n[X]/2) - posx))
-	dy := int(math.Floor(float64(n[Y]/2) - posy))
 
 	//put bubble to center
-	if dx != 0 {
-		Shift(dx)
+	if centerIdx[X] != 0 {
+		Shift(-centerIdx[X])
 	}
-	if dy != 0 {
-		YShift(dy)
+	if centerIdx[Y] != 0 {
+		YShift(-centerIdx[Y])
 	}
 
 }

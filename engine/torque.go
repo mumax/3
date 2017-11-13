821d90b6591fe9f0db873d67fff623912b2b04d8
aleste2 <victor@usal.es>
2017-11-13 09:45:34 +0100

    LLB + JH

--- engine/torque.go
+++ engine/torque.go
@@ -46,6 +46,30 @@ func SetTorque(dst *data.Slice) {
 	FreezeSpins(dst)
 }
 
+
+// Sets dst to the current Landau-Lifshitz-Bloch torque
+func SetLLBTorque(dst *data.Slice,hth1 *data.Slice,hth2 *data.Slice) {
+	SetEffectiveField(dst) // calc and store B_eff
+	alpha := Alpha.MSlice()
+	defer alpha.Recycle()
+	TCurie := TCurie.MSlice()
+	defer TCurie.Recycle()
+	Msat := Msat.MSlice()
+	defer Msat.Recycle()
+	Temp := Temp.MSlice()
+	defer Temp.Recycle()
+
+        cuda.Zero(hth1)       
+        B_therm.AddTo(hth1)
+        cuda.Zero(hth2)       
+        B_therm.AddTo(hth2)
+	if Precess {
+		cuda.LLBTorque(dst, M.Buffer(), dst, Temp,alpha,TCurie,Msat,hth1,hth2) // overwrite dst with torque
+	} else {
+		cuda.LLNoPrecess(dst, M.Buffer(), dst)
+	}
+}
+
 // Sets dst to the current Landau-Lifshitz torque
 func SetLLTorque(dst *data.Slice) {
 	SetEffectiveField(dst) // calc and store B_eff

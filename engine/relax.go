32a15318bf04f6d54f33128540e435dc8dabad64
jsampaio <jsampaio@users.noreply.github.com>
2018-01-11 12:21:20 +0100

    Update relax.go
    changing stopping criterion

--- engine/relax.go
+++ engine/relax.go
@@ -7,8 +7,12 @@ import (
 	"math"
 )
 
+var thresholdTorque := 1e-3; //to be replaced by a sane default value
+
 func init() {
 	DeclFunc("Relax", Relax, "Try to minimize the total energy")
+	DeclVar("RelaxThresholdTorque", &thresholdTorque, "Stopping maximum torque threshold for relax()")
+
 }
 
 // are we relaxing?
@@ -55,26 +59,23 @@ func Relax() {
 	}
 
 	// Now we are already close to equilibrium, but energy is too noisy to be used any further.
-	// So now we minimize the total torque which is less noisy and does not have to cross any
-	// bumps once we are close to equilibrium.
+	// So now we minimize the maximum torque which is less noisy.
 	solver := stepper.(*RK23)
 	defer stepper.Free() // purge previous rk.k1 because FSAL will be dead wrong.
-	avgTorque := func() float32 {
-		return cuda.Dot(solver.k1, solver.k1)
-	}
-	var T0, T1 float32 = 0, avgTorque()
-
-	// Step as long as torque goes down. Then increase the accuracy and step more.
-	for MaxErr > 1e-9 && !pause {
-		MaxErr /= math.Sqrt2
-		relaxSteps(N) // TODO: Play with other values
-		T0, T1 = T1, avgTorque()
-		for T1 < T0 && !pause {
-			relaxSteps(N) // TODO: Play with other values
-			T0, T1 = T1, avgTorque()
+	
+	maxTorque := func() float32 {
+		return cuda.MaxVecNorm(solver.k1) //perhaps replace by (faster?) sqrt(3)*cuda.MaxAbs(...)? 
+    	}
+	// run as long as the max torque is above threshold. Then increase the accuracy and step more.
+	for !pause {
+		for maxTorque() < thresholdTorque && !pause {
+		    relaxSteps(N)
 		}
-	}
-
+		MaxErr /= math.Sqrt2
+		if MaxErr < 1e-9 { 
+			break
+		}
+    	}
 	pause = true
 }
 

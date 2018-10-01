c8d594a5ac741fc97dc561b520311adf6022f333
JLeliaert <jonathan.leliaert@gmail.com>
2018-10-01 11:22:30 +0200

    fixed cuda version statement

--- cuda/cu/version.go
+++ cuda/cu/version.go
@@ -3,14 +3,17 @@ package cu
 // This file implements CUDA driver version management
 
 //#include <cuda.h>
+//#include <cuda_runtime_api.h>
 import "C"
 
 // Returns the CUDA driver version.
 func Version() int {
-	var version C.int
-	err := Result(C.cuDriverGetVersion(&version))
-	if err != SUCCESS {
-		panic(err)
-	}
-	return int(version)
+        var version C.int
+        err := Result(C.cuDriverGetVersion(&version))
+        if err != SUCCESS {
+                panic(err)
+        }
+        cu_version := Result(C.CUDART_VERSION)
+        return int(cu_version)
 }
+

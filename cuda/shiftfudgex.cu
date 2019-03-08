eeb82e81765e172a3657c133bc37673da90c09f1
jsampaio <jsampaio@users.noreply.github.com>
2019-03-08 18:08:25 +0100

    shift magnetisation using the current values at the border

---
+++ cuda/shiftfudgex.cu
@@ -0,0 +1,25 @@
+#include "stencil.h"
+
+// shift dst by shx cells (positive or negative) along X-axis.
+// new edge value is clampL at left edge or clampR at right edge.
+extern "C" __global__ void
+shiftfudgex(float* __restrict__  dst, float* __restrict__  src,
+       int Nx,  int Ny,  int Nz, int shx) {
+
+    int ix = blockIdx.x * blockDim.x + threadIdx.x;
+    int iy = blockIdx.y * blockDim.y + threadIdx.y;
+    int iz = blockIdx.z * blockDim.z + threadIdx.z;
+
+    if(ix < Nx && iy < Ny && iz < Nz) {
+        int ix2 = ix-shx;
+        float newval;
+        if (ix2 < 0) {
+            newval = src[idx(0, iy, iz)];
+        } else if (ix2 >= Nx) {
+            newval = src[idx(Nx-1, iy, iz)];
+        } else {
+            newval = src[idx(ix2, iy, iz)];
+        }
+        dst[idx(ix, iy, iz)] = newval;
+    }
+}

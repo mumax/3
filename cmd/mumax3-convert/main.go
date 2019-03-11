71ebf5f50910f8116f0885847089dd570f7305e6
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2019-03-11 18:55:56 +0100

    add numpy output format in mumax3-convert

--- cmd/mumax3-convert/main.go
+++ cmd/mumax3-convert/main.go
@@ -67,6 +67,7 @@ var (
 	flag_vtk       = flag.String("vtk", "", `"ascii" or "binary" VTK output`)
 	flag_dump      = flag.Bool("dump", false, `output in dump format`)
 	flag_csv       = flag.Bool("csv", false, `output in CSV format`)
+	flag_numpy     = flag.Bool("numpy", false, "Numpy output")
 	flag_json      = flag.Bool("json", false, `output in JSON format`)
 	flag_min       = flag.String("min", "auto", `Minimum of color scale: "auto" or value.`)
 	flag_max       = flag.String("max", "auto", `Maximum of color scale: "auto" or value.`)
@@ -266,6 +267,7 @@ var outputs = map[*bool]output{
 	flag_gnuplot: {".gplot", dumpGnuplot},
 	flag_dump:    {".dump", outputDUMP},
 	flag_csv:     {".csv", dumpCSV},
+	flag_numpy:   {".npy", dumpNUMPY},
 	flag_json:    {".json", dumpJSON},
 	flag_show:    {"", show},
 }

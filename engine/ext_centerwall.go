c22e9009ab41f74f7aae1380e9e2d59a46d34a1d
jsampaio <jsampaio@users.noreply.github.com>
2019-03-11 10:53:57 +0100

    ext_centerWall can follow only one region or one layer
    This change adds the possibility to ext_centerWall to follow only one region or one layer, which is useful for simulations of SAF (like the RKKY example) or odd-shaped geometries.
    This change adds the following script functions:
    ext_centerWallInRegion( region, component)
    ext_centerWallInLayer( layer, component)

--- engine/ext_centerwall.go
+++ engine/ext_centerwall.go
@@ -14,6 +14,7 @@ var (
 func init() {
 	DeclFunc("ext_centerWall", CenterWall, "centerWall(c) shifts m after each step to keep m_c close to zero")
 	DeclFunc("ext_centerWallInRegion", CenterWallInRegion, "centerWallInRegion(R, c) shifts m after each step to keep m_c in region R close to zero")
+	DeclFunc("ext_CenterWallInLayer", CenterWallInLayer, "centerWallInLayer(L, c) shifts m after each step to keep m_c in layer L close to zero")
 }
 
 func centerWall(c int) {
@@ -47,6 +48,33 @@ func CenterWall(magComp int) {
 	PostStep(func() { centerWall(magComp) })
 }
 
+// The same functions as above, now for just one layer
+func centerWallInLayerProc(layer, c int) {
+	M := &M
+	mc := CropLayer(M,layer).average()[c]
+	n := Mesh().Size()
+	tolerance := 4 / float64(n[X]) // x*2 * expected <m> change for 1 cell shift
+
+	zero := data.Vector{0, 0, 0}
+	if ShiftMagL == zero || ShiftMagR == zero {
+		sign := magsign(M.GetCell(0, n[Y]/2, layer)[c])
+		ShiftMagL[c] = float64(sign)
+		ShiftMagR[c] = -float64(sign)
+	}
+
+	sign := magsign(ShiftMagL[c])
+
+	if mc < -tolerance {
+		Shift(sign)
+	} else if mc > tolerance {
+		Shift(-sign)
+	}
+}
+
+func CenterWallInLayer(layer, magComp int) {
+	PostStep(func() { centerWallInLayerProc(layer, magComp) })
+}
+
 // The same functions as above, now for just one region
 func centerWallInRegionProc(region, c int) {
 	M := &M
@@ -60,9 +88,9 @@ func centerWallInRegionProc(region, c int) {
 		ShiftMagL[c] = float64(sign)
 		ShiftMagR[c] = -float64(sign)
 	}
-	
+
 	sign := magsign(ShiftMagL[c])
-	
+
 	if mc < -tolerance {
 		Shift(sign)
 	} else if mc > tolerance {
@@ -73,7 +101,6 @@ func centerWallInRegionProc(region, c int) {
 func CenterWallInRegion(region, magComp int) {
 	PostStep(func() { centerWallInRegionProc(region, magComp) })
 }
-
 
 func magsign(x float64) int {
 	if x > 0.1 {

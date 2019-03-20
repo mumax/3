af58e00fc5caf6ac00cf8b12da3910a98abfc0b3
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2019-03-20 15:15:43 +0100

    turn vet off in test.bash
    The go test tool runs a vet check by default since go1.10.
    This vet check yields false negatives, so let's turn this off.

--- test.bash
+++ test.bash
@@ -2,8 +2,16 @@
 
 source ./make.bash || exit 1
 
-go test -i github.com/mumax/3/... || exit 1
-go test $PKGS  github.com/mumax/3/... || exit 1
+# The go test tool runs a vet check by default since go1.10.
+# This vet check yields false negatives, so let's turn this off.
+VETTESTFLAG=""
+GOMINORVERSION=$( go version | sed -r 's/.*go1.([0-9]*).*/\1/g' )
+if (( $GOMINORVERSION >= 10 )); then
+    VETTESTFLAG="-vet=off"
+fi
+
+go test $VETTESTFLAG -i github.com/mumax/3/... || exit 1
+go test $VETTESTFLAG $PKGS  github.com/mumax/3/... || exit 1
 
 (cd test && ./run.bash) || exit 1
 

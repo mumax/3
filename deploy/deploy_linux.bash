f323db53e4469ffe5ea5f8b2f7ca414e1944bffa
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-02-03 14:31:46 +0100

    deploy mumax3 for cuda versions 7.0-10.2

--- deploy/deploy_linux.bash
+++ deploy/deploy_linux.bash
@@ -1,5 +1,12 @@
 # The cuda versions against which we will compile mumax3
-for CUDAVERSION in 9.2 10.0 10.1 10.2; do
+for CUDAVERSION in 7.0 7.5 8.0 9.0 9.1 9.2 10.0 10.1 10.2; do
+
+    # downgrade host compiler for nvcc for old cuda versions
+    if [ 1 -eq "$(echo "${CUDAVERSION} < 9.2" | bc)" ]; then
+        export NVCC_CCBIN=/usr/bin/gcc-4.8
+    else
+        export NVCC_CCBIN=/usr/bin/gcc
+    fi
 
     # The final location of the mumax3 executables and libs
     BUILDDIR=./build/mumax3.10_linux_cuda${CUDAVERSION}
@@ -13,10 +20,15 @@ for CUDAVERSION in 9.2 10.0 10.1 10.2; do
     # All supported compute capabilities of this cuda version
     #   We export CUDA_CC so that cuda/Makefile knows what to include in the fat wrappers
     case $CUDAVERSION in
-        "9.2")  export CUDA_CC="30 32 35 37 50 52 53 60 61 62 70 72";;
-        "10.0") export CUDA_CC="30 32 35 37 50 52 53 60 61 62 70 72 75";;
-        "10.1") export CUDA_CC="30 32 35 37 50 52 53 60 61 62 70 72 75";;
-        "10.2") export CUDA_CC="30 32 35 37 50 52 53 60 61 62 70 72 75";;
+        "7.0")  export CUDA_CC="20 30 32 35 37 50 52 53";;
+        "7.5")  export CUDA_CC="20 30 32 35 37 50 52 53";;
+        "8.0")  export CUDA_CC="20 30 32 35 37 50 52 53 60 61 62";;
+        "9.0")  export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70";;
+        "9.1")  export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70 72";;
+        "9.2")  export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70 72";;
+        "10.0") export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70 72 75";;
+        "10.1") export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70 72 75";;
+        "10.2") export CUDA_CC="   30 32 35 37 50 52 53 60 61 62 70 72 75";;
     esac
 
     # The path for shared libraries (relative to the build directory)

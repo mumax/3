c8b450a43248ec347341e8688f6c467d077f0141
nov2oj <nov2oj@users.noreply.github.com>
2020-05-26 10:41:42 +0200

    Update topologicalchargelattice.cu

--- cuda/topologicalchargelattice.cu
+++ cuda/topologicalchargelattice.cu
@@ -6,9 +6,12 @@
 #include "stencil.h"
 
 // Set s to the toplogogical charge density for lattices
+// Based on the solid angle subtended by triangle associated with three spins: a,b,c
 // 	  s = 2 atan(a . b x c /(1 + a.b + a.c + b.c) )
 // After M Boettcher et al, New J Phys 20, 103014 (2018), adapted from
 // B. Berg and M. Luescher, Nucl. Phys. B 190, 412 (1981).
+// A unit cell comprises two triangles, but s is a site-dependent quantity so we
+// double-count and average over four triangles.
 // See topologicalchargelattice.go.
 extern "C" __global__ void
 settopologicalchargelattice(float* __restrict__ s,
@@ -27,7 +30,7 @@ settopologicalchargelattice(float* __restrict__ s,
     int I = idx(ix, iy, iz);                      // central cell index
 
     float3 m0 = make_float3(mx[I], my[I], mz[I]); // +0
-    float3 axb = make_float3(0.0f, 0.0f, 0.0f);   // a x b
+    float3 bxc = make_float3(0.0f, 0.0f, 0.0f);   // b x c
     int i_;                                       // neighbour index
 
     if(is0(m0))
@@ -42,7 +45,8 @@ settopologicalchargelattice(float* __restrict__ s,
     // 2: (i,j+1)
     // 3: (i-1,j)
     // 4: (i,j-1)
-    // The four triangles are 012, 023, 034, 041
+    // The four triangles are therefore 012, 023, 034, 041
+    // The index order is important to preserve the same measure of chirality
     float trig012, trig023, trig034, trig041;
     float numer, denom;
 
@@ -75,29 +79,30 @@ settopologicalchargelattice(float* __restrict__ s,
             m4 = make_float3(mx[i_], my[i_], mz[i_]);
         }
 
-        // We don't care whether the neighbours exist or not, since the dot and cross products will be zero
-        // 012
-        axb     = cross(m0, m1);
-        numer   = dot(m2, axb);
+        // We don't care whether the neighbours exist, since the dot and
+        // cross products will be zero if they don't
+        // Triangle 012
+        bxc     = cross(m1, m2);
+        numer   = dot(m0, bxc);
         denom   = 1.0f + dot(m0, m1) + dot(m0, m2) + dot(m1, m2);
         trig012 = 2.0f * atan2(numer, denom);
 
-        // 023
-        axb     = cross(m0, m2);
-        numer   = dot(m3, axb);
+        // Triangle 023
+        bxc     = cross(m2, m3);
+        numer   = dot(m0, bxc);
         denom   = 1.0f + dot(m0, m2) + dot(m0, m3) + dot(m2, m3);
         trig023 = 2.0f * atan2(numer, denom);
 
-        // 034
-        axb     = cross(m0, m3);
-        numer   = dot(m4, axb);
+        // Triangle 034
+        bxc     = cross(m3, m4);
+        numer   = dot(m0, bxc);
         denom   = 1.0f + dot(m0, m3) + dot(m0, m4) + dot(m3, m4);
         trig034 = 2.0f * atan2(numer, denom);
 
-        // 041
-        axb     = cross(m0, m4);
-        numer   = dot(m1, axb);
-        denom   = 1.0f + dot(m0, m1) + dot(m0, m4) + dot(m1, m4);
+        // Triangle 041
+        bxc     = cross(m4, m1);
+        numer   = dot(m0, bxc);
+        denom   = 1.0f + dot(m0, m4) + dot(m0, m1) + dot(m4, m1);
         trig041 = 2.0f * atan2(numer, denom);
     }
 

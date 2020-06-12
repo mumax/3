6bbc745839a6a7a94d2c4fd2af78470378e1b9b9
nov2oj <nov2oj@users.noreply.github.com>
2020-06-12 23:41:36 +0200

    Updates to include PBC and finite sized systems

--- cuda/topologicalchargelattice.go
+++ cuda/topologicalchargelattice.go
@@ -6,8 +6,14 @@ import (
 )
 
 // Set s to the toplogogical charge density for lattices
-// 	  s = 2 atan[(]a . b x c /(1 + a.b + a.c + b.c) ]/ (dx dy)
-// After M Boettcher et al, New J Phys 20, 103014 (2018)
+// Based on the solid angle subtended by triangle associated with three spins: a,b,c
+//        s = 2 atan[(a . b x c /(1 + a.b + a.c + b.c)] / (dx dy)
+// After M Boettcher et al, New J Phys 20, 103014 (2018), adapted from
+// B. Berg and M. Luescher, Nucl. Phys. B 190, 412 (1981).
+// A unit cell comprises two triangles, but s is a site-dependent quantity so we
+// double-count and average over four triangles.
+// This implementation works best for extended systems with periodic boundaries and provides a
+// workable definition of the local charge density.
 // See topologicalchargelattice.cu
 func SetTopologicalChargeLattice(s *data.Slice, m *data.Slice, mesh *data.Mesh) {
   cellsize := mesh.CellSize()

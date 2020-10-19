5aa6db08dac5dd511bf012a2288794857d6df053
Jeroen Mulkers <jeroen.mulkers@gmail.com>
2020-10-19 15:58:41 +0200

    use gonum/plot instead of gnuplot
    The plot in the web gui is no longer created by a gnuplot subprocess.
    The gonum/plot package is used to create the plot instead.

--- engine/plot.go
+++ engine/plot.go
@@ -3,18 +3,18 @@ package engine
 import (
 	"bytes"
 	"errors"
-	"fmt"
-	"github.com/mumax/3/httpfs"
 	"image"
+	"image/color"
 	"image/png"
-	"io/ioutil"
 	"net/http"
-	"os/exec"
-	"sync/atomic"
-)
+	"strconv"
+	"strings"
 
-var nPlots int32       // counts number of active gnuplot processes
-const MAX_GNUPLOTS = 5 // maximum allowed number of gnuplot processes
+	"gonum.org/v1/plot"
+	"gonum.org/v1/plot/plotter"
+	"gonum.org/v1/plot/vg"
+	"gonum.org/v1/plot/vg/draw"
+)
 
 func (g *guistate) servePlot(w http.ResponseWriter, r *http.Request) {
 
@@ -24,68 +24,73 @@ func (g *guistate) servePlot(w http.ResponseWriter, r *http.Request) {
 	handle := func(err error) bool {
 		if err != nil {
 			w.Write(emptyIMG())
-			g.Set("plotErr", err.Error()+string(out))
+			g.Set("plotErr", "Plot error: "+err.Error()+string(out))
 			return true
 		} else {
 			return false
 		}
 	}
 
-	// limit max processes
-	atomic.AddInt32(&nPlots, 1)
-	defer atomic.AddInt32(&nPlots, -1)
-	if atomic.LoadInt32(&nPlots) > MAX_GNUPLOTS {
-		handle(errors.New("too many gnuplot processes"))
-		return
-	}
-
-	a := g.StringValue("usingx")
-	b := g.StringValue("usingy")
-
-	cmd := "gnuplot"
-	args := []string{"-e", fmt.Sprintf(`set format x "%%g"; set key off; set format y "%%g"; set term svg size 480,320 font 'Arial,10'; plot "-" u %v:%v w li; set output;exit;`, a, b)}
-	excmd := exec.Command(cmd, args...)
-
-	stdin, err := excmd.StdinPipe()
+	data, err := Table.Read()
 	if handle(err) {
 		return
 	}
 
-	stdout, err := excmd.StdoutPipe()
+	xColIdx, err := strconv.Atoi(strings.TrimSpace(g.StringValue("usingx")))
+	if err != nil || xColIdx < 0 || xColIdx >= len(data[0]) {
+		handle(errors.New("Invalid column index"))
+		return
+	}
+
+	yColIdx, err := strconv.Atoi(strings.TrimSpace(g.StringValue("usingy")))
+	if err != nil || yColIdx < 0 || yColIdx >= len(data[0]) {
+		handle(errors.New("Invalid column index"))
+		return
+	}
+
+	p, err := plot.New()
 	if handle(err) {
 		return
 	}
 
-	data, err := httpfs.Read(fmt.Sprintf(`%vtable.txt`, OD()))
-	if handle(err) {
-		return
+	header := Table.Header()
+	p.X.Label.Text = header[xColIdx].String()
+	p.Y.Label.Text = header[yColIdx].String()
+	p.X.Label.Padding = 0.2 * vg.Inch
+	p.Y.Label.Padding = 0.2 * vg.Inch
+
+	nPoints := len(data)
+	points := make(plotter.XYs, nPoints)
+	for i := 0; i < nPoints; i++ {
+		points[i].X = data[i][xColIdx]
+		points[i].Y = data[i][yColIdx]
 	}
 
-	err = excmd.Start()
+	lpLine, lpPoints, err := plotter.NewLinePoints(points)
 	if handle(err) {
 		return
 	}
-	defer excmd.Wait()
+	lpLine.Color = color.RGBA{R: 255, G: 150, B: 150, A: 255}
+	lpLine.Width = 2
+	lpPoints.Color = color.RGBA{R: 255, G: 0, B: 0, A: 255}
+	lpPoints.Shape = draw.CircleGlyph{}
+	lpPoints.Radius = 2
 
-	_, err = stdin.Write(data)
-	if handle(err) {
-		return
-	}
-	err = stdin.Close()
-	if handle(err) {
-		return
-	}
+	p.Add(lpLine, lpPoints)
 
-	out, err = ioutil.ReadAll(stdout)
+	wr, err := p.WriterTo(6*vg.Inch, 4*vg.Inch, "svg")
 	if handle(err) {
 		return
 	}
 
 	w.Header().Set("Content-Type", "image/svg+xml")
-	w.Write(out)
+	_, err = wr.WriteTo(w)
+	if handle(err) {
+		return
+	}
+
 	g.Set("plotErr", "")
 	return
-
 }
 
 var empty_img []byte

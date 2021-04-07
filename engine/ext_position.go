894bc11b414488c2d123d6466cc4a9d4d0fb9c50
jake love <jake.love.mail@gmail.com>
2021-04-07 12:48:04 +0200

    Fixed fence-post error in PositionLocal() where I was using posts instead of fences

--- engine/ext_position.go
+++ engine/ext_position.go
@@ -12,25 +12,32 @@ var (
 	PosWorld = NewVectorField("PositionWorld", "m", "Vector field containing real world position of each cell. The Origin is at the center of the mesh", PositionWorld)
 )
 
+func decrement_safe(x int) int {
+	if x > 1 {
+		x = x - 1
+	}
+
+	return x
+}
+
 func PositionLocal(dst *data.Slice) {
 
 	dims := Mesh().Size()
 
-	sx := 1.0 / float32(dims[X])
-	sy := 1.0 / float32(dims[Y])
-	sz := 1.0 / float32(dims[Z])
+	sx := 1.0 / float32(decrement_safe(dims[X]))
+	sy := 1.0 / float32(decrement_safe(dims[Y]))
+	sz := 1.0 / float32(decrement_safe(dims[Z]))
 
 	cuda.SpatialField(dst, sx, sy, sz)
 }
 
 func PositionWorld(dst *data.Slice) {
 
-	dims := Mesh().Size()
 	cellsize := Mesh().CellSize()
 
-	sx := float32(cellsize[X]) / float32(dims[X])
-	sy := float32(cellsize[Y]) / float32(dims[Y])
-	sz := float32(cellsize[Z]) / float32(dims[Z])
+	sx := float32(cellsize[X])
+	sy := float32(cellsize[Y])
+	sz := float32(cellsize[Z])
 
 	cuda.SpatialField(dst, sx, sy, sz)
 }

df28acdef61ba140af2f9ae68b81b8fd6d694867
João S <jsampaio@users.noreply.github.com>
2022-11-03 16:34:46 +0100

    Update zhangli2.cu
    changed the zhangli to use the user-defined gammaLL and not the constant gamma0. This was a bug, that causes a wrong calculation of the Zhang-Li torque in the cases that the user changes gammaLL.

--- cuda/zhangli2.cu
+++ cuda/zhangli2.cu
@@ -4,7 +4,8 @@
 #include "stencil.h"
 #include <stdint.h>
 
-#define PREFACTOR ((MUB) / (2 * QE * GAMMA0))
+// #define PREFACTOR ((MUB) / (2 * QE * GAMMA0))
+#define PREFACTOR ((HBAR) / (2 * QE))
 
 // spatial derivatives without dividing by cell size
 #define deltax(in) (in[idx(hclampx(ix+1), iy, iz)] - in[idx(lclampx(ix-1), iy, iz)])
@@ -21,6 +22,7 @@ addzhanglitorque2(float* __restrict__ tx, float* __restrict__ ty, float* __restr
                   float* __restrict__ alpha_, float alpha_mul,
                   float* __restrict__ xi_, float xi_mul,
                   float* __restrict__ pol_, float pol_mul,
+                  float* __restrict__ g_, float g_mul,
                   float cx, float cy, float cz,
                   int Nx, int Ny, int Nz, uint8_t PBC) {
 
@@ -37,8 +39,9 @@ addzhanglitorque2(float* __restrict__ tx, float* __restrict__ ty, float* __restr
     float alpha = amul(alpha_, alpha_mul, i);
     float xi    = amul(xi_, xi_mul, i);
     float pol   = amul(pol_, pol_mul, i);
+    float g     = amul(g_, g_mul, i);
     float invMs = inv_Msat(Ms_, Ms_mul, i);
-    float b = invMs * PREFACTOR / (1.0f + xi*xi);
+    float b = invMs * PREFACTOR / (g*(1.0f + xi*xi));
     float3 J = pol*vmul(jx_, jy_, jz_, jx_mul, jy_mul, jz_mul, i);
 
     float3 hspin = make_float3(0.0f, 0.0f, 0.0f); // (u·∇)m
@@ -62,4 +65,3 @@ addzhanglitorque2(float* __restrict__ tx, float* __restrict__ ty, float* __restr
     ty[i] += torque.y;
     tz[i] += torque.z;
 }
-

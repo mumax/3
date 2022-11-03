7351b864b8dde4d8c43e036b17d8eaaf3b15d469
João S <jsampaio@users.noreply.github.com>
2022-11-03 16:30:05 +0100

    Update lltorque.go
    added function ScaleGamma, which is used to implement the region-wise gamma factor.

--- cuda/lltorque.go
+++ cuda/lltorque.go
@@ -20,6 +20,16 @@ func LLTorque(torque, m, B *data.Slice, alpha MSlice) {
 		alpha.DevPtr(0), alpha.Mul(0), N, cfg)
 }
 
+// Scale up the torques by GFactor (to allow regionwise g)
+func ScaleGamma(torque *data.Slice, GFactor MSlice) {
+	N := torque.Len()
+	cfg := make1DConf(N)
+
+	k_scalegamma_async(torque.DevPtr(X), torque.DevPtr(Y), torque.DevPtr(Z),
+                GFactor.DevPtr(0), GFactor.Mul(0), N, cfg)
+
+}
+
 // Landau-Lifshitz torque with precession disabled.
 // Used by engine.Relax().
 func LLNoPrecess(torque, m, B *data.Slice) {

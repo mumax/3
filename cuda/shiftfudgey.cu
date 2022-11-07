cf650b691cf30ed1a5c677bf02f634b22632a987
João S <jsampaio@users.noreply.github.com>
2022-11-07 14:51:07 +0100

    copy the border cells when shifting M
    Adds a new shift mode, that copies the values at the border cells when shifting the magnetisation instead of using the values stored in ShiftMagL/R.
    Modified functions: engine.Shift
    new functions: cuda.ShiftFudgeX, cuda.ShiftFudgeY (and related .cu, .ptx files).

---
+++ cuda/shiftfudgey.cu
@@ -0,0 +1,25 @@
+#include "stencil.h"
+
+ // shift dst by shy cells (positive or negative) along Y-axis.
+ // new edge value is the current edge value.
+ extern "C" __global__ void
+ shiftfudgey(float* __restrict__  dst, float* __restrict__  src,
+        int Nx,  int Ny,  int Nz, int shy) {
+
+     int ix = blockIdx.x * blockDim.x + threadIdx.x;
+     int iy = blockIdx.y * blockDim.y + threadIdx.y;
+     int iz = blockIdx.z * blockDim.z + threadIdx.z;
+
+     if(ix < Nx && iy < Ny && iz < Nz) {
+         int iy2 = iy-shy;
+         float newval;
+         if (iy2 < 0) {
+             newval = src[idx(ix, 0, iz)];
+         } else if (iy2 >= Ny) {
+             newval = src[idx(ix, Ny-1, iz)];
+         } else {
+             newval = src[idx(ix, iy2, iz)];
+         }
+         dst[idx(ix, iy, iz)] = newval;
+     }
+ }

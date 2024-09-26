3000b3295bed58a0c58baf3b58d60dab34c5a57a
Jonathan Maes <jonathan145b@hotmail.com>
2024-09-26 11:55:45 +0200

    Add .go files to test with test/run.ps1

--- test/run.ps1
+++ test/run.ps1
@@ -3,7 +3,7 @@ $MUMAX = "$env:GOPATH\bin\mumax3.exe"
 
 # Enter the test directory to (re)compile the cuda kernels
 Set-Location ../test
-    # $mumaxfiles = Get-ChildItem -include ("*.mx3", "*.go") -Name
     $mumaxfiles = Get-ChildItem -filter "*.mx3" -Name
+    $mumaxandgofiles = Get-ChildItem -include ("*.mx3", "*.go") -Name
     & $MUMAX -vet $mumaxfiles
-    & $MUMAX -paranoid=false -failfast -cache=/tmp -http="" -f=true $mumaxfiles
+    & $MUMAX -paranoid=false -failfast -cache=/tmp -http="" -f=true $mumaxandgofiles

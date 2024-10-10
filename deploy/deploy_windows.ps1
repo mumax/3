ea39a845ed6268b9cb45ce339e1678bb60bf2fd9
Jonathan Maes <jonathan145b@hotmail.com>
2024-10-10 12:24:38 +0200

    Automate first switch block in deploy_windows.ps1

--- deploy/deploy_windows.ps1
+++ deploy/deploy_windows.ps1
@@ -5,26 +5,18 @@ param ( # Optional arguments. Example usage: ./deploy_windows.ps1 -CUDA_VERSIONS
     [Int[]]$CUDA_CC
 )
 
-foreach ($CUDA_VERSION in $CUDA_VERSIONS ) {
+foreach ($CUDA_VERSION_STR in $CUDA_VERSIONS ) {
     # The final location of executables and libraries ready to be shipped to the user.
-    $builddir = "build/mumax3.11_windows_cuda$CUDA_VERSION"
+    $builddir = "build/mumax3.11_windows_cuda$CUDA_VERSION_STR"
 
     # The nvidia toolkit installer for CUDA 12.6 should have set the environment 
     # variable CUDA_PATH_V12_6 which points to the root directory of the 
     # CUDA toolbox (or similar for other CUDA versions).
-    switch ( $CUDA_VERSION ) {
-        "10.0" { $CUDA_HOME = $env:CUDA_PATH_V10_0 }
-        "10.1" { $CUDA_HOME = $env:CUDA_PATH_V10_1 }
-        "10.2" { $CUDA_HOME = $env:CUDA_PATH_V10_2 }
-        "11.0" { $CUDA_HOME = $env:CUDA_PATH_V11_0 }
-        "11.1" { $CUDA_HOME = $env:CUDA_PATH_V11_1 }
-        "11.8" { $CUDA_HOME = $env:CUDA_PATH_V11_8 }
-        "12.0" { $CUDA_HOME = $env:CUDA_PATH_V12_0 }
-        "12.6" { $CUDA_HOME = $env:CUDA_PATH_V12_6 }
-        default {}
-    }
+    $CUDA_VERSION = [Version]::Parse($CUDA_VERSION_STR) # Convert to Version type for easy handling and comparison
+    $CUDA_ENV_VAR_NAME = "CUDA_PATH_V$($CUDA_VERSION.Major)_$($CUDA_VERSION.Minor)"
+    $CUDA_HOME = [Environment]::GetEnvironmentVariable($CUDA_ENV_VAR_NAME, "Machine")
     if ( -not $CUDA_HOME -or (-not ( Test-Path $CUDA_HOME )) ) {
-        Write-Output "CUDA version $CUDA_VERSION does not seem to be installed"
+        Write-Output "CUDA version $CUDA_VERSION_STR does not seem to be installed (env:$CUDA_ENV_VAR_NAME does not exist)."
         exit
     }
 
@@ -33,10 +25,10 @@ foreach ($CUDA_VERSION in $CUDA_VERSIONS ) {
     # See VS/CUDA compatibility matrix at https://quasar.ugent.be/files/doc/cuda-msvc-compatibility.html (with old VS downloads available).
     $VS2022 = "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.41.34120\bin\Hostx64\x64" # Supported by CUDA v11.6-v12.*
     $VS2017 = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Tools\MSVC\14.16.27023\bin\Hostx64\x64" # Supported by CUDA v8.0-v12.*
-    switch ( [Version]::Parse($CUDA_VERSION) ) { # Convert to Version type for easy comparison
+    switch ( $CUDA_VERSION ) {
         {$_ -lt [Version]::new(11.6)} { $CCBIN = $VS2017 }
         {$_ -ge [Version]::new(11.6)} { $CCBIN = if ($VS2022) {$VS2022} else {$VS2017} } # Use VS2017 if 2022 not installed
-        default { Write-Output "Failed to parse CUDA version $CUDA_VERSION" }
+        default { Write-Output "Failed to parse CUDA version $CUDA_VERSION_STR" }
     }
     if ( -not ( Test-Path $CCBIN ) ) {
         Write-Output "CCBIN for nvcc not found at $CCBIN"
@@ -47,7 +39,7 @@ foreach ($CUDA_VERSION in $CUDA_VERSIONS ) {
     # See supported CC for each CUDA version at https://stackoverflow.com/a/28933055
     # See min. driver version for each CUDA version at https://docs.nvidia.com/deploy/cuda-compatibility/#minor-version-compatibility
     if ( -not $CUDA_CC ) {
-        switch ( $CUDA_VERSION ) {
+        switch ( $CUDA_VERSION_STR ) {
             "10.0" { $CUDA_CC = 50,52,53,60,61,62,70,72,75 } # Min. Windows driver: >=411.31
             "10.1" { $CUDA_CC = 50,52,53,60,61,62,70,72,75 } # Min. Windows driver: >=418.96
             "10.2" { $CUDA_CC = 50,52,53,60,61,62,70,72,75 } # Min. Windows driver: >=441.22

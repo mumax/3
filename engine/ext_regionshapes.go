e6a961015990e90a50392c630c250a20cbc04e11
JLauzier <45887711+JLauzier@users.noreply.github.com>
2026-02-16 23:00:46 -0800

    Fix regionshapes to work even if regionhist is out of date
    region hist can become out of date if loadfile is used, but now this will still work

--- engine/ext_regionshapes.go
+++ engine/ext_regionshapes.go
@@ -1,8 +1,6 @@
 package engine
 
-import (
-	"github.com/mumax/3/data"
-)
+import "github.com/mumax/3/data"
 
 func init() {
 	DeclFunc("Region2Shape", Region2Shape, "Shape containing all cells belonging to the given region")
@@ -11,20 +9,77 @@ func init() {
 
 func Region2Shape(region int) Shape {
 	defRegionId(region)
+	mesh := Mesh()
+	arr := regions.HostList()
+	n := mesh.Size()
+	d := mesh.CellSize()
+	Lx, Ly, Lz := float64(n[X])*d[X], float64(n[Y])*d[Y], float64(n[Z])*d[Z]
 
 	return func(x, y, z float64) bool {
-		R := data.Vector{x, y, z}
-		return regions.get(R) == region
+
+		ix := int((x + 0.5*Lx) / d[X])
+		iy := int((y + 0.5*Ly) / d[Y])
+		iz := int((z + 0.5*Lz) / d[Z])
+
+		if ix < 0 || ix >= n[X] || iy < 0 || iy >= n[Y] || iz < 0 || iz >= n[Z] {
+			return false
+		}
+
+		i := data.Index(n, ix, iy, iz)
+		return arr[i] == byte(region)
 	}
 }
 
 func AllRegionShapes() func(int) Shape {
+	mesh := Mesh()
+	arr := regions.HostList()
+	n := mesh.Size()
+	d := mesh.CellSize()
+	Lx, Ly, Lz := float64(n[X])*d[X], float64(n[Y])*d[Y], float64(n[Z])*d[Z]
+
 	return func(region int) Shape {
 		defRegionId(region)
 
 		return func(x, y, z float64) bool {
-			R := data.Vector{x, y, z}
-			return regions.get(R) == region
+			ix := int((x + 0.5*Lx) / d[X])
+			iy := int((y + 0.5*Ly) / d[Y])
+			iz := int((z + 0.5*Lz) / d[Z])
+
+			if ix < 0 || ix >= n[X] || iy < 0 || iy >= n[Y] || iz < 0 || iz >= n[Z] {
+				return false
+			}
+
+			i := data.Index(n, ix, iy, iz)
+			return arr[i] == byte(region)
 		}
+	}
+}
+
+func ShiftShapeX(s Shape, dx int) Shape {
+	if dx == 0 {
+		return s
+	}
+
+	mesh := Mesh()
+	d := mesh.CellSize()
+
+	shift := float64(dx) * d[X]
+
+	return func(x, y, z float64) bool {
+		return s(x-shift, y, z)
+	}
+}
+
+func ShiftShapeY(s Shape, dy int) Shape {
+	if dy == 0 {
+		return s
+	}
+
+	mesh := Mesh()
+	d := mesh.CellSize()
+	shift := float64(dy) * d[Y]
+
+	return func(x, y, z float64) bool {
+		return s(x, y-shift, z)
 	}
 }

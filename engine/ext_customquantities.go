047f0a4fd6d504dacc2a36d1a7f1ec84726e85c0
JLauzier <45887711+JLauzier@users.noreply.github.com>
2026-03-18 01:58:14 -0700

    Feature:Custom Quantities
    This Branch adds the ability for the user to define promote arbitrary slices (both scalar masks and vector masks) into a Custom Quantity.
    
    Implementation follows xfong's implementation in the https://github.com/seeder-research/umagnus fork.

---
+++ engine/ext_customquantities.go
@@ -0,0 +1,43 @@
+package engine
+
+import (
+	"github.com/mumax/3/cuda"
+	"github.com/mumax/3/data"
+	"github.com/mumax/3/util"
+)
+
+func init() {
+	DeclFunc("CustomQuantity", CustomQuantity, "Creates a custom quantity from the provided data slice.")
+}
+
+// CustomQuantity creates a custom quantity from the provided data slice.
+func CustomQuantity(inSlice *data.Slice) Quantity {
+	util.Assert(inSlice.NComp() == 1 || inSlice.NComp() == 3)
+	size := Mesh().Size()
+	sliceSize := inSlice.Size()
+	util.Assert(size[X] == sliceSize[X] && size[Y] == sliceSize[Y] && size[Z] == sliceSize[Z])
+
+	retQuant := &customQuantity{nil, size}
+	if inSlice.NComp() == 1 {
+		retQuant.customquant = cuda.NewSlice(SCALAR, size)
+	} else {
+		retQuant.customquant = cuda.NewSlice(VECTOR, size)
+	}
+
+	data.Copy(retQuant.customquant, inSlice)
+	return retQuant
+}
+
+type customQuantity struct {
+	customquant *data.Slice
+	size        [3]int
+}
+
+func (q *customQuantity) NComp() int {
+	return q.customquant.NComp()
+}
+
+func (q *customQuantity) EvalTo(dst *data.Slice) {
+	util.Assert(dst.NComp() == q.customquant.NComp())
+	data.Copy(dst, q.customquant)
+}
